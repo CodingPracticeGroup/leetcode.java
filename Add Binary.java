76db749ae0528fa053cc6a1560b96452259fe7ab
Huijun Wu <huijun.wu.2010@gmail.com>
2013-09-15 16:59:43 -0700

    another solution, see history for more solutions

--- Add Binary.java
+++ Add Binary.java
@@ -1,41 +1,52 @@
 public class Solution {
-    public String addBinary(String a, String b) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        if(a==null) return b;
-        if(b==null) return a;
-        int alen=a.length()-1;
-        int blen=b.length()-1;
-        String c="";
-        int digit=0;int temp=0;
-        while(alen>=0&&blen>=0){
-            int tempa=a.charAt(alen)-'0';
-            int tempb=b.charAt(blen)-'0';
-            temp=(tempa+tempb+digit)%2;
-            digit=(tempa+tempb+digit)/2;
-            char tempc=(char) ('0'+temp);
-            c=(tempc)+c;
-            alen--;blen--;
-        }
-        while(alen>=0){
-            int tempa=a.charAt(alen)-'0';
-            temp=(tempa+digit)%2;
-            digit=(tempa+digit)/2;
-            char tempc=(char) ('0'+temp);
-            c=(tempc)+c;
-            alen--;
-        }
-        while(blen>=0){
-            int tempb=b.charAt(blen)-'0';
-            temp=(tempb+digit)%2;
-            digit=(tempb+digit)/2;
-            char tempc=(char) ('0'+temp);
-            c=(tempc)+c;
-            blen--;
-        }
-        if(digit>0){
-            c='1'+c;
-        }
-        return c;
-    }
+	public String addBinary(String a, String b) {
+		// Start typing your Java solution below
+		// DO NOT write main() function
+		String a_ = new StringBuilder(a).reverse().toString();
+		String b_ = new StringBuilder(b).reverse().toString();
+		StringBuilder sb = new StringBuilder();
+		int t = 0;
+		int len_a = a_.length();
+		int len_b = b_.length();
+		if (len_a < len_b) {
+			int i = 0;
+			while (i < len_a) {
+				int sum = a_.charAt(i) - '0' + b_.charAt(i) - '0' + t;
+				t = sum / 2;
+				sb.append(sum % 2);
+				i++;
+			}
+			while (i < len_b) {
+				int sum = b_.charAt(i) - '0' + t;
+				t = sum / 2;
+				sb.append(sum % 2);
+				i++;
+			}
+		} else if (len_a > len_b) {
+			int i = 0;
+			while (i < len_b) {
+				int sum = a_.charAt(i) - '0' + b_.charAt(i) - '0' + t;
+				t = sum / 2;
+				sb.append(sum % 2);
+				i++;
+			}
+			while (i < len_a) {
+				int sum = a_.charAt(i) - '0' + t;
+				t = sum / 2;
+				sb.append(sum % 2);
+				i++;
+			}
+		} else {
+			int i = 0;
+			while (i < len_b) {
+				int sum = a_.charAt(i) - '0' + b_.charAt(i) - '0' + t;
+				t = sum / 2;
+				sb.append(sum % 2);
+				i++;
+			}
+		}
+		if (t == 1)
+			sb.append(t);
+		return sb.reverse().toString();
+	}
 }
