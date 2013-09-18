4587044c2ddd0dfef0f0e406897b3418d897bb93
Huijun Wu <huijun.wu.2010@gmail.com>
2013-09-17 20:56:55 -0700

    just another code
    see history for better code

--- Triangle.java
+++ Triangle.java
@@ -1,19 +1,24 @@
 public class Solution {
-    public int minimumTotal(ArrayList<ArrayList<Integer>> triangle) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        int n=triangle.size();
-        if(0==n) return 0;
-        int[] dp = new int[n];
-        dp[0]=triangle.get(0).get(0);
-        for(int i=1;i<n;i++){
-            for(int j=i;j>=0;j--){
-                dp[j]=triangle.get(i).get(j)+
-                Math.min((i!=j?dp[j]:Integer.MAX_VALUE),
-                (j>0?dp[j-1]:Integer.MAX_VALUE));
-            }
-        }
-        Arrays.sort(dp);
-        return dp[0];
-    }
+	public int minimumTotal(ArrayList<ArrayList<Integer>> triangle) {
+		// Start typing your Java solution below
+		// DO NOT write main() function
+		int size = triangle.size();
+		int dp[] = new int[size];
+		for (int i = 0; i < size; i++)
+			dp[i] = 0;
+		for (ArrayList<Integer> row : triangle) {
+			int size2 = row.size();
+			if (size2 - 2 >= 0)
+				dp[size2 - 1] = row.get(size2 - 1) + dp[size2 - 2];
+			for (int i = size2 - 2; i > 0; i--) {
+				dp[i] = Math.min(dp[i - 1], dp[i]) + row.get(i);
+			}
+			dp[0] += row.get(0);
+		}
+		int min = dp[0];
+		for (int i = 1; i < size; i++)
+			if (dp[i] < min)
+				min = dp[i];
+		return min;
+	}
 }
