37d838af673e5e3ec24cf9771f444db378f983d2
hjhjhj <huizhouhust@gmail.com>
2013-09-19 23:57:27 -0500

    Update Best Time to Buy and Sell Stock II.java

--- Best Time to Buy and Sell Stock II.java
+++ Best Time to Buy and Sell Stock II.java
@@ -1,16 +1,15 @@
 public class Solution {
-	public int maxProfit(int[] prices) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		int sum = 0;
-		if (prices == null || prices.length == 0)
-			return sum;
-		int[] delta = new int[prices.length - 1];
-		for (int i = 1; i < prices.length; i++)
-			delta[i - 1] = prices[i] - prices[i - 1];
-		for (int i = 0; i < delta.length; i++)
-			if (delta[i] > 0)
-				sum += delta[i];
-		return sum;
-	}
+    public int maxProfit(int[] prices) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(prices==null) return 0;
+        int max=0;
+        int len=prices.length;
+        for(int i=1;i<len;i++){
+            if(prices[i]>prices[i-1]){
+                max+=prices[i]-prices[i-1];
+            }
+        }
+        return max;
+    }
 }
