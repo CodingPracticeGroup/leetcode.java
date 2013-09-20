aa863d3fdc0b0b51cb726c65a635046fd7b0fe6a
Huijun Wu <huijun.wu.2010@gmail.com>
2013-09-19 21:40:09 -0700

    code 11

--- Longest Valid Parentheses.java
+++ Longest Valid Parentheses.java
@@ -2,54 +2,43 @@ public class Solution {
 	public int longestValidParentheses(String s) {
 		// Start typing your Java solution below
 		// DO NOT write main() function
-		return Math.max(left2right(s), right2left(s));
-	}
-
-	int left2right(String s) {
+		if (s == null)
+			return 0;
 		int len = s.length();
-		int max = 0;
-		int current = 0;
-		int tmp_len = 0;
+		if (len < 2)
+			return 0;
+		int result1 = 0;
+		int restemp = 0;
+		int temp = 0;
+		// ordinary
 		for (int i = 0; i < len; i++) {
-			if (s.charAt(i) == '(') {
-				current++;
-			} else if (s.charAt(i) == ')') {
-				current--;
-			}
-			tmp_len++;
-			if (current == 0) {
-				if (tmp_len > max) {
-					max = tmp_len;
-				}
-			} else if (current < 0) {
-				current = 0;
-				tmp_len = 0;
-			}
+			if ('(' == s.charAt(i))
+				temp++;
+			else
+				temp--;
+			restemp++;
+			if (temp < 0) {
+				restemp = 0;
+				temp = 0;
+			} else if (temp == 0)
+				result1 = Math.max(result1, restemp);
 		}
-		return max;
-	}
-
-	int right2left(String s) {
-		int len = s.length();
-		int max = 0;
-		int current = 0;
-		int tmp_len = 0;
+		// reverse
+		int result2 = 0;
+		restemp = 0;
+		temp = 0;
 		for (int i = len - 1; i >= 0; i--) {
-			if (s.charAt(i) == ')') {
-				current++;
-			} else if (s.charAt(i) == '(') {
-				current--;
-			}
-			tmp_len++;
-			if (current == 0) {
-				if (tmp_len > max) {
-					max = tmp_len;
-				}
-			} else if (current < 0) {
-				current = 0;
-				tmp_len = 0;
-			}
+			if (')' == s.charAt(i))
+				temp++;
+			else
+				temp--;
+			restemp++;
+			if (temp < 0) {
+				restemp = 0;
+				temp = 0;
+			} else if (temp == 0)
+				result2 = Math.max(result2, restemp);
 		}
-		return max;
+		return Math.max(result1, result2);
 	}
 }
