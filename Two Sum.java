34c302409e80c0981d4844fb55d5e25c1af1d5ed
hjhjhj <huizhouhust@gmail.com>
2013-10-17 00:33:29 -0500

    Update Two Sum.java

--- Two Sum.java
+++ Two Sum.java
@@ -1,20 +1,40 @@
 public class Solution {
-	public int[] twoSum(int[] numbers, int target) {
-		// Note: The Solution object is instantiated only once and is reused by each test case.
-		Set s = new HashSet<Integer>();
-		for (int i = 0; i < numbers.length; i++)
-			s.add(numbers[i]);
-		int ret[] = new int[2];
-		int i;
-		for (i = 0; i < numbers.length; i++)
-			if (s.contains(target - numbers[i])) {
-				ret[0] = i + 1;
-				break;
-			}
-		for (int j = i; j < numbers.length; j++)
-			if (numbers[j] == target - numbers[ret[0] - 1]) {
-				ret[1] = j + 1;
-			}
-		return ret;
-	}
+    public int[] twoSum(int[] numbers, int target) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if(numbers==null) return null;
+        int n=numbers.length;
+        if(n==0) return null;
+        int[] temp = (int[])numbers.clone();
+        Arrays.sort(numbers);
+        int[] result=new int[2];
+        int i=0;
+        int j=n-1;
+        while(i<j){
+            if(numbers[i]+numbers[j]>target){
+                j--;
+            }else if(numbers[i]+numbers[j]<target){
+                i++;
+            }else{
+                break;
+            }
+        }
+        if(numbers[i]+numbers[j] == target){
+            for(int k=0;k<n;k++){
+                if(temp[k]==numbers[i]) {
+                    result[0]=k+1;break;
+                }
+            }
+            int k=0;
+            if(numbers[i]==numbers[j]){
+            	k=result[0];
+            }
+            for(;k<n;k++){
+                if(temp[k]==numbers[j]) {
+                	result[1]=k+1;break;
+                }
+            }
+        }
+        Arrays.sort(result);
+        return result;
+    }
 }
