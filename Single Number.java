1ec4e4e9c91084a9eb1891cdf1a3456ad056a9aa
hjhjhj <huizhouhust@gmail.com>
2013-10-17 00:43:22 -0500

    Update Single Number.java

--- Single Number.java
+++ Single Number.java
@@ -1,8 +1,12 @@
 public class Solution {
-	public int singleNumber(int[] A) {
-		// Note: The Solution object is instantiated only once and is reused by each test case.
-		for (int i = 1; i < A.length; i++)
-			A[0] ^= A[i];
-		return A[0];
-	}
+    public int singleNumber(int[] A) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        int n = A.length;
+        
+        int result=0;
+        for(int i=0;i<n;i++){
+            result^=A[i];
+        }
+        return result;
+    }
 }
