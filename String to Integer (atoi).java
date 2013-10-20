76b9f387fa0f34412d1e5ba2dd264f6ac926371e
hjhjhj <huizhouhust@gmail.com>
2013-10-19 23:27:22 -0500

    Update String to Integer (atoi).java

--- String to Integer (atoi).java
+++ String to Integer (atoi).java
@@ -1,37 +1,41 @@
 public class Solution {
-	public int atoi(String str) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		String str2 = str.trim();
-		if (str2.length() == 0)
-			return 0;
-		boolean minus = false;
-		if (str2.charAt(0) == '-') {
-			minus = true;
-			str2 = str2.substring(1);
-		} else if (str2.charAt(0) == '+') {
-			str2 = str2.substring(1);
-		}
-		int sum = 0;
-		for (int i = 0; i < str2.length(); i++) {
-			if (str2.charAt(i) < '0' || str2.charAt(i) > '9')
-				break;
-			if (minus == false && sum > Integer.MAX_VALUE / 10) {
-				return Integer.MAX_VALUE;
-			} else if (minus && sum + Integer.MIN_VALUE / 10 > 0) {
-				return Integer.MIN_VALUE;
-			}
-			sum *= 10;
-			if (minus == false && str2.charAt(i) - '0' > Integer.MAX_VALUE - sum) {
-				return Integer.MAX_VALUE;
-			} else if (minus && -(str2.charAt(i) - '0') < Integer.MIN_VALUE + sum) {
-				return Integer.MIN_VALUE;
-			}
-			sum += str2.charAt(i) - '0';
-		}
-		if (minus)
-			return -sum;
-		else
-			return sum;
-	}
+    public int atoi(String str) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        str=str.trim();
+        if(str.length()<=0) return 0;
+        int sign=1;
+        int i=0;
+        if(str.charAt(0)=='-'){
+            sign=-1;
+            i++;
+        }else if(str.charAt(0)=='+'){
+            i++;
+        }
+        
+        if(!Character.isDigit(str.charAt(i))){
+            return 0;
+        }
+        
+        int result=0;
+        int temp=0;
+        
+        while(i<str.length()){
+            temp=str.charAt(i)-'0';
+            if(temp<0||temp>9) break;
+            if(sign==-1){
+                if((Integer.MIN_VALUE+temp)/10+result>0)
+                {
+                    return Integer.MIN_VALUE;
+                }
+            }else if(result>=(Integer.MAX_VALUE-temp)/10){
+              result=Integer.MAX_VALUE;
+              break;
+            } 
+            result=result*10+temp;
+            i++;
+        }
+        
+        return sign*result;
+    }
 }
