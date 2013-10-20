abf8eb12d4cc42d5bfc5dbe02f6878a5bfc6b22b
Huijun Wu <huijun.wu.2010@gmail.com>
2013-10-19 20:36:06 -0700

    another version

--- Gas Station.java
+++ Gas Station.java
@@ -1,29 +1,35 @@
 public class Solution {
-    public int canCompleteCircuit(int[] gas, int[] cost) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
-        int n=gas.length;
-        
-        int residule=0;
-        int sumgas=0;
-        for(int i=0;i<n;i++){
-            sumgas+=gas[i];
-        }
-        int sumcost=0;
-        for(int i=0;i<n;i++){
-            sumcost+=cost[i];
-        }
-        if(sumgas<sumcost) return -1;
-        int result=0;
-        
-        for(int i=0;i<n;i++){
-            residule=gas[i]-cost[i];
-            result=i;
-            while(residule>0&&i<n-1){
-                i++;
-                residule+=gas[i]-cost[i];
-            }
-        }
-        
-        return result;
-    }
+	public int canCompleteCircuit(int[] gas, int[] cost) {
+		// Note: The Solution object is instantiated only once and is reused by each test case.
+		int total = gas.length;
+		int gas_sum = 0;
+		int cost_sum = 0;
+		for (int i = 0; i < total; i++) {
+			gas_sum += gas[i];
+			cost_sum += cost[i];
+		}
+		if (cost_sum > gas_sum) {
+			return -1;
+		}
+		int current = 0;
+		for (; current < total;) {
+			gas_sum = 0;
+			cost_sum = 0;
+			boolean pass = true;
+			int i;
+			for (i = current; i < total; i++) {
+				gas_sum += gas[i];
+				cost_sum += cost[i];
+				if (cost_sum > gas_sum) {
+					current = i + 1;
+					pass = false;
+					break;
+				}
+			}
+			if (pass) {
+				return current;
+			}
+		}
+		return current;
+	}
 }
