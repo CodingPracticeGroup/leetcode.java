b5f521ac24715d427dc13aecb61e4eb7cbd7dede
hjhjhj <huizhouhust@gmail.com>
2013-10-19 22:11:36 -0500

    Update Candy.java

--- Candy.java
+++ Candy.java
@@ -1,26 +1,22 @@
 public class Solution {
-	public int candy(int[] ratings) {
-		// Note: The Solution object is instantiated only once and is reused by each test case.
-		int[] left2right = new int[ratings.length];
-		int[] right2left = new int[ratings.length];
-		left2right[0] = 1;
-		for (int i = 1; i < ratings.length; i++) {
-			if (ratings[i] > ratings[i - 1])
-				left2right[i] = left2right[i - 1] + 1;
-			else
-				left2right[i] = 1;
-		}
-		right2left[ratings.length - 1] = 1;
-		for (int i = ratings.length - 2; i >= 0; i--) {
-			if (ratings[i] > ratings[i + 1])
-				right2left[i] = right2left[i + 1] + 1;
-			else
-				right2left[i] = 1;
-		}
-		int sum = 0;
-		for (int i = 0; i < ratings.length; i++) {
-			sum += Math.max(left2right[i], right2left[i]);
-		}
-		return sum;
-	}
+    public int candy(int[] ratings) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        int n=ratings.length;
+        int[] candys=new int[n];
+        candys[0]=1;
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1])
+              candys[i]=candys[i-1]+1;
+            else candys[i]=1;
+        }
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1])
+              candys[i]=Math.max(candys[i],candys[i+1]+1);
+        }
+        int result=0;
+        for(int i=0;i<n;i++){
+            result+=candys[i];
+        }
+        return result;
+    }
 }
