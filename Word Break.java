8bcdb3152046d687cc5feaee82766fb480810f98
sealove <huijun.wu.2010@gmail.com>
2014-03-01 15:53:17 -0700

    Word Break
    2nd edition

--- Word Break.java
+++ Word Break.java
@@ -1,26 +1,23 @@
+import java.util.Set;
+
 public class Solution {
-    public boolean wordBreak(String s, Set<String> dict) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
-        int n = s.length();
-        int[][] dp = new int[n][n];
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(dict.contains(s.substring(i,j+1))){
-                    dp[i][j]=1;
-                }
-            }
-        }
-        for(int i=0;i<n;i++){
-            if(dp[0][i]==1){
-                if(i==n-1) return true;
-                for(int j=i;j<n;j++){
-                    if(dp[i+1][j]==1){
-                        dp[0][j]=1;
-                    }
-                }
-            }
-        }
-        if(dp[0][n-1]==1) return true;
-        else return false;
-    }
+	public boolean wordBreak(String s, Set<String> dict) {
+		int len = s.length();
+		boolean[] dpCanDo = new boolean[len]; // substring i-len can be broken
+		for (int i = 0; i < len; i++) {
+			if (dict.contains(s.substring(i, len))) {
+				dpCanDo[i] = true;
+			}
+		}
+		for (int end = len - 1; end > 0; end--) {
+			if (dpCanDo[end]) {
+				for (int i = 0; i < end; i++) {
+					if (dict.contains(s.substring(i, end))) {
+						dpCanDo[i] = true;
+					}
+				}
+			}
+		}
+		return dpCanDo[0];
+	}
 }
