ec4fdd70d01808495657c3c9354ba382afa118e3
sealove <huijun.wu.2010@gmail.com>
2014-03-01 21:40:29 -0700

    Single Number II
    update 2

--- Single Number II.java
+++ Single Number II.java
@@ -1,14 +1,20 @@
 public class Solution {
 	public int singleNumber(int[] A) {
-		// Note: The Solution object is instantiated only once and is reused by each test case.
-		int ones = 0, twos = 0, xthrees = 0;
-		for (int i = 0; i < A.length; ++i) {
-			twos |= (ones & A[i]);
-			ones ^= A[i];
-			xthrees = ~(ones & twos);
-			ones &= xthrees;
-			twos &= xthrees;
+		int zeros = -1, ones = 0, twos = 0;
+		for (int i = 0; i < A.length; i++) {
+			int tmpZero = zeros;
+			int tmpOne = ones;
+			int tmpTwo = twos;
+			ones = (tmpOne | (tmpZero & A[i])) & (~(tmpOne & A[i]));
+			twos = (tmpTwo | (tmpOne & A[i])) & (~(tmpTwo & A[i]));
+			zeros = (tmpZero | (tmpTwo & A[i])) & (~(tmpZero & A[i]));
 		}
 		return ones;
 	}
+
+	public static void main(String[] args) throws Exception {
+		int A[] = new int[] { -2, -2, -2, 1, 1, 1, -3, -3, -3, -4 };
+		new Solution().singleNumber(A);
+	}
+
 }
