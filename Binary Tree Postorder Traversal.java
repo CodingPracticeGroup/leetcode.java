b2ea4cbef2c0bc6c872d7f1dcde3188a010c5d08
hjhjhj <huizhouhust@gmail.com>
2014-03-04 16:41:18 -0600

    Update Binary Tree Postorder Traversal.java

--- Binary Tree Postorder Traversal.java
+++ Binary Tree Postorder Traversal.java
@@ -37,4 +37,18 @@ public class Solution {
 
 		return ret;
 	}
+	public ArrayList<Integer> postorderTraversal(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        if(null==root) return result;
+        stack.push(root);
+        while(!stack.empty()){
+            TreeNode node = stack.peek();
+            //if(node!=null) result.add(node.val); else continue;
+            if(node.right==null&&node.left==null){TreeNode temp=stack.pop();result.add(temp.val);}
+            if(node.right!=null) {stack.push(node.right);node.right=null;}
+            if(node.left!=null) {stack.push(node.left);node.left=null;}
+        }
+        return result;
+    }
 }
