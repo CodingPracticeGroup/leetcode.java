4b8cc535c690cd480f6645b7fc24490b8740c940
sealove <huijun.wu.2010@gmail.com>
2014-03-04 23:11:14 -0700

    Populating Next Right Pointers in Each Node II

--- Populating Next Right Pointers in Each Node II.java
+++ Populating Next Right Pointers in Each Node II.java
@@ -1,50 +1,42 @@
 /**
- * Definition for binary tree with next pointer. public class TreeLinkNode { int val; TreeLinkNode
- * left, right, next; TreeLinkNode(int x) { val = x; } }
- */
+ * Definition for binary tree with next pointer.
+ */ 
+class TreeLinkNode {
+      int val;
+      TreeLinkNode left, right, next;
+      TreeLinkNode(int x) { val = x; }
+  }
+
 public class Solution {
     public void connect(TreeLinkNode root) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        if(root==null) return;
-        root.next=null;
-        TreeLinkNode first=root;
-        TreeLinkNode firstcd=null;
-        TreeLinkNode nextcd=null;
-        TreeLinkNode temp;
-        
-        while(first!=null){
-            temp=first;
-            firstcd=null;
-            //find first child
-            while(temp!=null){
-                if(temp.left!=null){
-                    firstcd=temp.left; break;
-                }
-                if(temp.right!=null){
-                    firstcd=temp.right; break;
-                }
-                temp=temp.next;
-            }
-            first=firstcd;
-            
-            while(firstcd!=null&&temp!=null){
-                nextcd=null;
-                while(temp!=null){
-                    //find nextchild
-                    if(firstcd==temp.right) {temp=temp.next;continue;}
-                    if(temp.left!=null&&temp.left!=firstcd){
-                        nextcd=temp.left; break;
+        if (root!=null) {
+            TreeLinkNode memory = root;
+            while (memory.left!=null || memory.right!=null) {
+                for (TreeLinkNode pointer = memory;pointer!=null;) {
+                    if (pointer.left!=null && pointer.right!=null) {
+                        pointer.left.next = pointer.right;
                     }
-                    if(temp.right!=null&&temp.right!=firstcd){
-                        nextcd=temp.right; break;
+                    TreeLinkNode pointerNext = pointer.next;
+                    while (pointerNext!=null && pointerNext.left==null && pointerNext.right==null) {
+                        pointerNext = pointerNext.next;
                     }
-                    temp=temp.next;
+                    if (pointerNext!=null) {
+                        if (pointer.right==null) {
+                            pointer.left.next = pointerNext.left!=null?pointerNext.left:pointerNext.right;
+                        } else {
+                            pointer.right.next = pointerNext.left!=null?pointerNext.left:pointerNext.right;
+                        }
+                    }
+                    pointer=pointerNext;
                 }
-                firstcd.next=nextcd;
-                firstcd=nextcd;
+                //
+                memory = memory.left!=null? memory.left: memory.right;
+                while (memory.next!=null && memory.left==null && memory.right==null) {
+                    memory = memory.next;
+                }
             }
         }
-        return;
     }
 }
