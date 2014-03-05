b275d122049786e0a0769c4f49dd7c78f854e366
hjhjhj <huizhouhust@gmail.com>
2014-03-04 19:04:31 -0600

    Update Evaluate Reverse Polish Notation.java

--- Evaluate Reverse Polish Notation.java
+++ Evaluate Reverse Polish Notation.java
@@ -26,6 +26,42 @@ public class Solution {
 		}
 		return stack.pollLast();
 	}
+	
+	public int evalRPN(String[] tokens) {
+        Stack<Integer> numbers = new Stack<Integer>();
+        if(tokens==null) return -1;
+        int size = tokens.length;
+        int result=-1;
+        if(size==0) return -1;
+        
+        for(int i=0;i<size;i++){
+           
+            int temp;
+            try{
+                temp=Integer.valueOf(tokens[i]);
+                numbers.push(temp);
+                continue;
+            }catch(Exception e){
+            }
+            
+            if(numbers.empty()) return -1;
+            int number1 = numbers.pop();
+            if(numbers.empty()) return -1;
+            int number2 = numbers.pop();
+            
+            if(tokens[i].equals("+")) temp= number1 + number2;
+            else if(tokens[i].equals("/")) temp= number2 / number1;
+            else if(tokens[i].equals("*")) temp= number1 * number2;
+            else if(tokens[i].equals("-")) temp= number2 - number1;
+            else  return -1; 
+            
+            numbers.push(temp);
+        }
+        if(numbers.empty()) return -1;
+        result= numbers.pop();
+        
+        return result;
+    }
 
 	public static void main(String[] args) {
 		String[] expr = new String[5];
