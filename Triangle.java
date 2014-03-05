6fbd922d95e22b9b2fc9fa595443b2236f9ba368
sealove <huijun.wu.2010@gmail.com>
2014-03-04 19:27:23 -0700

    Triangle

--- Triangle.java
+++ Triangle.java
@@ -1,24 +1,41 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Solution {
 	public int minimumTotal(ArrayList<ArrayList<Integer>> triangle) {
 		// Start typing your Java solution below
 		// DO NOT write main() function
-		int size = triangle.size();
-		int dp[] = new int[size];
-		for (int i = 0; i < size; i++)
-			dp[i] = 0;
-		for (ArrayList<Integer> row : triangle) {
-			int size2 = row.size();
-			if (size2 - 2 >= 0)
-				dp[size2 - 1] = row.get(size2 - 1) + dp[size2 - 2];
-			for (int i = size2 - 2; i > 0; i--) {
-				dp[i] = Math.min(dp[i - 1], dp[i]) + row.get(i);
+		ArrayList<Integer> dpLevel = new ArrayList<Integer>(triangle.size());
+		dpLevel.add(triangle.get(0).get(0));
+		for (int i = 1; i < triangle.size(); i++) {
+			ArrayList<Integer> dpLevelDuplicate = new ArrayList<Integer>(dpLevel);
+			dpLevel.clear();
+			for (int j = 0; j < triangle.get(i).size(); j++) {
+				if (j == 0) {
+					dpLevel.add(dpLevelDuplicate.get(0) + triangle.get(i).get(j));
+				} else if (j == triangle.get(i).size() - 1) {
+					dpLevel.add(dpLevelDuplicate.get(triangle.get(i - 1).size() - 1) + triangle.get(i).get(j));
+				} else {
+					dpLevel.add(Math.min(dpLevelDuplicate.get(j - 1), dpLevelDuplicate.get(j)) + triangle.get(i).get(j));
+				}
 			}
-			dp[0] += row.get(0);
 		}
-		int min = dp[0];
-		for (int i = 1; i < size; i++)
-			if (dp[i] < min)
-				min = dp[i];
-		return min;
+		return Collections.min(dpLevel);
 	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ArrayList<ArrayList<Integer>> triangle = new ArrayList<ArrayList<Integer>>();
+		ArrayList<Integer> row1 = new ArrayList<Integer>();
+		row1.add(-1);
+		ArrayList<Integer> row2 = new ArrayList<Integer>();
+		row2.add(-2);
+		row2.add(-3);
+		triangle.add(row1);
+		triangle.add(row2);
+		new Solution().minimumTotal(triangle);
+	}
+
 }
