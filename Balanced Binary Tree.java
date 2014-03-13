6a97abb765fc289e9e3f9b2832e6ba7a76944429
sealove <huijun.wu.2010@gmail.com>
2014-03-13 08:31:52 -0700

    Balanced Binary Tree

--- Balanced Binary Tree.java
+++ Balanced Binary Tree.java
@@ -1,29 +1,37 @@
 /**
- * Definition for binary tree public class TreeNode { int val; TreeNode left; TreeNode right;
- * TreeNode(int x) { val = x; } }
+ * Definition for binary tree
  */
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
+
 public class Solution {
-    public boolean isBalanced(TreeNode root) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        boolean[] flag=new boolean[1];
-        isB(root,flag);
-        return flag[0];
-    }
-    
-    public int isB(TreeNode node, boolean[] flag){
-        if(node==null){
-            flag[0]=true;
-            return 0;
-        }
-        boolean[] f1=new boolean[1];
-        int h1=isB(node.left,f1);
-        boolean[] f2=new boolean[1];
-        int h2=isB(node.right,f2);
-        if(f1[0]&&f2[0]){
-            flag[0]=(Math.abs(h1-h2)<=1);
-        }else flag[0]=false;
-        
-        return 1+Math.max(h1,h2);
-    }
+	public boolean isBalanced(TreeNode root) {
+		// Start typing your Java solution below
+		// DO NOT write main() function
+
+		// cannot use Boolean ret = new Boolean(true); because Boolean does not have setting function
+		boolean[] ret = new boolean[1];
+		ret[0] = true;
+		height(root, ret);
+		return ret[0];
+	}
+
+	int height(TreeNode root, boolean[] balanced) {
+		if (root == null) {
+			return 0;
+		}
+		int leftHeight = height(root.left, balanced);
+		int rightHeight = height(root.right, balanced);
+		if (Math.abs(leftHeight - rightHeight) > 1) {
+			balanced[0] = false;
+		}
+		return Math.max(leftHeight, rightHeight) + 1;
+	}
 }
