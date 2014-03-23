20a3fb6e2dd204371da7d30cc918a55c03941c91
hjhjhj <huizhouhust@gmail.com>
2014-03-22 21:03:36 -0500

    Update Same Tree.java

--- Same Tree.java
+++ Same Tree.java
@@ -1,27 +1,21 @@
 /**
  * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
  */
-class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
-
-	TreeNode(int x) {
-		val = x;
-	}
-}
-
 public class Solution {
-	public boolean isSameTree(TreeNode p, TreeNode q) {
-		if (p == null && q == null) {
-			return true;
-		}
-		if (p != null && q != null) {
-			if (p.val != q.val) {
-				return false;
-			}
-			return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-		}
-		return false;
-	}
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null) {
+            if(q==null) return true;
+            else return false;
+        }
+        if(q==null) return false;
+        
+        if(p.val!=q.val) return false;
+        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+    }
 }
