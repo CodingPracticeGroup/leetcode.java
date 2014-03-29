65ccdab1217c55f35ca1e297ea669cff08f174ce
sealove <huijun.wu.2010@gmail.com>
2014-03-29 10:42:51 -0700

    Minimum Path Sum

--- Minimum Path Sum.java
+++ Minimum Path Sum.java
@@ -1,22 +1,21 @@
 public class Solution {
 	public int minPathSum(int[][] grid) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		int m = grid.length;
-		int n = grid[0].length;
-		int[][] dp = new int[m][n];
+		int dp[][] = new int[grid.length][grid[0].length];
+
 		dp[0][0] = grid[0][0];
-		for (int i = 1; i < m; i++) {
+		for (int i = 1; i < grid.length; i++) {
 			dp[i][0] = dp[i - 1][0] + grid[i][0];
 		}
-		for (int i = 1; i < n; i++) {
+		for (int i = 1; i < grid[0].length; i++) {
 			dp[0][i] = dp[0][i - 1] + grid[0][i];
 		}
-		for (int i = 1; i < m; i++) {
-			for (int j = 1; j < n; j++) {
+
+		for (int i = 1; i < grid.length; i++) {
+			for (int j = 1; j < grid[0].length; j++) {
 				dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
 			}
 		}
-		return dp[m - 1][n - 1];
+
+		return dp[grid.length - 1][grid[0].length - 1];
 	}
 }
