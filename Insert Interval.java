486339e6f16264fb7cf8002d319961ac978a3d1b
sealove <huijun.wu.2010@gmail.com>
2014-03-29 16:24:46 -0700

    Insert Interval

--- Insert Interval.java
+++ Insert Interval.java
@@ -1,39 +1,46 @@
 import java.util.ArrayList;
-import java.util.Iterator;
 
 /**
- * Definition for an interval. public class Interval { int start; int end;
- * Interval() { start = 0; end = 0; } Interval(int s, int e) { start = s; end =
- * e; } }
+ * Definition for an interval.
  */
+class Interval {
+	int start;
+	int end;
+
+	Interval() {
+		start = 0;
+		end = 0;
+	}
+
+	Interval(int s, int e) {
+		start = s;
+		end = e;
+	}
+}
+
 public class Solution {
 	public ArrayList<Interval> insert(ArrayList<Interval> intervals, Interval newInterval) {
 		// Start typing your Java solution below
 		// DO NOT write main() function
-		ArrayList<Interval> r = new ArrayList<Interval>();
-		if (intervals.size() == 0) {
-			r.add(newInterval);
-			return r;
-		}
-		for (Iterator<Interval> it = intervals.iterator(); it.hasNext();) {
-			Interval tmp = it.next();
-			if (newInterval == null)
-				r.add(tmp);
-			else if (tmp.end < newInterval.start) {
-				r.add(tmp);
-			} else if (newInterval.end < tmp.start) {
-				if (newInterval != null) {
-					r.add(newInterval);
-					newInterval = null;
+		ArrayList<Interval> ret = new ArrayList<Interval>();
+		boolean insertOnce = true;
+		for (Interval i : intervals) {
+			if (i.end < newInterval.start) {
+				ret.add(i);
+			} else if (newInterval.end < i.start) {
+				if (insertOnce) {
+					ret.add(newInterval);
+					insertOnce = false;
 				}
-				r.add(tmp);
+				ret.add(i);
 			} else {
-				newInterval.start = Math.min(newInterval.start, tmp.start);
-				newInterval.end = Math.max(newInterval.end, tmp.end);
+				newInterval.start = Math.min(i.start, newInterval.start);
+				newInterval.end = Math.max(i.end, newInterval.end);
 			}
 		}
-		if (newInterval != null)
-			r.add(newInterval);
-		return r;
+		if (insertOnce) {
+			ret.add(newInterval);
+		}
+		return ret;
 	}
 }
