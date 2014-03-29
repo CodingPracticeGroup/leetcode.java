264f398b26adec0e0f1d515585608cae76b6de6e
sealove <huijun.wu.2010@gmail.com>
2014-03-29 10:23:40 -0700

    Add Binary

--- Add Binary.java
+++ Add Binary.java
@@ -1,52 +1,41 @@
+import java.util.Arrays;
+
 public class Solution {
 	public String addBinary(String a, String b) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		String a_ = new StringBuilder(a).reverse().toString();
-		String b_ = new StringBuilder(b).reverse().toString();
-		StringBuilder sb = new StringBuilder();
-		int t = 0;
-		int len_a = a_.length();
-		int len_b = b_.length();
-		if (len_a < len_b) {
-			int i = 0;
-			while (i < len_a) {
-				int sum = a_.charAt(i) - '0' + b_.charAt(i) - '0' + t;
-				t = sum / 2;
-				sb.append(sum % 2);
-				i++;
-			}
-			while (i < len_b) {
-				int sum = b_.charAt(i) - '0' + t;
-				t = sum / 2;
-				sb.append(sum % 2);
-				i++;
-			}
-		} else if (len_a > len_b) {
-			int i = 0;
-			while (i < len_b) {
-				int sum = a_.charAt(i) - '0' + b_.charAt(i) - '0' + t;
-				t = sum / 2;
-				sb.append(sum % 2);
-				i++;
-			}
-			while (i < len_a) {
-				int sum = a_.charAt(i) - '0' + t;
-				t = sum / 2;
-				sb.append(sum % 2);
-				i++;
-			}
-		} else {
-			int i = 0;
-			while (i < len_b) {
-				int sum = a_.charAt(i) - '0' + b_.charAt(i) - '0' + t;
-				t = sum / 2;
-				sb.append(sum % 2);
-				i++;
-			}
+		char[] aa = new StringBuilder(a).reverse().toString().toCharArray();
+		char[] bb = new StringBuilder(b).reverse().toString().toCharArray();
+		int aaa[] = new int[aa.length];
+		int bbb[] = new int[bb.length];
+		for (int i = 0; i < aa.length; i++) {
+			aaa[i] = aa[i] - '0';
 		}
-		if (t == 1)
-			sb.append(t);
-		return sb.reverse().toString();
+		for (int i = 0; i < bb.length; i++) {
+			bbb[i] = bb[i] - '0';
+		}
+		int len = Math.max(aaa.length, bbb.length);
+		int ccc[] = new int[len + 1];
+		int tmp = 0;
+		for (int i = 0; i < len; i++) {
+			int sum = 0;
+			sum += tmp;
+			if (i < aaa.length) {
+				sum += aaa[i];
+			}
+			if (i < bbb.length) {
+				sum += bbb[i];
+			}
+			ccc[i] = sum % 2;
+			tmp = sum / 2;
+		}
+		if (tmp != 0) {
+			ccc[len] = tmp;
+		} else {
+			ccc = Arrays.copyOfRange(ccc, 0, len);
+		}
+		StringBuilder ret = new StringBuilder();
+		for (int i = 0; i < ccc.length; i++) {
+			ret.append(ccc[i]);
+		}
+		return ret.reverse().toString();
 	}
 }
