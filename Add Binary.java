b69b01405464d123a5539a96b3fac0bfadda0571
sealove <huijun.wu.2010@gmail.com>
2014-03-29 10:24:48 -0700

    Add Binary

--- Add Binary.java
+++ Add Binary.java
@@ -1,41 +1,26 @@
-import java.util.Arrays;
-
 public class Solution {
 	public String addBinary(String a, String b) {
-		char[] aa = new StringBuilder(a).reverse().toString().toCharArray();
-		char[] bb = new StringBuilder(b).reverse().toString().toCharArray();
-		int aaa[] = new int[aa.length];
-		int bbb[] = new int[bb.length];
-		for (int i = 0; i < aa.length; i++) {
-			aaa[i] = aa[i] - '0';
-		}
-		for (int i = 0; i < bb.length; i++) {
-			bbb[i] = bb[i] - '0';
-		}
-		int len = Math.max(aaa.length, bbb.length);
-		int ccc[] = new int[len + 1];
+		// Start typing your Java solution below
+		// DO NOT write main() function
+		String a_ = new StringBuilder(a).reverse().toString();
+		String b_ = new StringBuilder(b).reverse().toString();
+		String[] a_arr = a_.split("");
+		String[] b_arr = b_.split("");
+		int i = 1;
 		int tmp = 0;
-		for (int i = 0; i < len; i++) {
-			int sum = 0;
-			sum += tmp;
-			if (i < aaa.length) {
-				sum += aaa[i];
-			}
-			if (i < bbb.length) {
-				sum += bbb[i];
-			}
-			ccc[i] = sum % 2;
+		StringBuilder sb = new StringBuilder();
+		while (i < a_arr.length || i < b_arr.length) {
+			int sum = tmp;
+			if (i < a_arr.length)
+				sum += Integer.valueOf(a_arr[i]);
+			if (i < b_arr.length)
+				sum += Integer.valueOf(b_arr[i]);
 			tmp = sum / 2;
+			sb.append(sum % 2);
+			i++;
 		}
-		if (tmp != 0) {
-			ccc[len] = tmp;
-		} else {
-			ccc = Arrays.copyOfRange(ccc, 0, len);
-		}
-		StringBuilder ret = new StringBuilder();
-		for (int i = 0; i < ccc.length; i++) {
-			ret.append(ccc[i]);
-		}
-		return ret.reverse().toString();
+		if (tmp > 0)
+			sb.append(tmp);
+		return sb.reverse().toString();
 	}
 }
