c2e95754586adfade6275037b1fa1d70044ac999
sealove <huijun.wu.2010@gmail.com>
2014-03-30 12:44:30 -0700

    Jump Game
    comments

--- Jump Game.java
+++ Jump Game.java
@@ -1,8 +1,10 @@
 public class Solution {
+	// greedy: local optimization leads to global optimization
+	// hint in question: max jump at location i -> local optimization
 	public boolean canJump(int[] A) {
-		int maxLoc = 0;
-		for (int i = 0; i <= maxLoc && i < A.length; i++) {
-			maxLoc = Math.max(maxLoc, i + A[i]);
+		int maxLoc = 0; // global optimization
+		for (int i = 0; i <= maxLoc && i < A.length; i++) { // iterate every local position i
+			maxLoc = Math.max(maxLoc, i + A[i]); // local optimization -> global optimization
 		}
 		return maxLoc >= A.length - 1;
 	}
