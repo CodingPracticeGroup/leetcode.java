b7e7c5a5cfe7079634369971d818fbf5d9576ca5
sealove <huijun.wu.2010@gmail.com>
2014-03-31 08:51:01 -0700

    Search Insert Position

--- Search Insert Position.java
+++ Search Insert Position.java
@@ -1,28 +1,12 @@
+import java.util.Arrays;
+
 public class Solution {
 	public int searchInsert(int[] A, int target) {
-		// Start typing your Java solution below
-		// DO NOT write main() function
-		if (A == null)
-			return 0;
-		int len = A.length;
-		if (len == 0)
-			return 0;
-
-		int l = 0;
-		int r = len - 1;
-		int m = 0;
-
-		while (l <= r) {
-			m = (l + r) / 2;
-			if (A[m] > target) {
-				r = m - 1;
-			} else if (A[m] < target) {
-				l = m + 1;
-			} else {
-				return m;
-			}
+		int ret = Arrays.binarySearch(A, target);
+		if (ret >= 0) {
+			return ret;
+		} else {
+			return -ret - 1; // (-(insertion point) - 1) = ret
 		}
-
-		return r + 1;
 	}
 }
