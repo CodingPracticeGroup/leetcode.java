b96b78ecf0306b16e7e9d06701763d0ab13da9ce
sealove <huijun.wu.2010@gmail.com>
2014-03-31 15:16:11 -0700

    Search for a Range

--- Search for a Range.java
+++ Search for a Range.java
@@ -1,41 +1,53 @@
 public class Solution {
-    public int[] searchRange(int[] A, int target) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
-        int[] result = new int[2];
-		if(null==A) return result;
-		int len=A.length;
-		if(len<1) return result;
-		
-		int start=0;
-		int end=len-1;
-		int mid=0;
-		//find low bound
-		while(start<end){
-			mid=(start+end)/2;
-			if(A[mid]<target){
-				start=mid+1;
-			}else{
-				end=mid;
+	public int[] searchRange(int[] A, int target) {
+		int[] ret = new int[2];
+
+		int left = 0;
+		int right = A.length - 1;
+		while (left <= right) {
+			int mid = (left + right) / 2; // floor
+			if (target < A[mid]) {
+				right = mid - 1;
+			} else if (A[mid] < target) {
+				left = mid + 1;
+			} else {
+				if (A[left] < target) {
+					right = mid;
+				} else {
+					break;
+				}
 			}
 		}
-		result[0]=-1;
-		result[1]=-1;
-		if(A[start]==target) result[0]=start;
-		else return result;
-		
-		//find upper bound
-		end=len-1;
-		while(start<end){
-		    mid=(start+end)/2;
-			if(A[mid]>target){
-				end=mid;
-			}else{
-				start=mid+1;
+		if (left > right) {
+			ret[0] = -1;
+			ret[1] = -1;
+			return ret;
+		}
+		ret[0] = left; // this is where to insert. note the floor and ceil differences
+
+		left = 0;
+		right = A.length - 1;
+		while (left <= right) {
+			int mid = (left + right) / 2 + (left + right) % 2; // ceil
+			if (target < A[mid]) {
+				right = mid - 1;
+			} else if (A[mid] < target) {
+				left = mid + 1;
+			} else {
+				if (target < A[right]) {
+					left = mid;
+				} else {
+					break;
+				}
 			}
 		}
-		if(A[start]==target) result[1]=len-1;
-		else result[1]=start-1;
-		
-		return result;
-    }
+		ret[1] = right;
+
+		return ret;
+	}
+
+	public static void main(String[] args) {
+		int[] A = { 2, 2 };
+		new Solution().searchRange(A, 3);
+	}
 }
