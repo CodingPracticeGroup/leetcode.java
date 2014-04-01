c0447ee562506ab51d9ce5abb3cb348502de50ff
sealove <huijun.wu.2010@gmail.com>
2014-03-31 21:38:14 -0700

    Longest Valid Parentheses

--- Longest Valid Parentheses.java
+++ Longest Valid Parentheses.java
@@ -2,54 +2,69 @@ public class Solution {
 	public int longestValidParentheses(String s) {
 		// Start typing your Java solution below
 		// DO NOT write main() function
-		return Math.max(left2right(s), right2left(s));
+		int len = s.length();
+		if (len == 0)
+			return 0;
+		int one = onetime(s);
+		int two = twotime(s);
+		return Math.max(one, two);
 	}
 
-	int left2right(String s) {
+	int onetime(String s) {
 		int len = s.length();
 		int max = 0;
-		int current = 0;
-		int tmp_len = 0;
+		int cur = 0;
+		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < len; i++) {
-			if (s.charAt(i) == '(') {
-				current++;
-			} else if (s.charAt(i) == ')') {
-				current--;
-			}
-			tmp_len++;
-			if (current == 0) {
-				if (tmp_len > max) {
-					max = tmp_len;
+			if (s.charAt(i) == '(') { // always valid
+				sb.append('(');
+			} else {
+				if (sb.length() > 0 && sb.charAt(sb.length() - 1) == '(') { // valid
+					sb.setLength(sb.length() - 1);
+					cur += 2;
+					if (sb.length() == 0) {
+						if (cur > max)
+							max = cur;
+					}
+				} else { // invalid
+					if (cur > max)
+						max = cur;
+					sb.setLength(0);
+					cur = 0;
 				}
-			} else if (current < 0) {
-				current = 0;
-				tmp_len = 0;
 			}
 		}
+		if (sb.length() == 0 && cur > max)
+			max = cur;
 		return max;
 	}
 
-	int right2left(String s) {
+	int twotime(String s) {
 		int len = s.length();
 		int max = 0;
-		int current = 0;
-		int tmp_len = 0;
+		int cur = 0;
+		StringBuilder sb = new StringBuilder();
 		for (int i = len - 1; i >= 0; i--) {
-			if (s.charAt(i) == ')') {
-				current++;
-			} else if (s.charAt(i) == '(') {
-				current--;
-			}
-			tmp_len++;
-			if (current == 0) {
-				if (tmp_len > max) {
-					max = tmp_len;
+			if (s.charAt(i) == ')') { // always valid
+				sb.append(')');
+			} else {
+				if (sb.length() > 0 && sb.charAt(sb.length() - 1) == ')') { // valid
+					sb.setLength(sb.length() - 1);
+					cur += 2;
+					if (sb.length() == 0) {
+						if (cur > max)
+							max = cur;
+					}
+				} else { // invalid
+					if (cur > max)
+						max = cur;
+					sb.setLength(0);
+					cur = 0;
 				}
-			} else if (current < 0) {
-				current = 0;
-				tmp_len = 0;
 			}
 		}
+		if (sb.length() == 0 && cur > max)
+			max = cur;
 		return max;
 	}
 }
