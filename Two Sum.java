1ae937b055303e162b19986acf97a9677ff3cb2e
sealove <huijun.wu.2010@gmail.com>
2014-04-02 09:27:58 -0700

    Two Sum

--- Two Sum.java
+++ Two Sum.java
@@ -1,40 +1,19 @@
+import java.util.HashMap;
+
 public class Solution {
-    public int[] twoSum(int[] numbers, int target) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
-        if(numbers==null) return null;
-        int n=numbers.length;
-        if(n==0) return null;
-        int[] temp = (int[])numbers.clone();
-        Arrays.sort(numbers);
-        int[] result=new int[2];
-        int i=0;
-        int j=n-1;
-        while(i<j){
-            if(numbers[i]+numbers[j]>target){
-                j--;
-            }else if(numbers[i]+numbers[j]<target){
-                i++;
-            }else{
-                break;
-            }
-        }
-        if(numbers[i]+numbers[j] == target){
-            for(int k=0;k<n;k++){
-                if(temp[k]==numbers[i]) {
-                    result[0]=k+1;break;
-                }
-            }
-            int k=0;
-            if(numbers[i]==numbers[j]){
-            	k=result[0];
-            }
-            for(;k<n;k++){
-                if(temp[k]==numbers[j]) {
-                	result[1]=k+1;break;
-                }
-            }
-        }
-        Arrays.sort(result);
-        return result;
-    }
+	public int[] twoSum(int[] numbers, int target) {
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (int i = 0; i < numbers.length; i++) {
+			map.put(numbers[i], i);
+		}
+		int[] ret = new int[2];
+		for (int i = 0; i < numbers.length; i++) {
+			if (map.containsKey(target - numbers[i]) && map.get(target - numbers[i]) != i) {
+				ret[0] = i + 1;
+				ret[1] = map.get(target - numbers[i]) + 1;
+				return ret;
+			}
+		}
+		return ret;
+	}
 }
