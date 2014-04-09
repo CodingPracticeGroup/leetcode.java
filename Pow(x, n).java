f938f8d1fffccfd3ae4e691d597b7375635f2ee6
hjhjhj <huizhouhust@gmail.com>
2014-04-08 22:35:59 -0500

    Update Pow(x, n).java

--- Pow(x, n).java
+++ Pow(x, n).java
@@ -1,28 +1,33 @@
 public class Solution {
-	public double pow(double x, int n) {
-		if (n >= 0) {
-			return powPositive(x, n);
-		} else {
-			return 1 / powPositive(x, -n);
+    public double pow(double x, int n) {
+        if (n > 0) {
+			return powP(x, n);
+		} else if(n < 0) {
+		    if(n==Integer.MIN_VALUE){
+		        return 1 / (powP(x, -n-1) * x);
+		    }
+			return 1 / powP(x, -n);
+		}else{
+		    return 1.0;
 		}
-	}
-
-	private double powPositive(double x, int n) {
-		if (n == 0) {
-			return 1;
-		} else if (n == 1) {
-			return x;
-		}
-
-		int half = n / 2;
-		double halfRet = powPositive(x, half);
-		double ret = halfRet * halfRet;
-
-		int remainder = n % 2;
-		if (remainder == 1) {
-			ret *= x;
-		}
-
-		return ret;
-	}
+    }
+    
+    public double powP(double x, int n){
+        int bitmask = 0x01<<30;
+        //find the most significant bit
+        while((n&bitmask)==0){
+            bitmask=bitmask>>1;
+        }
+        bitmask=bitmask>>1;
+        double result=x;
+        while(bitmask!=0){
+            //n=2^2+2^1+...
+            result*=result;
+            if((bitmask&n)!=0){
+                result*=x;
+            }
+            bitmask=bitmask>>1;
+        }
+        return result;
+    }
 }
