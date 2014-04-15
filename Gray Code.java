46162e996c1d8a148c5837c22764241ec1df9bcb
hjhjhj <huizhouhust@gmail.com>
2014-04-14 22:00:43 -0500

    Recursive

--- Gray Code.java
+++ Gray Code.java
@@ -1,19 +1,20 @@
-import java.util.ArrayList;
-import java.util.Collections;
-
 public class Solution {
-	// http://en.wikipedia.org/wiki/Gray_code#Constructing_an_n-bit_Gray_code
-	public ArrayList<Integer> grayCode(int n) {
-		ArrayList<Integer> ret = new ArrayList<Integer>();
-		ret.add(0);
-		for (int i = 1; i <= n; i++) {
-			ArrayList<Integer> ret2 = new ArrayList<Integer>(ret);
-			Collections.reverse(ret2);
-			for (Integer i2 : ret2) {
-				i2 |= 1 << i - 1;
-				ret.add(i2);
-			}
-		}
-		return ret;
-	}
+    public ArrayList<Integer> grayCode(int n) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        code(n,result);
+        return result;
+    }
+    
+    public void code(int n, ArrayList<Integer> result){
+        if(0==n) {result.add(0);} 
+        else{
+            code(n-1,result);
+            int size=result.size();
+            for(int i=size-1;i>=0;i--){
+                int temp=result.get(i);
+                temp|=1<<(n-1);
+                result.add(temp);
+            }
+        }
+    }
 }
