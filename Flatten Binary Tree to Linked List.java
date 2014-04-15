7a7800539c05b6c4edfe14fbd4421b1c087e4605
hjhjhj <huizhouhust@gmail.com>
2014-04-15 14:15:58 -0500

    Update Flatten Binary Tree to Linked List.java

--- Flatten Binary Tree to Linked List.java
+++ Flatten Binary Tree to Linked List.java
@@ -1,42 +1,25 @@
 /**
  * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
  */
-class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
-
-	TreeNode(int x) {
-		val = x;
-	}
-}
-
 public class Solution {
-	public void flatten(TreeNode root) {
-		if (root != null) {
-			preOrderTraversal(root);
-		}
-	}
-
-	// return last node of pre-order in sub tree
-	private TreeNode preOrderTraversal(TreeNode root) {
-		if (root.left == null && root.right == null) {
-			return root;
-		} else if (root.left != null && root.right == null) {
-			root.right = root.left;
-			root.left = null;
-			return preOrderTraversal(root.right);
-		} else if (root.left == null && root.right != null) {
-			return preOrderTraversal(root.right);
-		} else { // if (root.left != null && root.right!=null) {
-			TreeNode left = root.left;
-			TreeNode right = root.right;
-
-			root.right = root.left;
-			root.left = null;
-
-			preOrderTraversal(left).right = right;
-			return preOrderTraversal(right);
-		}
-	}
+    public void flatten(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode temp = root;
+        while(temp!=null || !stack.empty()){
+            if(temp.right!=null){stack.push(temp.right);}
+            if(temp.left!=null){
+                temp.right=temp.left;
+                temp.left=null;
+            }else if(!stack.empty()){
+                temp.right=stack.pop();
+            }
+            temp=temp.right;
+        }
+    }
 }
