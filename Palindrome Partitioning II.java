fa0376cb1790ec0ccebe2b09e41e5c9aac7f11a5
hjhjhj <huizhouhust@gmail.com>
2014-11-17 01:03:48 -0600

    Update Palindrome Partitioning II.java

--- Palindrome Partitioning II.java
+++ Palindrome Partitioning II.java
@@ -1,58 +1,36 @@
 public class Solution {
-	public int minCut(String s) {
-		int len = s.length();
-		// System.out.println(System.currentTimeMillis());
-		// sub string [i, j) where 0<=i<len and 0<j<=len
-		boolean dpIsPalindrome[][] = new boolean[len + 1][len + 1];
-		for (int i = 0; i < len; i++) {
-			dpIsPalindrome[i][i + 1] = true;
-			for (int j = 0; 0 < i - j && i + 1 + j < len; j++) {
-				if (dpIsPalindrome[i - j][i + 1 + j] && s.charAt(i - j - 1) == s.charAt(i + 1 + j)) {
-					dpIsPalindrome[i - j - 1][i + 1 + j + 1] = true;
-				} else {
-					break;
-				}
-			}
-		}
-		for (int i = 0; i < len - 1; i++) {
-			if (s.charAt(i) == s.charAt(i + 1)) {
-				dpIsPalindrome[i][i + 2] = true;
-				for (int j = 0; 0 < i - j && i + 2 + j < len; j++) {
-					if (dpIsPalindrome[i - j][i + 2 + j] && s.charAt(i - j - 1) == s.charAt(i + 2 + j)) {
-						dpIsPalindrome[i - j - 1][i + 2 + j + 1] = true;
-					} else {
-						break;
-					}
-				}
-			}
-		}
-		// System.out.println(System.currentTimeMillis());
-		// sub string [i, len)
-		int dpMinCut[] = new int[len];
-		dpMinCut[len - 1] = 0;
-		for (int i = len - 2; i >= 0; i--) {
-			if (dpIsPalindrome[i][len]) {
-				dpMinCut[i] = 0;
-			} else {
-				int min = len;
-				for (int j = i + 1; j < len; j++) {
-					if (dpIsPalindrome[i][j] && min > dpMinCut[j]) {
-						min = dpMinCut[j];
-					}
-				}
-				dpMinCut[i] = 1 + min;
-			}
-		}
-		// System.out.println(System.currentTimeMillis());
-		//
-		return dpMinCut[0];
-	}
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		new Solution().minCut("a");
-	}
-
+    public int minCut(String s) {
+        if(null==s||s.length()==0) return 0;
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        
+        for(int i=0;i<n;i++){
+            dp[i][i]=true;
+        }
+        for(int i=0;i<n;i++){
+            //odd
+            int l=i-1;
+            int r=i+1;
+            while(l>=0&&r<s.length()&&s.charAt(l)==s.charAt(r)){
+                dp[l--][r++]=true;
+            }
+            //even
+             l=i-1;
+             r=i;
+            while(l>=0&&r<s.length()&&s.charAt(l)==s.charAt(r)){
+                dp[l--][r++]=true;
+            }
+        }
+        int [] cuts = new int[n+1];
+        cuts[n]=0;
+        for(int i=n-1;i>=0;i--){
+            cuts[i]=n-i;
+            for(int j=i;j<n;j++){
+                if(dp[i][j]){
+                    cuts[i]=Math.min(cuts[i],1+cuts[j+1]);
+                }
+            }
+        }
+        return cuts[0]-1;
+    }
 }
