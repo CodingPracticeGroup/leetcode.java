6f9da5c577ef609de627c01f6390a7cba9f9f3fb
sealove <huijun.wu.2010@gmail.com>
2015-10-07 01:44:12 -0700

    first update submission

--- Two Sum.java
+++ Two Sum.java
@@ -2,18 +2,29 @@ import java.util.HashMap;
 
 public class Solution {
 	public int[] twoSum(int[] numbers, int target) {
-		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-		for (int i = 0; i < numbers.length; i++) {
-			map.put(numbers[i], i);
-		}
-		int[] ret = new int[2];
-		for (int i = 0; i < numbers.length; i++) {
-			if (map.containsKey(target - numbers[i]) && map.get(target - numbers[i]) != i) {
-				ret[0] = i + 1;
-				ret[1] = map.get(target - numbers[i]) + 1;
-				return ret;
-			}
-		}
-		return ret;
+        Map<Integer, Integer> val_idx = new HashMap<>();
+    
+        int[] halves = new int[] {-1, -1};
+    
+        IntStream.range(0, nums.length).map(i -> {
+          if (nums[i] * 2 == target) {
+            if (halves[0] == -1) {
+              halves[0] = i+1;
+            } else if (halves[1] == -1) {
+              halves[1] = i+1;
+            }
+          } else {
+            val_idx.put(nums[i], i);
+          }
+          return i;
+        }).count();
+    
+        if (halves[1] > -1) {
+          return halves;
+        }
+    
+        return IntStream.range(0, nums.length)
+            .filter(i -> val_idx.containsKey(target - nums[i]))
+            .sorted().map(i -> i + 1).toArray();
 	}
 }
