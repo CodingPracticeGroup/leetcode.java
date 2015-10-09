1b6a1ba272803480a414630fbca02e7e0e1fe959
sealove <huijun.wu.2010@gmail.com>
2015-10-08 17:58:02 -0700

    update

--- Longest Substring Without Repeating Characters.java
+++ Longest Substring Without Repeating Characters.java
@@ -1,30 +1,22 @@
 import java.util.HashSet;
+import java.util.Set;
 
 public class Solution {
-	public int lengthOfLongestSubstring(String s) {
-		if (s.length() == 0) {
-			return 0;
-		}
-		int start = 0;
-		int end = 1;
-		HashSet<Character> set = new HashSet<Character>();
-		set.add(s.charAt(start));
-		int max = end - start;
-		while (end <= s.length()) {
-			while (end < s.length() && !set.contains(s.charAt(end))) {
-				set.add(s.charAt(end++));
-			}
-			if (end == s.length()) {
-				return Math.max(max, end - start);
-			} else {
-				max = Math.max(max, end - start);
-			}
-			while (s.charAt(start) != s.charAt(end)) {
-				set.remove(s.charAt(start++));
-			}
-			start++;
-			end++;
-		}
-		return max;
-	}
+  public int lengthOfLongestSubstring(String s) {
+    int ret = -1;
+    Set<Character> memory = new HashSet<Character>();
+    for (int i = 0, j = 0; j < s.length(); j++) {
+      char c = s.charAt(j);
+      if (memory.contains(c)) {
+        ret = Math.max(ret, memory.size());
+        while (s.charAt(i) != c) {
+          memory.remove(s.charAt(i++));
+        }
+        memory.remove(s.charAt(i++));
+      }
+      memory.add(c);
+    }
+    ret = Math.max(ret, memory.size());
+    return ret;
+  }
 }
