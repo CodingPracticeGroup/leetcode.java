7a5832c456eeba9efdf308260ead7f93bb80fd6e
sealove <huijun.wu.2010@gmail.com>
2015-10-08 17:03:25 -0700

    update stream collector to map

--- Two Sum.java
+++ Two Sum.java
@@ -1,31 +1,30 @@
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.function.Function;
 
 public class Solution {
-	public int[] twoSum(int[] numbers, int target) {
-        Map<Integer, Integer> val_idx = new HashMap<>();
-    
-        int[] halves = new int[] {-1, -1};
-    
-        IntStream.range(0, nums.length).map(i -> {
-          if (nums[i] * 2 == target) {
-            if (halves[0] == -1) {
-              halves[0] = i+1;
-            } else if (halves[1] == -1) {
-              halves[1] = i+1;
-            }
-          } else {
-            val_idx.put(nums[i], i);
-          }
-          return i;
-        }).count();
-    
-        if (halves[1] > -1) {
-			Arrays.sort(halves);
-          return halves;
-        }
-    
-        return IntStream.range(0, nums.length)
-            .filter(i -> val_idx.containsKey(target - nums[i]))
-            .sorted().map(i -> i + 1).toArray();
-	}
+  public int[] twoSum(int[] nums, int target) {
+    int ret[] = new int[] {-1, -1};
+
+    Map<Integer, Integer> val_idx =
+        IntStream.range(0, nums.length).boxed()
+            .collect(Collectors.toConcurrentMap(i -> nums[i], Function.identity(), (v1, v2) -> {
+              if (v1 >= 0 && v2 >= 0 && nums[v1] + nums[v2] == target) {
+                ret[0] = v1 + 1;
+                ret[1] = v2 + 1;
+              }
+              return -1;
+            }));
+
+    if (ret[1] >= 0) {
+      Arrays.sort(ret);
+      return ret;
+    }
+
+    return IntStream.range(0, nums.length)
+        .filter(i -> nums[i] * 2 != target && val_idx.containsKey(target - nums[i]))
+        .map(i -> i + 1).sorted().toArray();
+  }
 }
