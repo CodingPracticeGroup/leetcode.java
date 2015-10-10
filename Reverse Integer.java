899071f8ff43965eb18d1fe86cc68a78d3963aa3
sealove <huijun.wu.2010@gmail.com>
2015-10-10 01:26:46 -0700

    update

--- Reverse Integer.java
+++ Reverse Integer.java
@@ -1,9 +1,18 @@
 public class Solution {
-	public int reverse(int x) {
-		if (x < 0) {
-			return Integer.valueOf("-" + new StringBuilder().append(-x).reverse().toString());
-		} else {
-			return Integer.valueOf(new StringBuilder().append(x).reverse().toString());
-		}
-	}
+  private int reverse_positive(long x) {
+    long tmp = Long.valueOf(new StringBuilder(String.valueOf(x)).reverse().toString());
+    if (tmp > Integer.MAX_VALUE) {
+      return 0;
+    } else {
+      return (int) tmp;
+    }
+  }
+
+  public int reverse(int x) {
+    if (x < 0) {
+      return -reverse_positive(-(long) x);
+    } else {
+      return reverse_positive(x);
+    }
+  }
 }
