93546e91ddf69a71f496cf554792f239e9787db7
sealove <huijun.wu.2010@gmail.com>
2015-10-10 01:14:34 -0700

    update

--- ZigZag Conversion.java
+++ ZigZag Conversion.java
@@ -1,44 +1,52 @@
 public class Solution {
-	public String convert(String s, int nRows) {
-		if (nRows == 1) {
-			return s;
-		}
-		int nCols = s.length();
-		char[][] table = new char[nRows][nCols];
-		int x = 0, y = 0;
-		boolean down = true;
-		for (int i = 0; i < s.length(); i++) {
-			table[x][y] = s.charAt(i);
-			if (down) {
-				if (x == nRows - 1) {
-					x--;
-					y++;
-					down = false;
-				} else {
-					x++;
-				}
-			} else {
-				if (x == 0) {
-					x++;
-					down = true;
-				} else {
-					y++;
-					x--;
-				}
-			}
-		}
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < nRows; i++) {
-			for (int j = 0; j < nCols; j++) {
-				if (table[i][j] != 0) {
-					sb.append(table[i][j]);
-				}
-			}
-		}
-		return sb.toString();
-	}
+  public String convert(String s, int numRows) {
+    if (numRows == 1) {
+      return s;
+    }
 
-	public static void main(String[] args) {
-		new Solution().convert("AB", 1);
-	}
+    int i, j;
+    int len = s.length();
+    char[][] map = new char[numRows][len];
+    for (i = 0; i < numRows; i++) {
+      for (j = 0; j < len; j++) {
+        map[i][j] = 0;
+      }
+    }
+
+    i = 0;
+    j = 0;
+    boolean down = true;
+    for (int k = 0; k < len; k++) {
+      char c = s.charAt(k);
+      map[i][j] = c;
+
+      if (down) {
+        if (i == numRows - 1) {
+          down = false;
+          i--;
+          j++;
+        } else {
+          i++;
+        }
+      } else {
+        if (i == 0) {
+          down = true;
+          i++;
+        } else {
+          i--;
+          j++;
+        }
+      }
+    }
+
+    StringBuilder sb = new StringBuilder();
+    for (i = 0; i < numRows; i++) {
+      for (j = 0; j < len; j++) {
+        if (map[i][j] != 0) {
+          sb.append(map[i][j]);
+        }
+      }
+    }
+    return sb.toString();
+  }
 }
