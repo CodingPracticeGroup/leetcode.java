84616f3884398c17e0342036588c17e7520055aa
sealove <huijun.wu.2010@gmail.com>
2015-10-10 01:53:55 -0700

    update

--- String to Integer (atoi).java
+++ String to Integer (atoi).java
@@ -1,34 +1,53 @@
 public class Solution {
-    public int atoi(String str) {
-        if(null==str) return 0;
-        str=str.trim();
-        int len=str.length();
-        if(len==0) return 0;
-        
-        int index=0;
-        boolean positive=true;
-        if(str.charAt(index)=='-'){
-            positive=false; index++;
-        }
-        else if(str.charAt(index)=='+'){
-            positive=true; index++;
-        }
-        int result=0;
-        while(index<len){
-            if(!Character.isDigit(str.charAt(index))){break;}
-            int temp= str.charAt(index)-'0';
-            //check large number
-            if(positive&&result>(Integer.MAX_VALUE-temp)/10){
-                return Integer.MAX_VALUE;
-            }
-            if(!positive&&((-1)*result<(Integer.MIN_VALUE+temp)/10)){
-                return Integer.MIN_VALUE;
-            }
-            //basic case
-            result=result*10+temp;
-            index++;
-        }
-        if(positive) return result;
-        return (-1)*result;
+  public int myAtoi(String str) {
+    str = str.trim();
+    int len = str.length();
+    if (len == 0) {
+      return 0;
     }
+
+    boolean positive = true;
+    if (str.charAt(0) == '-') {
+      positive = false;
+      str = str.substring(1);
+      len = str.length();
+    } else if (str.charAt(0) == '+') {
+      str = str.substring(1);
+      len = str.length();
+    }
+
+    boolean digitExists = false;
+    int i = 0;
+    while (i < len) {
+      if (str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+        digitExists = true;
+        i++;
+      } else {
+        break;
+      }
+    }
+    if (!digitExists) {
+      return 0;
+    }
+
+    str = str.substring(0, i);
+    if (str.length() > String.valueOf(Integer.MIN_VALUE).length()) {
+      if (positive) {
+        return Integer.MAX_VALUE;
+      } else {
+        return Integer.MIN_VALUE;
+      }
+    }
+
+    long tmp = Long.valueOf(str);
+    if (!positive) {
+      tmp = -tmp;
+    }
+    if (tmp > Integer.MAX_VALUE) {
+      return Integer.MAX_VALUE;
+    } else if (tmp < Integer.MIN_VALUE) {
+      return Integer.MIN_VALUE;
+    }
+    return (int) tmp;
+  }
 }
