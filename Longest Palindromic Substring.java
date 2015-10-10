2cfb37375a99ade2cc5e4f50c3720acbc6053010
sealove <huijun.wu.2010@gmail.com>
2015-10-10 01:00:21 -0700

    update

--- Longest Palindromic Substring.java
+++ Longest Palindromic Substring.java
@@ -1,31 +1,23 @@
 public class Solution {
-	public String longestPalindrome(String s) {
-		int start = 0;
-		int end = 0;
-		int length = 1;
-
-		for (int i = 0; i < s.length(); i++) { // center [i,i] to grow j
-			for (int j = 1; 0 <= i - j && i + j < s.length() && s.charAt(i - j) == s.charAt(i + j); j++) {
-				if (length < 1 + j * 2) {
-					length = 1 + j * 2;
-					start = i - j;
-					end = i + j;
-				}
-			}
-		}
-
-		for (int i = 0; i + 1 < s.length(); i++) { // center [i,j] to grow k
-			int j = i + 1;
-			for (int k = 0; 0 <= i - k && j + k < s.length() && s.charAt(i - k) == s.charAt(j + k); k++) {
-				if (length < 2 + k * 2) {
-					length = 2 + k * 2;
-					start = i - k;
-					end = j + k;
-				}
-			}
-
-		}
-
-		return s.substring(start, end + 1);
-	}
+  public String longestPalindrome(String s) {
+    int len = s.length();
+    int maxi = 0, maxj = 0;
+    for (int i = 0; i < len; i++) {
+      for (int j = 1; i - j >= 0 && i + j < len && s.charAt(i - j) == s.charAt(i + j); j++) {
+        if ((maxj - maxi) < (i + j) - (i - j)) {
+          maxi = i - j;
+          maxj = i + j;
+        }
+      }
+    }
+    for (int i = 0; i < len - 1; i++) {
+      for (int j = 0; i - j >= 0 && i + 1 + j < len && s.charAt(i - j) == s.charAt(i + 1 + j); j++) {
+        if ((maxj - maxi) < (i + 1 + j) - (i - j)) {
+          maxi = i - j;
+          maxj = i + 1 + j;
+        }
+      }
+    }
+    return s.substring(maxi, maxj + 1);
+  }
 }
