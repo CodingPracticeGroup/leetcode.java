b35ec91fda2f1bbcfca82474e0bf075346ee8ece
sealove <huijun.wu.2010@gmail.com>
2015-10-11 01:07:14 -0700

    update

--- 3Sum.java
+++ 3Sum.java
@@ -1,34 +1,37 @@
 public class Solution {
-    public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
-        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-        if(null==num) return result;
-        int len=num.length;
-        if(len<3) return result;
-        
-        Arrays.sort(num);
-        
-        for(int i=0;i<len-2;i++){
-            //remove duplicates
-            if(i>0&&num[i]==num[i-1]) continue;
-            //two sum
-            int head=i+1;
-            int tail=len-1;
-            while(head<tail){
-                if(num[head]+num[tail]+num[i]==0){
-                    ArrayList<Integer> temp = new ArrayList<Integer>();
-                    temp.add(num[head]);temp.add(num[tail]);temp.add(num[i]);
-                    Collections.sort(temp);
-                    result.add(temp);
-                    do{head++;}while(head<tail&&num[head]==num[head-1]); 
-                    do{tail--;}while(head<tail&&num[tail]==num[tail+1]); 
-                }else if(num[head]+num[tail]+num[i]<0){
-                    head++;
-                }else{
-                    tail--;
-                }
-            }
-        }
-        
-        return result;
+  public List<List<Integer>> threeSum(int[] nums) {
+    Arrays.sort(nums);
+    Set<Integer> exist = // Arrays.stream(nums).boxed().collect(Collectors.toSet());
+        new HashSet<Integer>();
+    for (int i = 0; i < nums.length; i++) {
+      exist.add(nums[i]);
     }
+    Set<List<Integer>> ret = new HashSet<>();
+    int i = 0;
+    while (i < nums.length) {
+      int j = i + 1;
+      while (j < nums.length) {
+        int target = 0 - nums[i] - nums[j];
+        if ((nums[j] == target && j + 1 < nums.length && nums[j + 1] == target)
+            || (nums[j] < target && exist.contains(target))) {
+          List<Integer> l = new ArrayList<>();
+          l.add(nums[i]);
+          l.add(nums[j]);
+          l.add(target);
+          ret.add(l);
+        }
+
+        j++;
+        while (j < nums.length && nums[j - 1] == nums[j]) {
+          j++;
+        }
+      }
+
+      i++;
+      while (i < nums.length && nums[i - 1] == nums[i]) {
+        i++;
+      }
+    }
+    return new ArrayList<List<Integer>>(ret);
+  }
 }
