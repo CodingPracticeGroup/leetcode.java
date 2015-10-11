e10e430b4d2141ffd828f40858441ce61d62c4e6
sealove <huijun.wu.2010@gmail.com>
2015-10-10 19:24:47 -0700

    update

--- Container With Most Water.java
+++ Container With Most Water.java
@@ -1,16 +1,18 @@
 public class Solution {
-	public int maxArea(int[] height) {
-		int left = 0;
-		int right = height.length - 1;
-		int max = 0;
-		while (left < right) {
-			max = Math.max(max, Math.min(height[left], height[right]) * (right - left));
-			if (height[left] < height[right]) {
-				left++; // we want to improve left, so we change left
-			} else {
-				right--;
-			}
-		}
-		return max;
-	}
+  public int maxArea(int[] height) {
+    int i = 0, j = height.length - 1;
+    int max = 0;
+    while (i < j) {
+      max = Math.max(max, (j - i) * Math.min(height[i], height[j]));
+
+      // 如果不移短的只移长的，短的决定高度，不会变大，所以移短的
+      // 为什么往内移动？反证法
+      if (height[i] < height[j]) {
+        i++;
+      } else {
+        j--;
+      }
+    }
+    return max;
+  }
 }
