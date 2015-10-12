2e1a47134d10b7c04480a4811d1a686ddfd54053
sealove <huijun.wu.2010@gmail.com>
2015-10-11 21:18:01 -0700

    update

--- Divide Two Integers.java
+++ Divide Two Integers.java
@@ -1,5 +1,46 @@
 public class Solution {
-	public int divide(int dividend, int divisor) {
-		return dividend / divisor;
-	}
+  public int divide(int dividend, int divisor) {
+    if (divisor == 0)
+      return Integer.MAX_VALUE;
+
+    boolean negative = false;
+    long dividend_long = dividend, divisor_long = divisor;
+    if (dividend < 0) {
+      negative = !negative;
+      dividend_long = -dividend_long;
+    }
+    if (divisor < 0) {
+      negative = !negative;
+      divisor_long = -divisor_long;
+    }
+    if (dividend_long < divisor_long) {
+      return 0;
+    }
+
+    int shift = 0;
+    while (dividend_long >= divisor_long) {
+      divisor_long = divisor_long << 1;
+      shift++;
+    }
+
+    long ret = 0;
+    while (shift >= 0) {
+      if (dividend_long >= divisor_long) {
+        dividend_long -= divisor_long;
+        ret += 1L << shift;
+      }
+      shift--;
+      divisor_long = divisor_long >> 1;
+    }
+
+    if (negative) {
+      if (-ret < Integer.MIN_VALUE)
+        return Integer.MIN_VALUE;
+      return (int) -ret;
+    } else {
+      if (ret > Integer.MAX_VALUE)
+        return Integer.MAX_VALUE;
+      return (int) ret;
+    }
+  }
 }
--- Implement strStr().java
+++ Implement strStr().java
@@ -1,10 +1,5 @@
 public class Solution {
-	public String strStr(String haystack, String needle) {
-		int idx = haystack.indexOf(needle);
-		if (idx < 0) {
-			return null;
-		} else {
-			return haystack.substring(idx, haystack.length());
-		}
-	}
+  public int strStr(String haystack, String needle) {
+    return haystack.indexOf(needle);
+  }
 }
