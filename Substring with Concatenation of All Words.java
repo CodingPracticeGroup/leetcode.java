ab7ba26c9e86c0d6ad38c389afa685d197f66d75
sealove <huijun.wu.2010@gmail.com>
2015-10-12 01:14:02 -0700

    update

--- Substring with Concatenation of All Words.java
+++ Substring with Concatenation of All Words.java
@@ -1,54 +1,55 @@
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 
 public class Solution {
-	public ArrayList<Integer> findSubstring(String S, String[] L) {
-		int limit = S.length() - L.length * L[0].length();
-		ArrayList<Integer> ret = new ArrayList<Integer>();
-		for (int i = 0; i <= limit; i++) {
-			if (window(S, i, L[0].length(), L) == true) {
-				ret.add(i);
-			}
-		}
-		return ret;
-	}
+  public List<Integer> findSubstring(String s, String[] words) {
+    List<Integer> ret = new ArrayList<>();
 
-	private boolean window(String S, int idx, int len, String[] L) {
-		HashMap<String, Integer> map = resetMap(L);
-		for (int i = 0; i < L.length; i++) {
-			String str = S.substring(idx + i * len, idx + i * len + len);
-			if (map.containsKey(str)) {
-				int count = map.get(str);
-				if (count > 0) {
-					map.put(str, count - 1);
-				} else {
-					return false;
-				}
-			} else {
-				return false;
-			}
-		}
-		return true;
-	}
+    int word_len = words[0].length();
+    int window_len = word_len * words.length;
 
-	private HashMap<String, Integer> resetMap(String[] L) {
-		HashMap<String, Integer> map = new HashMap<String, Integer>();
-		for (String s : L) {
-			if (map.containsKey(s)) {
-				map.put(s, map.get(s) + 1);
-			} else {
-				map.put(s, 1);
-			}
-		}
-		return map;
-	}
+    Map<String, Long> words_count =
+        Arrays.stream(words).collect(
+            Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		String[] L = new String[] { "a" };
-		new Solution().findSubstring("a", L);
-	}
+    for (int j = 0; j < word_len && j + window_len <= s.length(); j++) {
+      List<String> window_words = new ArrayList<>();
+      for (int i = j; i < j + window_len; i += word_len) {
+        window_words.add(s.substring(i, i + word_len));
+      }
+      Map<String, Long> window_words_count =
+          window_words.stream().collect(
+              Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
+      if (window_words_count.equals(words_count)) {
+        ret.add(j);
+      }
+      for (int i = j + word_len; i <= s.length() - window_len; i += word_len) {
+        String add = s.substring(i + window_len - word_len, i + window_len);
+        if (window_words_count.containsKey(add)) {
+          window_words_count.put(add, window_words_count.get(add) + 1);
+        } else {
+          window_words_count.put(add, 1L);
+        }
+
+        String remove = s.substring(i - word_len, i);
+        if (window_words_count.get(remove) == 1) {
+          window_words_count.remove(remove);
+        } else {
+          window_words_count.put(remove, window_words_count.get(remove) - 1);
+        }
+
+        if (window_words_count.equals(words_count)) {
+          ret.add(i);
+        }
+      }
+    }
+
+    return ret;
+  }
 }
