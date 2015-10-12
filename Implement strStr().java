38e4ae6050ba34c23a860b0bf4024042da67d60a
sealove <huijun.wu.2010@gmail.com>
2015-10-11 21:21:33 -0700

    update

--- Implement strStr().java
+++ Implement strStr().java
@@ -1,5 +1,10 @@
 public class Solution {
   public int strStr(String haystack, String needle) {
-    return haystack.indexOf(needle);
+    for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+      if (haystack.startsWith(needle, i)) {
+        return i;
+      }
+    }
+    return -1;
   }
 }
