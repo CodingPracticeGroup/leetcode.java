1bbc0cd4dcf22e5a370a57e6a7e71a6f787530c2
sealove <huijun.wu.2010@gmail.com>
2015-10-13 02:38:58 -0700

    update

--- Sudoku Solver.java
+++ Sudoku Solver.java
@@ -1,144 +1,69 @@
 public class Solution {
-	public void solveSudoku(char[][] board) {
-		recursion(board);
-	}
+  private boolean solveSudoku_check(char[][] board, int i, int j) {
+    Set<Character> s =
+        Stream.of('1', '2', '3', '4', '5', '6', '7', '8', '9').collect(Collectors.toSet());
+    Set<Character> row = new HashSet<>(s);
+    for (int k = 0; k < 9; k++) {
+      if (board[i][k] != '.') {
+        if (row.contains(board[i][k])) {
+          row.remove(board[i][k]);
+        } else {
+          return false;
+        }
+      }
+    }
+    Set<Character> col = new HashSet<>(s);
+    for (int k = 0; k < 9; k++) {
+      if (board[k][j] != '.') {
+        if (col.contains(board[k][j])) {
+          col.remove(board[k][j]);
+        } else {
+          return false;
+        }
+      }
+    }
+    Set<Character> block = new HashSet<>(s);
+    int i_ = (i / 3) * 3 + 1;
+    int j_ = (j / 3) * 3 + 1;
+    for (int k = i_ - 1; k <= i_ + 1; k++) {
+      for (int m = j_ - 1; m <= j_ + 1; m++) {
+        if (board[k][m] != '.') {
+          if (block.contains(board[k][m])) {
+            block.remove(board[k][m]);
+          } else {
+            return false;
+          }
+        }
+      }
+    }
+    return true;
+  }
 
-	private boolean recursion(char[][] board) {
-		if (done(board)) { // found
-			return true; // report
-		} else {
-			int i = 0;
-			int j = 0;
-			boolean notFound = true;
-			for (i = 0; notFound && i < 9; i++) {
-				for (j = 0; notFound && j < 9; j++) {
-					if (board[i][j] == '.') {
-						notFound = false;
-						break;
-					}
-				}
-				if (!notFound) {
-					break;
-				}
-			}
-			// candidates
-			for (char k = '1'; k <= '9'; k++) {
-				char track = board[i][j];
-				// forward
-				board[i][j] = k;
-				// prune
-				if (valid(board, i, j)) {
-					// explore
-					boolean found = recursion(board);
-					if (found) {
-						return true;
-					}
-				}
-				// backward
-				board[i][j] = track;
-			}
-			return false;
-		}
-	}
+  private boolean solveSudoku_(char[][] board, int i, int j) {
+    outerloop: for (; i < 9; i++) {
+      for (j = 0; j < 9; j++) {
+        if (board[i][j] == '.') {
+          break outerloop;
+        }
+      }
+    }
+    if (i == 9 && j == 9) {// found
+      return true;// report
+    }
 
-	private boolean done(char[][] board) {
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
-				if (board[i][j] == '.') {
-					return false;
-				}
-			}
-		}
-		return true;
-	}
+    for (char k = '1'; k <= '9'; k++) {// candidates
+      board[i][j] = k;// forward
+      if (solveSudoku_check(board, i, j)) {// prune
+        if (solveSudoku_(board, i, j)) {// explore
+          return true;
+        }
+      }
+    }
+    board[i][j] = '.';// backward
+    return false;
+  }
 
-	private boolean valid(char[][] board, int i, int j) {
-		int check[] = new int[9];
-		for (int x = 0; x < 9; x++) {
-			if (board[i][x] != '.' && check[board[i][x] - '1'] == 1) {
-				return false;
-			} else {
-				if (board[i][x] != '.') {
-					check[board[i][x] - '1'] = 1;
-				}
-			}
-		}
-		check = new int[9];
-		for (int x = 0; x < 9; x++) {
-			if (board[x][j] != '.' && check[board[x][j] - '1'] == 1) {
-				return false;
-			} else {
-				if (board[x][j] != '.') {
-					check[board[x][j] - '1'] = 1;
-				}
-			}
-		}
-		int i3 = (i / 3) * 3 + 1;
-		int j3 = (j / 3) * 3 + 1;
-		check = new int[9];
-		if (board[i3 - 1][j3 - 1] != '.' && check[board[i3 - 1][j3 - 1] - '1'] == 1) {
-			return false;
-		} else {
-			if (board[i3 - 1][j3 - 1] != '.') {
-				check[board[i3 - 1][j3 - 1] - '1'] = 1;
-			}
-		}
-		if (board[i3 - 1][j3] != '.' && check[board[i3 - 1][j3] - '1'] == 1) {
-			return false;
-		} else {
-			if (board[i3 - 1][j3] != '.') {
-				check[board[i3 - 1][j3] - '1'] = 1;
-			}
-		}
-		if (board[i3 - 1][j3 + 1] != '.' && check[board[i3 - 1][j3 + 1] - '1'] == 1) {
-			return false;
-		} else {
-			if (board[i3 - 1][j3 + 1] != '.') {
-				check[board[i3 - 1][j3 + 1] - '1'] = 1;
-			}
-		}
-		if (board[i3][j3 - 1] != '.' && check[board[i3][j3 - 1] - '1'] == 1) {
-			return false;
-		} else {
-			if (board[i3][j3 - 1] != '.') {
-				check[board[i3][j3 - 1] - '1'] = 1;
-			}
-		}
-		if (board[i3][j3] != '.' && check[board[i3][j3] - '1'] == 1) {
-			return false;
-		} else {
-			if (board[i3][j3] != '.') {
-				check[board[i3][j3] - '1'] = 1;
-			}
-		}
-		if (board[i3][j3 + 1] != '.' && check[board[i3][j3 + 1] - '1'] == 1) {
-			return false;
-		} else {
-			if (board[i3][j3 + 1] != '.') {
-				check[board[i3][j3 + 1] - '1'] = 1;
-			}
-		}
-		if (board[i3 + 1][j3 - 1] != '.' && check[board[i3 + 1][j3 - 1] - '1'] == 1) {
-			return false;
-		} else {
-			if (board[i3 + 1][j3 - 1] != '.') {
-				check[board[i3 + 1][j3 - 1] - '1'] = 1;
-			}
-		}
-		if (board[i3 + 1][j3] != '.' && check[board[i3 + 1][j3] - '1'] == 1) {
-			return false;
-		} else {
-			if (board[i3 + 1][j3] != '.') {
-				check[board[i3 + 1][j3] - '1'] = 1;
-			}
-		}
-		if (board[i3 + 1][j3 + 1] != '.' && check[board[i3 + 1][j3 + 1] - '1'] == 1) {
-			return false;
-		} else {
-			if (board[i3 + 1][j3 + 1] != '.') {
-				check[board[i3 + 1][j3 + 1] - '1'] = 1;
-			}
-		}
-		return true;
-	}
+  public void solveSudoku(char[][] board) {
+    solveSudoku_(board, 0, 0);
+  }
 }
