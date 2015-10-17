580e4409b551faf266cb67d63564f25864ac10d1
sealove <huijun.wu.2010@gmail.com>
2015-10-17 03:26:07 -0700

    update

--- Word Search.java
+++ Word Search.java
@@ -1,44 +1,45 @@
 public class Solution {
-	public boolean exist(char[][] board, String word) {
-		for (int i = 0; i < board.length; i++) {
-			for (int j = 0; j < board[0].length; j++) {
-				if (dfs(board, word, 0, i, j)) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+  private boolean exist_dfs(char[][] board, String word, int i, int j, int m, int n, int start) {
+    if (start == word.length() - 1 && board[i][j] == word.charAt(start)) {
+      return true;
+    } else {
+      if (board[i][j] == word.charAt(start)) {
+        char tmp = board[i][j];
+        board[i][j] = 0;
+        if (i - 1 >= 0 && exist_dfs(board, word, i - 1, j, m, n, start + 1)) {
+          board[i][j] = tmp;
+          return true;
+        }
+        if (i + 1 < m && exist_dfs(board, word, i + 1, j, m, n, start + 1)) {
+          board[i][j] = tmp;
+          return true;
+        }
+        if (j + 1 < n && exist_dfs(board, word, i, j + 1, m, n, start + 1)) {
+          board[i][j] = tmp;
+          return true;
+        }
+        if (j - 1 >= 0 && exist_dfs(board, word, i, j - 1, m, n, start + 1)) {
+          board[i][j] = tmp;
+          return true;
+        }
+        board[i][j] = tmp;
+      }
+      return false;
+    }
+  }
 
-	private boolean dfs(char[][] board, String word, int start, int x, int y) {
-		if (start + 1 == word.length()) {
-			if (board[x][y] == word.charAt(start)) {
-				return true;
-			} else {
-				return false;
-			}
-		}
-
-		if (board[x][y] != word.charAt(start)) {
-			return false;
-		}
-
-		char cache = board[x][y];
-		board[x][y] = '+'; // this is cheating
-		if (0 <= x - 1 && dfs(board, word, start + 1, x - 1, y)) {
-			board[x][y] = cache;
-			return true;
-		} else if (x + 1 < board.length && dfs(board, word, start + 1, x + 1, y)) {
-			board[x][y] = cache;
-			return true;
-		} else if (0 <= y - 1 && dfs(board, word, start + 1, x, y - 1)) {
-			board[x][y] = cache;
-			return true;
-		} else if (y + 1 < board[0].length && dfs(board, word, start + 1, x, y + 1)) {
-			board[x][y] = cache;
-			return true;
-		}
-		board[x][y] = cache;
-		return false;
-	}
+  public boolean exist(char[][] board, String word) {
+    int m = board.length;
+    int n = board[0].length;
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (board[i][j] == word.charAt(0)) {
+          if (exist_dfs(board, word, i, j, m, n, 0)) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  }
 }
