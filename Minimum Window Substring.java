97bf5167da53cb5347b2daf839e76d62826535a5
sealove <huijun.wu.2010@gmail.com>
2015-10-16 21:55:47 -0700

    update

--- Minimum Window Substring.java
+++ Minimum Window Substring.java
@@ -1,57 +1,90 @@
 public class Solution {
-	public static int size = 260;
+  private void minWindow_map_put(Map<Character, Long> map, char c) {
+    if (map.containsKey(c)) {
+      map.put(c, map.get(c) + 1);
+    } else {
+      map.put(c, 1L);
+    }
+  }
 
-	public String minWindow(String S, String T) {
-		// use array to replace hash map. char is integer
-		int validLen = 0;
-		int[] charCountT = new int[size];
-		for (int i = 0; i < T.length(); i++) {
-			if (charCountT[T.charAt(i)] == 0) {
-				validLen++;
-			}
-			charCountT[T.charAt(i)]++;
-		}
+  private void minWindow_map_remove(Map<Character, Long> map, char c) {
+    map.put(c, map.get(c) - 1);
+    if (map.get(c) == 0) {
+      map.remove(c);
+    }
+  }
 
-		// use (validCount == validLen) to check.
-		// if check every time instead of tracking validCount, cannot pass large test
-		int bestLeft = 0, bestRight = S.length() - 1; // [bestLeft, bestRight]
-		int[] charCountS = new int[size];
-		int left = 0, right = 0; // [left, right]
-		int validCount = 0;
-		boolean exist = false;
-		for (right = 0; right < S.length(); right++) {
-			charCountS[S.charAt(right)]++;
-			if (charCountS[S.charAt(right)] == charCountT[S.charAt(right)]) {
-				validCount++;
-			}
+  private int minWindow_j(String s, Map<Character, Long> T, Map<Character, Long> window) {
+    int j = 0;
+    Map<Character, Long> T_ = new HashMap<>(T);
+    for (j = 0; j < s.length(); j++) {
+      char c = s.charAt(j);
+      if (T.containsKey(c)) {
+        minWindow_map_put(window, c);
+      }
+      if (T_.containsKey(c)) {
+        minWindow_map_remove(T_, c);
+        if (T_.isEmpty()) {
+          break;
+        }
+      }
+    }
+    if (T_.isEmpty()) {
+      return j;
+    } else {
+      return -1;
+    }
+  }
 
-			if (validCount == validLen) {
-				exist = true;
-				while (validCount == validLen) {
-					if (charCountS[S.charAt(left)] == charCountT[S.charAt(left)]) {
-						validCount--;
-					}
-					charCountS[S.charAt(left)]--;
-					left++;
-				}
-				left--;
-				charCountS[S.charAt(left)]++;
-				if (charCountS[S.charAt(left)] == charCountT[S.charAt(left)]) {
-					validCount++;
-				}
+  private int minWindow_i(String s, Map<Character, Long> T, int i, Map<Character, Long> window) {
+    while (i < s.length()) {
+      char c = s.charAt(i);
+      if (T.containsKey(c)) {
+        if (window.get(c) > T.get(c)) {
+          minWindow_map_remove(window, c);
+          i++;
+        } else {
+          return i;
+        }
+      } else {
+        i++;
+      }
+    }
+    return i;
+  }
 
-				if (right - left < bestRight - bestLeft) {
-					bestLeft = left;
-					bestRight = right;
-				}
-			}
-		}
-
-		if (!exist) {
-			return "";
-		}
-
-		return S.substring(bestLeft, bestRight + 1);
-	}
-
+  public String minWindow(String s, String t) {
+    Map<Character, Long> T =
+        t.chars().mapToObj(x -> new Character((char) x))
+            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    int i = 0, j = 0;
+    int minLen = s.length();
+    String ret = "";
+    Map<Character, Long> window = new HashMap<>();
+    // [i, j]
+    j = minWindow_j(s, T, window);
+    if (j < 0) {
+      return ret;
+    }
+    i = minWindow_i(s, T, 0, window);
+    minLen = j - i + 1;
+    ret = s.substring(i, j + 1);
+    //
+    while (j < s.length()) {
+      for (j++; j < s.length() && s.charAt(j) != s.charAt(i); j++) {
+        if (T.containsKey(s.charAt(j))) {
+          minWindow_map_put(window, s.charAt(j));
+        }
+      }
+      if (j < s.length() && s.charAt(j) == s.charAt(i)) {
+        i++;
+        i = minWindow_i(s, T, i, window);
+        if (j - i + 1 < minLen) {
+          minLen = j - i + 1;
+          ret = s.substring(i, j + 1);
+        }
+      }
+    }
+    return ret;
+  }
 }
