62346e73bc0be29348d3ff78be49d9a07480d139
sealove <huijun.wu.2010@gmail.com>
2015-10-18 00:49:28 -0700

    update

--- Binary Tree Inorder Traversal.java
+++ Binary Tree Inorder Traversal.java
@@ -1,41 +1,15 @@
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-
-/**
- * Definition for binary tree
- */
-class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
-
-	TreeNode(int x) {
-		val = x;
-	}
-}
-
 public class Solution {
+  private void inorderTraversal_preorder(TreeNode root, List<Integer> ret) {
+    if (root != null) {
+      inorderTraversal_preorder(root.left, ret);
+      ret.add(root.val);
+      inorderTraversal_preorder(root.right, ret);
+    }
+  }
 
-	// pre-order and in-order are the same. post-order is different
-	public ArrayList<Integer> inorderTraversal(TreeNode root) {
-		ArrayList<Integer> ret = new ArrayList<Integer>();
-
-		// stack is not track. stack is used to mark first/second visit
-		// ArrayDeque cannot hold null, but LinkedList can
-		ArrayDeque<TreeNode> stack = new ArrayDeque<TreeNode>();
-		TreeNode pointer = root;
-		while (!stack.isEmpty() || pointer != null) {
-			if (pointer != null) {
-				stack.push(pointer);// first time visit node
-				pointer = pointer.left; // go left
-			} else if (!stack.isEmpty()) {
-				TreeNode tn = stack.pop(); // second time visit node
-				ret.add(tn.val); // in-order
-				pointer = tn.right; // go right
-			}
-		}
-
-		return ret;
-	}
-
+  public List<Integer> inorderTraversal(TreeNode root) {
+    List<Integer> ret = new ArrayList<>();
+    inorderTraversal_preorder(root, ret);
+    return ret;
+  }
 }
