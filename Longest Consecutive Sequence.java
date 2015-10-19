d9fa9e94349945c97d57e06cd7b4725e9a72edb3
sealove <huijun.wu.2010@gmail.com>
2015-10-19 05:11:04 -0700

    update

--- Longest Consecutive Sequence.java
+++ Longest Consecutive Sequence.java
@@ -1,40 +1,23 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class Solution {
   public int longestConsecutive(int[] nums) {
-    Map<Integer, Integer> start_end = new HashMap<>();
-    Map<Integer, Integer> end_start = new HashMap<>();
-    for (Integer i : nums) {
-      if (start_end.containsKey(i + 1)) {
-        int old_end = start_end.remove(i + 1);
-        if (start_end.containsKey(i)) {
-          old_end = Math.max(old_end, start_end.get(i));
-        }
-        start_end.put(i, old_end);
-      } else if (!start_end.containsKey(i)) {
-        start_end.put(i, i);
-      }
-      if (end_start.containsKey(i - 1)) {
-        int old_start = end_start.remove(i - 1);
-        if (end_start.containsKey(i)) {
-          old_start = Math.min(old_start, end_start.get(i));
-        }
-        end_start.put(i, old_start);
-      } else if (!end_start.containsKey(i)) {
-        end_start.put(i, i);
-      }
-      if (start_end.containsKey(i) && end_start.containsKey(i)) {
-        int old_end = start_end.remove(i);
-        int old_start = end_start.remove(i);
-        start_end.put(old_start, old_end);
-        end_start.put(old_end, old_start);
-      }
-    }
+    Set<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
     int max = 0;
-    for (Integer start : start_end.keySet()) {
-      Integer end = start_end.get(start);
-      max = Math.max(max, end - start + 1);
+    while (!set.isEmpty()) {
+      int cur = set.iterator().next();
+      set.remove(cur);
+
+      int left = cur - 1;
+      while (set.contains(left)) {
+        set.remove(left);
+        left--;
+      }
+      int right = cur + 1;
+      while (set.contains(right)) {
+        set.remove(right);
+        right++;
+      }
+
+      max = Math.max(max, right - left - 1);
     }
     return max;
   }
