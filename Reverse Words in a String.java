541c15069c36fd99d38e6e5e0c8ead9eb6959f6a
sealove <huijun.wu.2010@gmail.com>
2015-10-21 01:01:52 -0700

    update

--- Reverse Words in a String.java
+++ Reverse Words in a String.java
@@ -1,17 +1,7 @@
 public class Solution {
   public String reverseWords(String s) {
-    String[] strarr = s.trim().split(" ");
+    String[] strarr = s.trim().replaceAll("\\s+", " ").split(" ");
     return IntStream.range(0, strarr.length).mapToObj(x -> strarr[strarr.length - 1 - x])
-        .reduce((x, y) -> {
-          if (x.equals("") && y.equals("")) {
-            return "";
-          } else if (x.equals("")) {
-            return y;
-          } else if (y.equals("")) {
-            return x;
-          } else {
-            return x + " " + y;
-          }
-        }).get();
+        .reduce((x, y) -> x + " " + y).get();
   }
 }
