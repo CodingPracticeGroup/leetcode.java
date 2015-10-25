75e5098e57eedff71099eb6dfc51a6c2f946401e
sealove <huijun.wu.2010@gmail.com>
2015-10-25 06:00:18 -0700

    update

--- Basic Calculator II.java
+++ Basic Calculator II.java
@@ -1,22 +1,24 @@
 public class Solution {
   public int calculate(String s) {
-    String ss = s.replaceAll("\\s*", "");
-    String sss2[] =
-        Arrays.stream(ss.split("\\d+")).filter(x -> !x.equals(""))
-            .toArray(size -> new String[size]);
-    String sss1[] = ss.split("[\\+\\-\\*\\/]");
+    s = s.replaceAll("\\s*", "");
+    s = s.replaceAll("\\+", " \\+ ");
+    s = s.replaceAll("\\-", " \\- ");
+    s = s.replaceAll("\\*", " \\* ");
+    s = s.replaceAll("\\/", " \\/ ");
+    s = s.trim();
+    String ss[] = s.split(" ");
     Deque<String> stack = new ArrayDeque<>();
-    stack.offerLast(sss1[0]);
-    for (int i = 0; i < sss2.length; i++) {
-      if (sss2[i].equals("*")) {
+    for (int i = 0; i < ss.length; i++) {
+      if (ss[i].equals("*")) {
         stack.offerLast(String.valueOf(Integer.parseInt(stack.pollLast())
-            * Integer.parseInt(sss1[i + 1])));
-      } else if (sss2[i].equals("/")) {
+            * Integer.parseInt(ss[i + 1])));
+        i++;
+      } else if (ss[i].equals("/")) {
         stack.offerLast(String.valueOf(Integer.parseInt(stack.pollLast())
-            / Integer.parseInt(sss1[i + 1])));
+            / Integer.parseInt(ss[i + 1])));
+        i++;
       } else {
-        stack.offerLast(sss2[i]);
-        stack.offerLast(sss1[i + 1]);
+        stack.offerLast(ss[i]);
       }
     }
     int ret = Integer.parseInt(stack.pollFirst());
