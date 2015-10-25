c27c13b77e53357abcc97a4f97a72f6d5dc86aba
sealove <huijun.wu.2010@gmail.com>
2015-10-25 03:31:05 -0700

    update

--- Sliding Window Maximum.java
+++ Sliding Window Maximum.java
@@ -3,15 +3,15 @@ public class Solution {
     if (nums.length == 0)
       return new int[0];
     int[] ret = new int[nums.length - k + 1];
-    Queue<Integer> maxheap = new PriorityQueue<>(Collections.reverseOrder());
+    Deque<Integer> window = new ArrayDeque<>();
     for (int i = 0; i < k; i++) {
-      maxheap.offer(nums[i]);
+      window.offerLast(nums[i]);
     }
-    ret[0] = maxheap.peek();
+    ret[0] = Collections.max(window);
     for (int i = 1; i + k - 1 < nums.length; i++) {
-      maxheap.remove(nums[i - 1]);
-      maxheap.offer(nums[i + k - 1]);
-      ret[i] = maxheap.peek();
+      window.pollFirst();
+      window.offerLast(nums[i + k - 1]);
+      ret[i] = Collections.max(window);
     }
     return ret;
   }
