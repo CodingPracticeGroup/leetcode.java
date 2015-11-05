1bceabc57e7ac79aeb0b97dcf97c4a7ffa2e49cd
sealove <huijun.wu.2010@gmail.com>
2015-11-05 09:10:34 -0700

    update

--- Rotate Array.java
+++ Rotate Array.java
@@ -1,18 +1,17 @@
 public class Solution {
   private void rotate_reverse(int[] nums, int start, int end) {
-    for (int i = 0; start + i < end - 1 - i; i++) {
-      int tmp = nums[start + i];
-      nums[start + i] = nums[end - 1 - i];
-      nums[end - 1 - i] = tmp;
+    while (start < end) {
+      int tmp = nums[start];
+      nums[start] = nums[end - 1];
+      nums[end - 1] = tmp;
+      start++;
+      end--;
     }
   }
 
   public void rotate(int[] nums, int k) {
-    if (k == 0)
-      return;
-    k %= nums.length;
     rotate_reverse(nums, 0, nums.length);
-    rotate_reverse(nums, 0, k);
-    rotate_reverse(nums, k, nums.length);
+    rotate_reverse(nums, 0, k % nums.length);
+    rotate_reverse(nums, k % nums.length, nums.length);
   }
 }
