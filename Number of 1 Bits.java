10b77e64eeb949f6c4152e34548858e802824725
sealove <huijun.wu.2010@gmail.com>
2015-11-05 09:19:20 -0700

    update

--- Number of 1 Bits.java
+++ Number of 1 Bits.java
@@ -1,11 +1,16 @@
 public class Solution {
+  private boolean hammingWeight_test(int n, int offset) {
+    return (n & (1 << offset)) != 0;
+  }
+
   // you need to treat n as an unsigned value
   public int hammingWeight(int n) {
-    int ret = 0;
+    int count = 0;
     for (int i = 0; i < 32; i++) {
-      if (((n >>> i) & 1) > 0)
-        ret++;
+      if (hammingWeight_test(n, i)) {
+        count++;
+      }
     }
-    return ret;
+    return count;
   }
 }
--- Reverse Bits.java
+++ Reverse Bits.java
@@ -1,10 +1,19 @@
 public class Solution {
+  private boolean reverseBits_test(int n, int offset) {
+    return (n & (1 << offset)) != 0;
+  }
+
+  private int reverseBits_set(int n, int offset) {
+    return n | (1 << offset);
+  }
+
   // you need treat n as an unsigned value
   public int reverseBits(int n) {
     int ret = 0;
     for (int i = 0; i < 32; i++) {
-      if (((1 << i) & n) != 0)
-        ret |= 1 << (31 - i);
+      if (reverseBits_test(n, i)) {
+        ret = reverseBits_set(ret, 31 - i);
+      }
     }
     return ret;
   }
