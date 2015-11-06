a7bf31b2f31e13d60032d1705c7e31655b5f3a11
sealove <huijun.wu.2010@gmail.com>
2015-11-06 09:51:57 -0700

    update

--- Power of Two.java
+++ Power of Two.java
@@ -1,12 +1,17 @@
 public class Solution {
+  private boolean isPowerOfTwo_test(int n, int offset) {
+    return (n & (1 << offset)) != 0;
+  }
+
   public boolean isPowerOfTwo(int n) {
-    long power = 1L;
-    while (power < n) {
-      power = power << 1;
-    }
-    if (power == n)
-      return true;
-    else
+    if (n <= 0)
       return false;
+    int count = 0;
+    for (int i = 0; i < 32; i++) {
+      if (isPowerOfTwo_test(n, i)) {
+        count++;
+      }
+    }
+    return count == 1;
   }
 }
