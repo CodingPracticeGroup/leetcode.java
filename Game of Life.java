e8066e9544336a44fa9b8e4fad9ffc88195528b9
sealove <huijun.wu.2010@gmail.com>
2015-11-07 13:56:55 -0700

    update

--- Game of Life.java
+++ Game of Life.java
@@ -1,54 +1,57 @@
 public class Solution {
-  private int gameOfLife_count(int[][] board, int i, int j, int[][] cache, int m, int n) {
-    int count = 0;
-
-    if (j - 1 >= 0 && cache[0][j - 1] == 1)
-      count++;
-    if (cache[0][j] == 1)
-      count++;
-    if (j + 1 < n && cache[0][j + 1] == 1)
-      count++;
-    if (j - 1 >= 0 && cache[1][j - 1] == 1)
-      count++;
+  private void gameOfLife_calc(int[][] board, int i, int j, int[][] mem, int m, int n) {
+    int live = 0;
+    if (j - 1 >= 0 && mem[1][j - 1] == 1)
+      live++;
+    if (mem[1][j] == 1)
+      live++;
+    if (j + 1 < n && mem[1][j + 1] == 1)
+      live++;
+    if (j - 1 >= 0 && mem[0][j - 1] == 1)
+      live++;
 
     if (j + 1 < n && board[i][j + 1] == 1)
-      count++;
+      live++;
     if (i + 1 < m && j - 1 >= 0 && board[i + 1][j - 1] == 1)
-      count++;
+      live++;
     if (i + 1 < m && board[i + 1][j] == 1)
-      count++;
+      live++;
     if (i + 1 < m && j + 1 < n && board[i + 1][j + 1] == 1)
-      count++;
+      live++;
 
-    return count;
+    if (board[i][j] == 1) {
+      if (live < 2)
+        board[i][j] = 0;
+      else if (live == 2 || live == 3)
+        board[i][j] = 1;
+      else if (live > 3)
+        board[i][j] = 0;
+    } else {
+      if (live == 3)
+        board[i][j] = 1;
+    }
   }
 
   public void gameOfLife(int[][] board) {
     int m = board.length;
+    if (m == 0)
+      return;
     int n = board[0].length;
-    int[][] cache = new int[2][n];
-    Arrays.fill(cache[0], -1);
-    Arrays.fill(cache[1], -1);
+    if (n == 0)
+      return;
 
+    int[][] mem = new int[2][n];
+    Arrays.fill(mem[0], -1);
+    Arrays.fill(mem[1], -1);
     for (int i = 0; i < m; i++) {
-      for (int k = 0; k < n; k++) {
-        cache[0][k] = cache[1][k];
-        cache[1][k] = board[i][k];
+      for (int j = 0; j < n; j++) {
+        mem[0][j] = board[i][j];
+        gameOfLife_calc(board, i, j, mem, m, n);
       }
       for (int j = 0; j < n; j++) {
-        int count = gameOfLife_count(board, i, j, cache, m, n);
-        if (board[i][j] == 1) {
-          if (count < 2 || count > 3)
-            board[i][j] = 0;
-          else
-            board[i][j] = 1;
-        } else {
-          if (count == 3)
-            board[i][j] = 1;
-          else
-            board[i][j] = 0;
-        }
+        mem[1][j] = mem[0][j];
       }
+      Arrays.fill(mem[0], -1);
     }
   }
 }
