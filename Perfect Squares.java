e99327935c4f6c049c3e97b512839cd354de50d0
sealove <huijun.wu.2010@gmail.com>
2015-11-07 16:12:17 -0700

    pdate

--- Perfect Squares.java
+++ Perfect Squares.java
@@ -1,17 +1,13 @@
 public class Solution {
   public int numSquares(int n) {
     int dp[] = new int[n + 1];
-    for (int i = 0; i <= n; i++) {
+    for (int i = 0; i < dp.length; i++)
       dp[i] = i;
-    }
-    for (int i = 1; i * i <= n; i++) {
+    for (int i = 1; i * i <= n; i++)
       dp[i * i] = 1;
-    }
-    for (int i = 1; i <= n; i++) {
-      for (int j = 1; j * j + i <= n; j++) {
+    for (int i = 1; i <= n; i++)
+      for (int j = 1; j * j + i <= n; j++)
         dp[j * j + i] = Math.min(dp[j * j + i], dp[i] + 1);
-      }
-    }
     return dp[n];
   }
 }
