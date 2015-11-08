788444e20e8ba1cae14c2e185ec7f1bf1856409c
sealove <huijun.wu.2010@gmail.com>
2015-11-08 11:16:55 -0700

    update

--- Integer to English Words.java
+++ Integer to English Words.java
@@ -1,156 +1,152 @@
 public class Solution {
-  private String numberToWords_3(String str) {
+  private boolean numberToWords_0(String s) {
+    for (int i = 0; i < s.length(); i++) {
+      if (s.charAt(i) != '0')
+        return false;
+    }
+    return true;
+  }
+
+  private void numberToWords_1to9(char c, StringBuilder sb) {
+    switch (c) {
+      case '1':
+        sb.append(" One ");
+        break;
+      case '2':
+        sb.append(" Two ");
+        break;
+      case '3':
+        sb.append(" Three ");
+        break;
+      case '4':
+        sb.append(" Four ");
+        break;
+      case '5':
+        sb.append(" Five ");
+        break;
+      case '6':
+        sb.append(" Six ");
+        break;
+      case '7':
+        sb.append(" Seven ");
+        break;
+      case '8':
+        sb.append(" Eight ");
+        break;
+      case '9':
+        sb.append(" Nine ");
+        break;
+    }
+  }
+
+  private void numberToWords_1to9ty(char c, StringBuilder sb) {
+    switch (c) {
+      case '1':
+        sb.append(" Ten ");
+        break;
+      case '2':
+        sb.append(" Twenty ");
+        break;
+      case '3':
+        sb.append(" Thirty ");
+        break;
+      case '4':
+        sb.append(" Forty ");
+        break;
+      case '5':
+        sb.append(" Fifty ");
+        break;
+      case '6':
+        sb.append(" Sixty ");
+        break;
+      case '7':
+        sb.append(" Seventy ");
+        break;
+      case '8':
+        sb.append(" Eighty ");
+        break;
+      case '9':
+        sb.append(" Ninety ");
+        break;
+    }
+  }
+
+  private String numberToWords_3(String s) {
     StringBuilder sb = new StringBuilder();
-    int len = str.length();
-    if (len - 3 >= 0)
-      switch (str.charAt(len - 3)) {
-        case '1':
-          sb.append("One Hundred");
-          break;
-        case '2':
-          sb.append("Two Hundred");
-          break;
-        case '3':
-          sb.append("Three Hundred");
-          break;
-        case '4':
-          sb.append("Four Hundred");
-          break;
-        case '5':
-          sb.append("Five Hundred");
-          break;
-        case '6':
-          sb.append("Six Hundred");
-          break;
-        case '7':
-          sb.append("Seven Hundred");
-          break;
-        case '8':
-          sb.append("Eight Hundred");
-          break;
-        case '9':
-          sb.append("Nine Hundred");
-          break;
+    if (numberToWords_0(s))
+      return sb.toString();
+    if (s.charAt(0) != '0') {
+      numberToWords_1to9(s.charAt(0), sb);
+      sb.append(" Hundred ");
+    }
+    if (s.charAt(1) != '0' && s.charAt(2) != '0') {
+      if (s.charAt(1) == '1') {
+        switch (s.charAt(2)) {
+          case '1':
+            sb.append(" Eleven ");
+            break;
+          case '2':
+            sb.append(" Twelve ");
+            break;
+          case '3':
+            sb.append(" Thirteen ");
+            break;
+          case '4':
+            sb.append(" Fourteen ");
+            break;
+          case '5':
+            sb.append(" Fifteen ");
+            break;
+          case '6':
+            sb.append(" Sixteen ");
+            break;
+          case '7':
+            sb.append(" Seventeen ");
+            break;
+          case '8':
+            sb.append(" Eighteen ");
+            break;
+          case '9':
+            sb.append(" Nineteen ");
+            break;
+        }
+      } else {
+        numberToWords_1to9ty(s.charAt(1), sb);
+        numberToWords_1to9(s.charAt(2), sb);
       }
-    if (len - 2 >= 0)
-      switch (str.charAt(len - 2)) {
-        case '2':
-          sb.append(" Twenty ");
-          break;
-        case '3':
-          sb.append(" Thirty ");
-          break;
-        case '4':
-          sb.append(" Forty ");
-          break;
-        case '5':
-          sb.append(" Fifty ");
-          break;
-        case '6':
-          sb.append(" Sixty ");
-          break;
-        case '7':
-          sb.append(" Seventy ");
-          break;
-        case '8':
-          sb.append(" Eighty ");
-          break;
-        case '9':
-          sb.append(" Ninety ");
-          break;
-      }
-    if (len - 2 >= 0 && str.charAt(len - 2) == '1') {
-      switch (str.charAt(len - 1)) {
-        case '0':
-          sb.append(" Ten");
-          break;
-        case '1':
-          sb.append(" Eleven");
-          break;
-        case '2':
-          sb.append(" Twelve");
-          break;
-        case '3':
-          sb.append(" Thirteen");
-          break;
-        case '4':
-          sb.append(" Fourteen");
-          break;
-        case '5':
-          sb.append(" Fifteen");
-          break;
-        case '6':
-          sb.append(" Sixteen");
-          break;
-        case '7':
-          sb.append(" Seventeen");
-          break;
-        case '8':
-          sb.append(" Eighteen");
-          break;
-        case '9':
-          sb.append(" Nineteen");
-          break;
-      }
-    } else {
-      switch (str.charAt(len - 1)) {
-        case '1':
-          sb.append(" One");
-          break;
-        case '2':
-          sb.append(" Two");
-          break;
-        case '3':
-          sb.append(" Three");
-          break;
-        case '4':
-          sb.append(" Four");
-          break;
-        case '5':
-          sb.append(" Five");
-          break;
-        case '6':
-          sb.append(" Six");
-          break;
-        case '7':
-          sb.append(" Seven");
-          break;
-        case '8':
-          sb.append(" Eight");
-          break;
-        case '9':
-          sb.append(" Nine");
-          break;
-      }
+    } else if (s.charAt(1) != '0') {
+      numberToWords_1to9ty(s.charAt(1), sb);
+    } else if (s.charAt(2) != '0') {
+      numberToWords_1to9(s.charAt(2), sb);
     }
     return sb.toString();
   }
 
   public String numberToWords(int num) {
-    String numstr = String.valueOf(num);
-    if (numstr.equals("0"))
+    if (num == 0)
       return "Zero";
-    Deque<String> numsplit = new ArrayDeque<>();
-    int i = numstr.length() - 3;
-    for (; i >= 0; i -= 3) {
-      numsplit.offerFirst(numstr.substring(i, i + 3));
+    String nn = String.valueOf(num);
+    if (nn.length() % 3 != 0) {
+      StringBuilder nnn = new StringBuilder(nn);
+      for (int i = 0; i < 3 - nn.length() % 3; i++) {
+        nnn.insert(0, '0');
+      }
+      nn = nnn.toString();
     }
-    if (i + 3 > 0)
-      numsplit.offerFirst(numstr.substring(0, i + 3));
-    Deque<String> unit = new ArrayDeque<>();
-    unit.offerFirst(" ");
-    unit.offerFirst(" Thousand ");
-    unit.offerFirst(" Million ");
-    unit.offerFirst(" Billion ");
-    StringBuilder sb = new StringBuilder();
-    while (!numsplit.isEmpty()) {
-      String t = numberToWords_3(numsplit.pollLast());
-      if (t.trim().length() > 0)
-        sb.insert(0, unit.pollLast());
-      else
-        unit.pollLast();
-      sb.insert(0, t);
+    StringBuilder ret = new StringBuilder();
+    for (int i = 0; i < nn.length(); i += 3) {
+      String ss = numberToWords_3(nn.substring(i, i + 3));
+      if (!ss.equals("")) {
+        ret.append(ss);
+        if ((nn.length() - i - 3) / 3 == 1) {
+          ret.append(" Thousand ");
+        } else if ((nn.length() - i - 3) / 3 == 2) {
+          ret.append(" Million  ");
+        } else if ((nn.length() - i - 3) / 3 == 3) {
+          ret.append(" Billion  ");
+        }
+      }
     }
-    return sb.toString().trim().replaceAll(" +", " ");
+    return ret.toString().replaceAll("\\s+", " ").trim();
   }
 }
