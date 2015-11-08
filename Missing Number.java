7261cde41c587edb888b757222dae8fd2727af7f
sealove <huijun.wu.2010@gmail.com>
2015-11-08 11:20:38 -0700

    update

--- Missing Number.java
+++ Missing Number.java
@@ -1,21 +1,10 @@
 public class Solution {
   public int missingNumber(int[] nums) {
-    for (int i = 0; i < nums.length; i++) {
-      if (nums[i] != i) {
-        int target = nums[i];
-        nums[i] = -1;
-        while (0 <= target && target < nums.length && nums[target] != target) {
-          int tmp = nums[target];
-          nums[target] = target;
-          target = tmp;
-        };
-      }
+    int n = nums.length;
+    int total = (1 + n) * n / 2;
+    for (int i : nums) {
+      total -= i;
     }
-    for (int i = 0; i < nums.length; i++) {
-      if (nums[i] < 0) {
-        return i;
-      }
-    }
-    return nums.length;
+    return total;
   }
 }
