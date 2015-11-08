3ce02ece4950a55cfb0557689331110c05126163
sealove <huijun.wu.2010@gmail.com>
2015-11-08 10:14:30 -0700

    update

--- H-Index.java
+++ H-Index.java
@@ -1,11 +1,15 @@
 public class Solution {
   public int hIndex(int[] citations) {
-    Arrays.sort(citations);
-    for (int i = 0; i < citations.length; i++) {
-      if (citations[i] >= citations.length - i) {
-        return citations.length - i;
+    int low = 0;
+    int high = citations.length;
+    while (low < high) {
+      int mid = low + (int) Math.ceil((high - low) / 2.0);
+      if (Arrays.stream(citations).filter(x -> x >= mid).count() >= mid) {
+        low = mid;
+      } else {
+        high = mid - 1;
       }
     }
-    return 0;
+    return low; // low==high
   }
 }
