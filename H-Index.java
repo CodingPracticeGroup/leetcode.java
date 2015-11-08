34bb7dca643c2e793400e6544e2ad218ef5b7db8
sealove <huijun.wu.2010@gmail.com>
2015-11-08 10:08:17 -0700

    update

--- H-Index.java
+++ H-Index.java
@@ -1,19 +1,11 @@
 public class Solution {
   public int hIndex(int[] citations) {
-    Map<Integer, Long> map =
-        Arrays.stream(citations).boxed()
-            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-    long total = map.values().stream().reduce(0L, Long::sum);
-    long left = map.containsKey(0) ? total - map.get(0) : total;
-    for (int i = 1; i <= citations.length; i++) {
-      if (left >= i) {
-        if (map.containsKey(i)) {
-          left -= map.get(i);
-        }
-      } else {
-        return i - 1;
+    Arrays.sort(citations);
+    for (int i = 0; i < citations.length; i++) {
+      if (citations[i] >= citations.length - i) {
+        return citations.length - i;
       }
     }
-    return citations.length;
+    return 0;
   }
 }
