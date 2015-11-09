a303090d2694ece132a0699a40f89b2b97f9463a
sealove <huijun.wu.2010@gmail.com>
2015-11-08 23:58:00 -0700

    updat

--- Ugly Number II.java
+++ Ugly Number II.java
@@ -1,35 +1,31 @@
 public class Solution {
   public int nthUglyNumber(int n) {
-    LinkedList<Integer> list = new LinkedList<>();
-    list.add(1);
-    int l2 = 0;
-    int l3 = 0;
-    int l5 = 0;
-    for (int i = 2; i <= n; i++) {
-      int u2 = list.get(l2) * 2;
-      while (u2 <= list.peekLast()) {
-        u2 = list.get(++l2) * 2;
+    int dp[] = new int[n];
+    dp[0] = 1;
+    int idx2 = 0;
+    int idx3 = 0;
+    int idx5 = 0;
+    for (int i = 1; i < n; i++) {
+      int tmp2 = dp[idx2] * 2;
+      while (tmp2 <= dp[i - 1]) {
+        tmp2 = dp[++idx2] * 2;
       }
-      int u3 = list.get(l3) * 3;
-      while (u3 <= list.peekLast()) {
-        u3 = list.get(++l3) * 3;
+      int tmp3 = dp[idx3] * 3;
+      while (tmp3 <= dp[i - 1]) {
+        tmp3 = dp[++idx3] * 3;
       }
-      int u5 = list.get(l5) * 5;
-      while (u5 <= list.peekLast()) {
-        u5 = list.get(++l5) * 5;
+      int tmp5 = dp[idx5] * 5;
+      while (tmp5 <= dp[i - 1]) {
+        tmp5 = dp[++idx5] * 5;
       }
-      int min = Math.min(u2, Math.min(u3, u5));
-      if (u2 == min) {
-        list.offerLast(u2);
-        l2++;
-      } else if (u3 == min) {
-        list.offerLast(u3);
-        l3++;
-      } else if (u5 == min) {
-        list.offerLast(u5);
-        l5++;
+      if (tmp2 <= tmp3 && tmp2 <= tmp5) {
+        dp[i] = tmp2;
+      } else if (tmp3 <= tmp2 && tmp3 <= tmp5) {
+        dp[i] = tmp3;
+      } else if (tmp5 <= tmp2 && tmp5 <= tmp3) {
+        dp[i] = tmp5;
       }
     }
-    return list.peekLast();
+    return dp[n - 1];
   }
 }
