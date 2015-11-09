81032755c63f64f08cdcabafd9dbca5b8a4ed076
sealove <huijun.wu.2010@gmail.com>
2015-11-09 00:25:33 -0700

    update

--- Different Ways to Add Parentheses.java
+++ Different Ways to Add Parentheses.java
@@ -1,40 +1,39 @@
 public class Solution {
-  private List<Integer> diffWaysToCompute(String input, int start, int end) {
+  private List<Integer> diffWaysToCompute_bi_re(String input, int start, int end,
+      Map<String, List<Integer>> mem) {
+    String key = input.substring(start, end);
+    if (mem.containsKey(key)) {
+      return mem.get(key);
+    }
     List<Integer> ret = new ArrayList<>();
     for (int i = start; i < end; i++) {
+      List<Integer> left = diffWaysToCompute_bi_re(input, start, i, mem);
+      List<Integer> right = diffWaysToCompute_bi_re(input, i + 1, end, mem);
       if (input.charAt(i) == '+') {
-        List<Integer> left = diffWaysToCompute(input, start, i);
-        List<Integer> right = diffWaysToCompute(input, i + 1, end);
-        for (int j : left) {
-          for (int k : right) {
+        for (Integer j : left)
+          for (Integer k : right)
             ret.add(j + k);
-          }
-        }
       } else if (input.charAt(i) == '-') {
-        List<Integer> left = diffWaysToCompute(input, start, i);
-        List<Integer> right = diffWaysToCompute(input, i + 1, end);
-        for (int j : left) {
-          for (int k : right) {
+        for (Integer j : left)
+          for (Integer k : right)
             ret.add(j - k);
-          }
-        }
       } else if (input.charAt(i) == '*') {
-        List<Integer> left = diffWaysToCompute(input, start, i);
-        List<Integer> right = diffWaysToCompute(input, i + 1, end);
-        for (int j : left) {
-          for (int k : right) {
+        for (Integer j : left)
+          for (Integer k : right)
             ret.add(j * k);
-          }
-        }
       }
     }
-    if (ret.isEmpty()) {
-      ret.add(Integer.parseInt(input.substring(start, end)));
+    if (ret.size() == 0) {
+      if (start == end)
+        ret.add(0);
+      else
+        ret.add(Integer.parseInt(input.substring(start, end)));
     }
+    mem.put(key, ret);
     return ret;
   }
 
   public List<Integer> diffWaysToCompute(String input) {
-    return diffWaysToCompute(input, 0, input.length());
+    return diffWaysToCompute_bi_re(input, 0, input.length(), new HashMap<String, List<Integer>>());
   }
 }
