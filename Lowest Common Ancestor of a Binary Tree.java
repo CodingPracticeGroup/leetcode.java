1a0e1748a66e5403fb4669076836d306e3aab8dc
sealove <huijun.wu.2010@gmail.com>
2015-11-09 01:40:57 -0700

    updat

--- Lowest Common Ancestor of a Binary Tree.java
+++ Lowest Common Ancestor of a Binary Tree.java
@@ -1,33 +1,35 @@
 public class Solution {
-  private void lowestCommonAncestor(TreeNode root, TreeNode r, Deque<TreeNode> ret,
+  private void lowestCommonAncestor_bt(TreeNode root, TreeNode p, List<TreeNode> ret,
       Deque<TreeNode> stack) {
     if (root != null) {
       stack.offerLast(root);
-      if (root == r) {
+      if (root == p) {
         ret.addAll(stack);
       } else {
-        lowestCommonAncestor(root.left, r, ret, stack);
-        lowestCommonAncestor(root.right, r, ret, stack);
+        if (ret.isEmpty())
+          lowestCommonAncestor_bt(root.left, p, ret, stack);
+        if (ret.isEmpty())
+          lowestCommonAncestor_bt(root.right, p, ret, stack);
       }
       stack.pollLast();
     }
   }
 
   public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-    Deque<TreeNode> p_path = new ArrayDeque<>();
-    lowestCommonAncestor(root, p, p_path, new ArrayDeque<TreeNode>());
-    Deque<TreeNode> q_path = new ArrayDeque<>();
-    lowestCommonAncestor(root, q, q_path, new ArrayDeque<TreeNode>());
-    TreeNode last = root;
-    p_path.pollFirst();
-    q_path.pollFirst();
-    while (!p_path.isEmpty()) {
-      TreeNode ptn = p_path.pollFirst();
-      TreeNode qtn = q_path.pollFirst();
-      if (ptn != qtn)
-        return last;
-      last = ptn;
+    List<TreeNode> pathp = new ArrayList<>();
+    lowestCommonAncestor_bt(root, p, pathp, new ArrayDeque<TreeNode>());
+    List<TreeNode> pathq = new ArrayList<>();
+    lowestCommonAncestor_bt(root, q, pathq, new ArrayDeque<TreeNode>());
+    int len = Math.min(pathp.size(), pathq.size());
+    for (int i = 0; i < len; i++) {
+      if (pathp.get(i) != pathq.get(i)) {
+        return pathp.get(i - 1);
+      }
     }
-    return last;
+    if (len == pathp.size()) {
+      return pathp.get(len - 1);
+    } else {
+      return pathq.get(len - 1);
+    }
   }
 }
