bd0915cf05d570442f515c065954e951ce73073d
sealove <huijun.wu.2010@gmail.com>
2015-11-09 01:33:31 -0700

    update

--- Product of Array Except Self.java
+++ Product of Array Except Self.java
@@ -1,13 +1,15 @@
 public class Solution {
   public int[] productExceptSelf(int[] nums) {
     int[] ret = new int[nums.length];
-    for (int i = 0; i < nums.length; i++) {
-      ret[i] = nums[i] * (i - 1 >= 0 ? ret[i - 1] : 1);
+    ret[0] = nums[0];
+    for (int i = 1; i < nums.length; i++) {
+      ret[i] = ret[i - 1] * nums[i];
     }
-    int last = 1;
+    int rl = 1;
     for (int i = nums.length - 1; i >= 0; i--) {
-      ret[i] = last * (i - 1 >= 0 ? ret[i - 1] : 1);
-      last *= nums[i];
+      if (i + 1 < nums.length)
+        rl *= nums[i + 1];
+      ret[i] = (i - 1 >= 0 ? ret[i - 1] : 1) * rl;
     }
     return ret;
   }
