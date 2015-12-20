3764f7f53fd0415f8d7a14947e86d614a6009785
Huijun Wu <Huijun Wu>
2015-12-20 14:02:37 +0800

    treemap/treeset: log k, ceiling/floor[inclusive], lower/higher(exclusive)

--- Contains Duplicate III.java
+++ Contains Duplicate III.java
@@ -1,18 +1,27 @@
 public class Solution {
   public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-    if (k < 1 || t < 0)
-      return false;
-    TreeSet<Long> window = new TreeSet<>();
+    TreeMap<Long, Integer> window = new TreeMap<>(); // n * log k
     for (int i = 0; i < nums.length; i++) {
-      Long s = window.ceiling((long) nums[i]);
-      if (s != null && s - nums[i] <= t)
+      if (i - k - 1 >= 0) {
+        window.put((long) nums[i - k - 1], window.get((long) nums[i - k - 1]) - 1);
+        if (window.get((long) nums[i - k - 1]) == 0) {
+          window.remove((long) nums[i - k - 1]);
+        }
+      }
+      //
+      Long floor = window.floorKey((long) nums[i]);
+      if (floor != null && nums[i] - floor <= t) {
         return true;
-      s = window.floor((long) nums[i]);
-      if (s != null && nums[i] - s <= t)
+      }
+      Long ceiling = window.ceilingKey((long) nums[i]);
+      if (ceiling != null && ceiling - nums[i] <= t) {
         return true;
-      if (i >= k)
-        window.remove((long) nums[i - k]);
-      window.add((long) nums[i]);
+      }
+      //
+      if (!window.containsKey((long) nums[i])) {
+        window.put((long) nums[i], 0);
+      }
+      window.put((long) nums[i], window.get((long) nums[i]) + 1);
     }
     return false;
   }
