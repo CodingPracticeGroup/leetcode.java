a3bc7d908e9f6c31c0fc345a52836cbbeeb53298
Huijun Wu <Huijun Wu>
2015-12-20 17:03:48 +0800

    dfs with bt

--- Combination Sum III.java
+++ Combination Sum III.java
@@ -1,26 +1,26 @@
 public class Solution {
-  private void combinationSum3_bt(int k, int n, List<List<Integer>> ret, Deque<Integer> stack) {
-    if (n == 0 && stack.size() == k) { // report
+  void bt(int k, int n, List<List<Integer>> ret, LinkedList<Integer> stack) {
+    if (k == 0 && n == 0) {
       ret.add(new ArrayList<Integer>(stack));
     } else {
-      if (n <= 0 || stack.size() >= k)
-        return; // prune
-      int start = stack.isEmpty() ? 1 : stack.peekLast() + 1;
-      if (start > n)
-        return; // prune
-      for (int i = start; i <= 9; i++) {
-        if (i <= n) { // prune
-          stack.offerLast(i);
-          combinationSum3_bt(k, n - i, ret, stack);
-          stack.pollLast();
-        }
+      if (k <= 0 || n <= 0) {
+        return;
+      }
+      int last = 0;
+      if (!stack.isEmpty()) {
+        last = stack.peekLast();
+      }
+      for (int i = last + 1; i <= 9; i++) {
+        stack.offerLast(i);
+        bt(k - 1, n - i, ret, stack);
+        stack.pollLast();
       }
     }
   }
 
   public List<List<Integer>> combinationSum3(int k, int n) {
     List<List<Integer>> ret = new ArrayList<>();
-    combinationSum3_bt(k, n, ret, new ArrayDeque<Integer>());
+    bt(k, n, ret, new LinkedList<Integer>());
     return ret;
   }
 }
