0d3e235867a5b8e5c7867b8e542cb3a1ed1fdd6f
Huijun Wu <Huijun Wu>
2015-12-20 16:48:04 +0800

    treemap: nlogn, sort and associate

--- The Skyline Problem.java
+++ The Skyline Problem.java
@@ -1,60 +1,68 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Solution {
-  private int getSkyline_left(int[] idx, int key) {
-    int low = 0, high = idx.length - 1;
-    while (low < high) {
-      int mid = (low + high) / 2; // default towards to 0/low
-      if (idx[mid] < key) {
-        low = mid + 1;
-      } else if (idx[mid] > key) {
-        high = mid - 1;
-      } else {
-        high = mid; // keep 'high' hit possibility
-      }
-    }
-    return high;
-  }
-
-  private int getSkyline_right(int[] idx, int key) {
-    int low = 0, high = idx.length - 1;
-    while (low < high) {
-      int mid = (low + high + 1) / 2; // towards to 'high'
-      if (idx[mid] < key) {
-        low = mid + 1;
-      } else if (key < idx[mid]) {
-        high = mid - 1;
-      } else {
-        low = mid; // keep 'low' hit possibility
-      }
-    }
-    return low;
-  }
-
   public List<int[]> getSkyline(int[][] buildings) {
-    int size = buildings.length * 2;
-    int[] idx = new int[size];
-    for (int i = 0; i < buildings.length; i++) {
-      idx[i * 2] = buildings[i][0];
-      idx[i * 2 + 1] = buildings[i][1];
-    }
-    Arrays.sort(idx);
-
-    int[] max_height = new int[buildings.length * 2];
-    Arrays.fill(max_height, 0);
-
-    for (int i = 0; i < buildings.length; i++) {
-      int left = getSkyline_left(idx, buildings[i][0]);
-      int right = getSkyline_right(idx, buildings[i][1]);
-      for (int j = left; j < right; j++) {
-        max_height[j] = Math.max(max_height[j], buildings[i][2]);
+    TreeMap<Integer, Map<Integer, Integer>> idx_heightAction_count = new TreeMap<>();
+    for (int[] b : buildings) {
+      // create index
+      if (!idx_heightAction_count.containsKey(b[0])) {
+        idx_heightAction_count.put(b[0], new TreeMap<Integer, Integer>());
       }
+      if (!idx_heightAction_count.containsKey(b[1])) {
+        idx_heightAction_count.put(b[1], new TreeMap<Integer, Integer>());
+      }
+      // increase height at index 0
+      if (!idx_heightAction_count.get(b[0]).containsKey(b[2])) {
+        idx_heightAction_count.get(b[0]).put(b[2], 0);
+      }
+      int old = idx_heightAction_count.get(b[0]).get(b[2]);
+      idx_heightAction_count.get(b[0]).put(b[2], old + 1);
+      // decrease height at index 1
+      if (!idx_heightAction_count.get(b[1]).containsKey(-b[2])) {
+        idx_heightAction_count.get(b[1]).put(-b[2], 0);
+      }
+      old = idx_heightAction_count.get(b[1]).get(-b[2]);
+      idx_heightAction_count.get(b[1]).put(-b[2], old + 1);
     }
 
+    TreeMap<Integer, Integer> active_height = new TreeMap<>();
     List<int[]> ret = new ArrayList<>();
     int last_height = 0;
-    for (int i = 0; i < size; i++) {
-      if (max_height[i] != last_height) {
-        ret.add(new int[] {idx[i], max_height[i]});
-        last_height = max_height[i];
+    for (Map.Entry<Integer, Map<Integer, Integer>> e : idx_heightAction_count.entrySet()) {
+      // for each idx
+      int idx = e.getKey();
+      Map<Integer, Integer> actionHeight_count = e.getValue();
+      for (Map.Entry<Integer, Integer> ee : actionHeight_count.entrySet()) {
+        // for each height action
+        int actionHeight = ee.getKey();
+        int count = ee.getValue();
+        if (actionHeight < 0) {
+          int old = active_height.get(-actionHeight);
+          if (old - count == 0) {
+            active_height.remove(-actionHeight);
+          } else {
+            active_height.put(-actionHeight, old - count);
+          }
+        } else {
+          if (!active_height.containsKey(actionHeight)) {
+            active_height.put(actionHeight, 0);
+          }
+          int old = active_height.get(actionHeight);
+          active_height.put(actionHeight, old + count);
+        }
+      }
+      // judge after processing actions for each idx
+      if (active_height.isEmpty()) {
+        ret.add(new int[] {idx, 0});
+      } else {
+        int cur = active_height.lastKey();
+        if (cur != last_height) {
+          ret.add(new int[] {idx, cur});
+          last_height = cur;
+        }
       }
     }
     return ret;
