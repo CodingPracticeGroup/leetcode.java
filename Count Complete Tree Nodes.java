f19198b996ebc719ad92d127b598ea88075e5d41
Huijun Wu <Huijun Wu>
2015-12-20 12:14:01 +0800

    not necessary to binary search in the last level. instead, judge perfect tree and perfect tree height

--- Count Complete Tree Nodes.java
+++ Count Complete Tree Nodes.java
@@ -1,23 +1,29 @@
 public class Solution {
-  private int countNodes_perfect(TreeNode root) {
-    int cnt_left = 0;
-    for (TreeNode p = root; p != null; p = p.left) {
-      cnt_left++;
+  int perfect(TreeNode root) {
+    int c_l = 0;
+    for (TreeNode tn = root; tn.left != null; tn = tn.left) {
+      c_l++;
     }
-    int cnt_right = 0;
-    for (TreeNode p = root; p != null; p = p.right) {
-      cnt_right++;
+    int c_r = 0;
+    for (TreeNode tn = root; tn.right != null; tn = tn.right) {
+      c_r++;
     }
-    if (cnt_left == cnt_right)
-      return cnt_left;
-    return -1;
+    if (c_l == c_r) {
+      return c_l;
+    } else {
+      return -1;
+    }
   }
 
   public int countNodes(TreeNode root) {
-    int h = countNodes_perfect(root);
-    if (h == -1)
-      return 1 + countNodes(root.left) + countNodes(root.right);
-    else
-      return (1 << h) - 1;
+    if (root == null) {
+      return 0;
+    }
+    int h = perfect(root);
+    if (h >= 0) {
+      return (1 << (h + 1)) - 1; // Math.pow timeout
+    } else {
+      return countNodes(root.left) + countNodes(root.right) + 1;
+    }
   }
 }
