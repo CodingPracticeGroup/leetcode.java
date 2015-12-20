0118552d4bebcf15f4b0e886dd5aee769e066514
Huijun Wu <Huijun Wu>
2015-12-20 18:59:48 +0800

    cycle in array, dp

--- House Robber II.java
+++ House Robber II.java
@@ -1,23 +1,27 @@
 public class Solution {
-  private int rob_(int[] nums, int start, int end) {
-    if (start >= end)
-      return 0;
-    if (start + 1 == end)
+  int rob_internal(int[] nums, int start, int end) {
+    if (end - start == 2) {
+      return Math.max(nums[start], nums[start + 1]);
+    } else if (end - start == 1) {
       return nums[start];
+    }
     int dp[] = new int[end - start];
     dp[0] = nums[start];
-    dp[1] = Math.max(dp[0], nums[start + 1]);
-    for (int i = 2; start + i < end; i++) {
-      dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[start + i]);
+    dp[1] = Math.max(nums[start], nums[start + 1]);
+    for (int i = start + 2; i < end; i++) {
+      dp[i - start] = Math.max(dp[i - 1 - start], dp[i - 2 - start] + nums[i]);
     }
     return dp[end - start - 1];
   }
 
   public int rob(int[] nums) {
-    if (nums.length == 1)
+    if (nums.length == 0) {
+      return 0;
+    } else if (nums.length == 1) {
       return nums[0];
-    int rob_first = rob_(nums, 0, nums.length - 1);
-    int rob_last = rob_(nums, 1, nums.length);
-    return Math.max(rob_first, rob_last);
+    } else if (nums.length == 2) {
+      return Math.max(nums[0], nums[1]);
+    }
+    return Math.max(rob_internal(nums, 0, nums.length - 1), rob_internal(nums, 1, nums.length));
   }
 }
