dfa121d8a6ee640013e1f12126edbd79dd152fb1
huijunwu <huijun.wu.2010@gmail.com>
2016-01-17 16:09:28 -0700

    math from wiki

--- Add Digits.java
+++ Add Digits.java
@@ -1,5 +1,9 @@
 public class Solution {
   public int addDigits(int num) {
+    return num - 9 * ((num - 1) / 9);
+  }
+
+  public int addDigits_(int num) {
     while (num > 9) {
       num = String.valueOf(num).chars().map(x -> x - '0').reduce(0, (a, b) -> a + b);
     }
