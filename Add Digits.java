8d467a811522bf64087e12c45afad73030589ebe
huijunwu <huijun.wu.2010@gmail.com>
2016-01-17 16:00:42 -0700

    stream one line, but not math optimal

--- Add Digits.java
+++ Add Digits.java
@@ -1,7 +1,7 @@
 public class Solution {
   public int addDigits(int num) {
     while (num > 9) {
-      num = String.valueOf(num).chars().map(x -> x - '0').reduce((x, y) -> x + y).getAsInt();
+      num = String.valueOf(num).chars().map(x -> x - '0').reduce(0, (a, b) -> a + b);
     }
     return num;
   }
