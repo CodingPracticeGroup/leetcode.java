1a3bbd962d4f817d2720ad1f7bbe3388533d1d64
huijunwu <huijun.wu.2010@gmail.com>
2016-01-18 09:49:23 -0700

    math, Math.pow, Math.log

--- Power of Three.java
+++ Power of Three.java
@@ -1,11 +1,18 @@
 public class Solution {
-  public boolean isPowerOfThree(int n) {
-    for (long i = 1; i <= n;) {
-      if (i == n) {
-        return true;
-      }
-      i *= 3;
+  public boolean isPowerOfThree_(int n) {
+    if (n <= 0) {
+      return false;
     }
-    return false;
+    while (n % 3 == 0) {
+      n /= 3;
+    }
+    return n == 1;
+  }
+
+  public boolean isPowerOfThree(int n) {
+    if (n <= 0) {
+      return false;
+    }
+    return Math.pow(3, Math.round(Math.log(n) / Math.log(3))) == n;
   }
 }
