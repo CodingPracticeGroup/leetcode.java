71030d791bb73b0d1c7c30f8ed49aabcc6be43be
huijunwu <huijun.wu.2010@gmail.com>
2016-01-18 10:17:48 -0700

    stack, queue, Deque

--- Implement Queue using Stacks.java
+++ Implement Queue using Stacks.java
@@ -1,40 +1,37 @@
 class MyQueue {
-  Deque<Integer> s1 = new ArrayDeque<>();
-  Deque<Integer> s2 = new ArrayDeque<>();
+  Deque<Integer> stack_tmp = new ArrayDeque<>();
+  Deque<Integer> stack_last = new ArrayDeque<>();
 
   // Push element x to the back of queue.
   public void push(int x) {
-    s1.push(x);
+    stack_last.push(x);
   }
 
   // Removes the element from in front of queue.
   public void pop() {
-    int count = s1.size();
-    for (int i = 1; i < count; i++) {
-      s2.push(s1.pop());
+    while (stack_last.size() > 1) {
+      stack_tmp.push(stack_last.pop());
     }
-    s1.pop();
-    while (!s2.isEmpty()) {
-      s1.push(s2.pop());
+    stack_last.clear();
+    while (!stack_tmp.isEmpty()) {
+      stack_last.push(stack_tmp.pop());
     }
   }
 
   // Get the front element.
   public int peek() {
-    int count = s1.size();
-    for (int i = 1; i < count; i++) {
-      s2.push(s1.pop());
+    while (stack_last.size() > 1) {
+      stack_tmp.push(stack_last.pop());
     }
-    int ret = s1.peek();
-    s2.push(s1.pop());
-    while (!s2.isEmpty()) {
-      s1.push(s2.pop());
+    int ret = stack_last.peek();
+    while (!stack_tmp.isEmpty()) {
+      stack_last.push(stack_tmp.pop());
     }
     return ret;
   }
 
   // Return whether the queue is empty.
   public boolean empty() {
-    return s1.isEmpty();
+    return stack_last.isEmpty();
   }
 }
