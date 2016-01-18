f4b25306a32dc88228b7f31084ce5aaae9952d6c
huijunwu <huijun.wu.2010@gmail.com>
2016-01-17 17:48:16 -0700

    linkedlist, loop termination

--- Odd Even Linked List.java
+++ Odd Even Linked List.java
@@ -1,8 +1,8 @@
 public class Solution {
   public ListNode oddEvenList(ListNode head) {
-    ListNode evenHead = new ListNode(0);
+    ListNode even = new ListNode(0);
     ListNode p = head;
-    ListNode q = evenHead;
+    ListNode q = even;
     while (p != null && p.next != null) {
       q.next = p.next;
       p.next = p.next.next;
@@ -11,10 +11,10 @@ public class Solution {
       q.next = null;
 
       if (p.next == null) {
-        p.next = evenHead.next;
+        p.next = even.next;
         break;
       } else if (p.next.next == null) {
-        p.next.next = evenHead.next;
+        p.next.next = even.next;
         break;
       } else {
         p = p.next;
