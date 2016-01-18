44afa0f7af9eb9b9b015055c0d5e2d699d5d5827
huijunwu <huijun.wu.2010@gmail.com>
2016-01-18 08:06:20 -0700

    array, hashset, set.add(duplicate)

--- Contains Duplicate.java
+++ Contains Duplicate.java
@@ -1,11 +1,10 @@
 public class Solution {
   public boolean containsDuplicate(int[] nums) {
-    Set<Integer> seen = new HashSet<>();
+    Set<Integer> s = new HashSet<>();
     for (int i : nums) {
-      if (seen.contains(i)) {
+      boolean b = s.add(i);
+      if (b == false) {
         return true;
-      } else {
-        seen.add(i);
       }
     }
     return false;
