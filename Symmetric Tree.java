28b5507927190aedf0d11100ff390cbed68ebbf2
huijunwu <huijun.wu.2010@gmail.com>
2016-01-18 10:29:41 -0700

    tree, recursion

--- Symmetric Tree.java
+++ Symmetric Tree.java
@@ -1,16 +1,17 @@
 public class Solution {
-  private boolean isSymmetric_(TreeNode root1, TreeNode root2) {
-    if (root1 == null && root2 == null)
+  private boolean mirror(TreeNode l, TreeNode r) {
+    if (l == null && r == null) {
       return true;
-    if (root1 != null && root2 != null)
-      return root1.val == root2.val && isSymmetric_(root1.left, root2.right)
-          && isSymmetric_(root1.right, root2.left);
+    } else if (l != null && r != null) {
+      return l.val == r.val && mirror(l.left, r.right) && mirror(l.right, r.left);
+    }
     return false;
   }
 
   public boolean isSymmetric(TreeNode root) {
-    if (root == null)
+    if (root == null) {
       return true;
-    return isSymmetric_(root.left, root.right);
+    }
+    return mirror(root.left, root.right);
   }
 }
