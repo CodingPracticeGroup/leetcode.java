7a64beac810b5d6867098751b7723235e4e1243d
huijunwu <huijun.wu.2010@gmail.com>
2016-01-17 17:19:17 -0700

    java8stream on string, map.equals

--- Valid Anagram.java
+++ Valid Anagram.java
@@ -1,17 +1,9 @@
 public class Solution {
   public boolean isAnagram(String s, String t) {
-    Map<Integer, Long> s_count =
-        s.chars().boxed()
-            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-    Map<Integer, Long> t_count =
-        t.chars().boxed()
-            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-    if (s_count.size() != t_count.size())
-      return false;
-    for (Integer i : s_count.keySet()) {
-      if (!s_count.get(i).equals(t_count.get(i)))
-        return false;
-    }
-    return true;
+    Map<Integer, Long> ss = s.chars().boxed()
+        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    Map<Integer, Long> tt = t.chars().boxed()
+        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    return ss.equals(tt);
   }
 }
