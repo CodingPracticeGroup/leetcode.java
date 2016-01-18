f5905a5d50a245840b57477c0abe79ae1d568319
huijunwu <huijun.wu.2010@gmail.com>
2016-01-17 17:53:08 -0700

    array, map-count

--- Majority Element.java
+++ Majority Element.java
@@ -1,19 +1,19 @@
 public class Solution {
   public int majorityElement(int[] nums) {
-    int candidate = nums[0];
-    int candidate_count = 1;
-    for (int i = 1; i < nums.length; i++) {
-      if (candidate == nums[i]) {
-        candidate_count++;
+    int ret = 0;
+    int count = 0;
+    for (int i : nums) {
+      if (count == 0) {
+        ret = i;
+        count = 1;
       } else {
-        candidate_count--;
-        if (candidate_count == 0) {
-          i++;
-          candidate = nums[i];
-          candidate_count = 1;
+        if (i == ret) {
+          count++;
+        } else {
+          count--;
         }
       }
     }
-    return candidate;
+    return ret;
   }
 }
