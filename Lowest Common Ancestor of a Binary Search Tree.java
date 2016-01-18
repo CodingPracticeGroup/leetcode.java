0e154c44087b450d7cb0c3d955e3264971a51cea
huijunwu <huijun.wu.2010@gmail.com>
2016-01-17 18:12:48 -0700

    bstree, rootpath, linkedlist.peek

--- Lowest Common Ancestor of a Binary Search Tree.java
+++ Lowest Common Ancestor of a Binary Search Tree.java
@@ -1,36 +1,27 @@
 public class Solution {
-  private void lowestCommonAncestor_(TreeNode root, TreeNode p, List<TreeNode> ret,
-      Deque<TreeNode> stack) {
-    if (ret.size() == 0) { // prune
-      stack.offerLast(root);
-      if (root == p) { // report
-        ret.addAll(stack);
+  LinkedList<TreeNode> rootPath(TreeNode root, TreeNode p) {
+    LinkedList<TreeNode> ret = new LinkedList<>();
+    while (root != p) {
+      ret.offerLast(root);
+      if (p.val < root.val) {
+        root = root.left;
       } else {
-        if (p.val < root.val) { // prune
-          lowestCommonAncestor_(root.left, p, ret, stack); // explore
-        } else if (root.val < p.val) { // prune
-          lowestCommonAncestor_(root.right, p, ret, stack); // explore
-        }
+        root = root.right;
       }
-      stack.pollLast();
     }
+    ret.offerLast(p);
+    return ret;
   }
 
   public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-    List<TreeNode> path_p = new ArrayList<>();
-    lowestCommonAncestor_(root, p, path_p, new ArrayDeque<TreeNode>());
-    List<TreeNode> path_q = new ArrayList<>();
-    lowestCommonAncestor_(root, q, path_q, new ArrayDeque<TreeNode>());
-    int len = Math.min(path_p.size(), path_q.size());
-    for (int i = 0; i < len; i++) {
-      if (path_p.get(i) != path_q.get(i)) {
-        return path_p.get(i - 1);
-      }
+    LinkedList<TreeNode> pp = rootPath(root, p);
+    LinkedList<TreeNode> qq = rootPath(root, q);
+
+    TreeNode ret = null;
+    while (pp.peekFirst() == qq.peekFirst()) {
+      ret = pp.pollFirst();
+      qq.pollFirst();
     }
-    if (len == path_p.size()) {
-      return path_p.get(len - 1);
-    } else {
-      return path_q.get(len - 1);
-    }
+    return ret;
   }
 }
