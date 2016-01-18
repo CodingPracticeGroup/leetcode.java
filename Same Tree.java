955dcb6d99f2693e3aefd72e2d1d788cc00567a3
huijunwu <huijun.wu.2010@gmail.com>
2016-01-17 17:07:39 -0700

    tree, recursion

--- Same Tree.java
+++ Same Tree.java
@@ -1,12 +1,13 @@
 public class Solution {
   public boolean isSameTree(TreeNode p, TreeNode q) {
-    if (p != null && q != null) {
-      if (p.val != q.val)
-        return false;
-      return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-    }
-    if (p == null && q == null)
+    if (p == null && q == null) {
       return true;
-    return false;
+    } else if (p == null || q == null) {
+      return false;
+    }
+    if (p.val != q.val) {
+      return false;
+    }
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
   }
 }
