22a18d0e5fb17fc4b04a87d6e11fb11cc95fb576
huijunwu <huijun.wu.2010@gmail.com>
2016-01-18 10:10:00 -0700

    linkedlist

--- Merge Two Sorted Lists.java
+++ Merge Two Sorted Lists.java
@@ -1,23 +1,52 @@
 public class Solution {
-  public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-    ListNode head = new ListNode(0);
-    ListNode p = head;
-    while (l1 != null && l2 != null) {
-      if (l1.val < l2.val) {
-        p.next = l1;
+  public ListNode mergeTwoLists_(ListNode l1, ListNode l2) {
+    ListNode ret = new ListNode(0);
+    ListNode p = ret;
+    while (!(l1 == null && l2 == null)) {
+      ListNode q = null;
+      if (l1 == null) {
+        q = l2;
+        l2 = l2.next;
+      } else if (l2 == null) {
+        q = l1;
         l1 = l1.next;
       } else {
-        p.next = l2;
+        if (l1.val < l2.val) {
+          q = l1;
+          l1 = l1.next;
+        } else {
+          q = l2;
+          l2 = l2.next;
+        }
+      }
+      q.next = null;
+      //
+      p.next = q;
+      p = p.next;
+    }
+    return ret.next;
+  }
+
+  public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    ListNode ret = new ListNode(0);
+    ListNode p = ret;
+    while (l1 != null && l2 != null) {
+      ListNode q = null;
+      if (l1.val < l2.val) {
+        q = l1;
+        l1 = l1.next;
+      } else {
+        q = l2;
         l2 = l2.next;
       }
+      p.next = q;
       p = p.next;
     }
     if (l1 != null) {
       p.next = l1;
-    }
-    if (l2 != null) {
+    } else {
       p.next = l2;
     }
-    return head.next;
+    return ret.next;
   }
 }
