c56b1e0d42e3a5b6cd8cb1d716fbd7c02c9c6290
huijunwu <huijun.wu.2010@gmail.com>
2016-01-18 08:27:00 -0700

    bit, Integer.bitCount, >>>/<</>>

--- Number of 1 Bits.java
+++ Number of 1 Bits.java
@@ -1,16 +1,16 @@
 public class Solution {
-  private boolean hammingWeight_test(int n, int offset) {
-    return (n & (1 << offset)) != 0;
-  }
-
   // you need to treat n as an unsigned value
   public int hammingWeight(int n) {
-    int count = 0;
+    int ret = 0;
     for (int i = 0; i < 32; i++) {
-      if (hammingWeight_test(n, i)) {
-        count++;
+      if (((n >>> i) & 1) != 0) {
+        ret++;
       }
     }
-    return count;
+    return ret;
+  }
+
+  public int hammingWeight_(int n) {
+    return Integer.bitCount(n);
   }
 }
