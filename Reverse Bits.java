571af61d0d6d06182266e4849383f9b0b442dcdb
huijunwu <huijun.wu.2010@gmail.com>
2016-01-18 19:21:42 -0700

    bit, Integer.reverse, int_offset_test/set

--- Reverse Bits.java
+++ Reverse Bits.java
@@ -1,4 +1,27 @@
 public class Solution {
+  // you need treat n as an unsigned value
+  public int reverseBits_(int n) {
+    return Integer.reverse(n);
+  }
+
+  public int reverseBits__(int n) {
+    int ret = 0;
+    for (int i = 0; i < 32; i++) {
+      int b = (n << i) & Integer.MIN_VALUE;
+      ret = (ret >>> 1) | b;
+    }
+    return ret;
+  }
+
+  public int reverseBits(int n) {
+    int ret = 0;
+    for (int i = 0; i < 32; i++) {
+      int b = (n >>> i) & 1;
+      ret = (ret << 1) | b;
+    }
+    return ret;
+  }
+
   private boolean reverseBits_test(int n, int offset) {
     return (n & (1 << offset)) != 0;
   }
@@ -8,7 +31,7 @@ public class Solution {
   }
 
   // you need treat n as an unsigned value
-  public int reverseBits(int n) {
+  public int reverseBits___(int n) {
     int ret = 0;
     for (int i = 0; i < 32; i++) {
       if (reverseBits_test(n, i)) {
