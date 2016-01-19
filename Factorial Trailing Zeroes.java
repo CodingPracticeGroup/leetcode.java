4a95578b5d305a62b59e388eabae72381c71f22d
huijunwu <huijun.wu.2010@gmail.com>
2016-01-18 17:53:48 -0700

    math

--- Factorial Trailing Zeroes.java
+++ Factorial Trailing Zeroes.java
@@ -1,6 +1,15 @@
 public class Solution {
   public int trailingZeroes(int n) {
     int ret = 0;
+    while (n > 0) {
+      n /= 5;
+      ret += n;
+    }
+    return ret;
+  }
+
+  public int trailingZeroes_(int n) {
+    int ret = 0;
     long pow = 5L;
     while (n >= pow) {
       ret += n / pow;
