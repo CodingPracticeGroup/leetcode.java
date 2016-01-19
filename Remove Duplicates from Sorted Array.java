5140866ab16512656edbd7f26a5a766a1b321869
huijunwu <huijun.wu.2010@gmail.com>
2016-01-18 17:28:18 -0700

    array, two pointers

--- Remove Duplicates from Sorted Array.java
+++ Remove Duplicates from Sorted Array.java
@@ -1,20 +1,11 @@
 public class Solution {
-  private int removeDuplicates_find(int[] nums, int j) {
-    int tmp = nums[j];
-    while (j < nums.length && nums[j] == tmp) {
-      j++;
-    }
-    return j;
-  }
-
   public int removeDuplicates(int[] nums) {
-    int i = 0, j = 0;
-    while (j < nums.length) {
-      int n = nums[j];
-      nums[i] = n;
-      i++;
-      j = removeDuplicates_find(nums, j);
+    int writer = 1;
+    for (int reader = 1; reader < nums.length; reader++) {
+      if (nums[reader] != nums[writer - 1]) {
+        nums[writer++] = nums[reader];
+      }
     }
-    return i;
+    return writer;
   }
 }
