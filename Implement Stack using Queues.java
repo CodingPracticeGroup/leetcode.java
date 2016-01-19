31968d253972885aae82d67024bee09b75380340
huijunwu <huijun.wu.2010@gmail.com>
2016-01-18 18:26:11 -0700

    stack, queue, Deque

--- Implement Stack using Queues.java
+++ Implement Stack using Queues.java
@@ -1,40 +1,38 @@
 class MyStack {
-  private Deque<Integer> q1 = new ArrayDeque<>();
-  private Deque<Integer> q2 = new ArrayDeque<>();
+  Deque<Integer> queue = new ArrayDeque<>();
+  Deque<Integer> queue_tmp = new ArrayDeque<>();
 
   // Push element x onto stack.
   public void push(int x) {
-    q1.offer(x);
+    queue.offer(x);
   }
 
   // Removes the element on top of the stack.
   public void pop() {
-    int count = q1.size();
-    for (int i = 1; i < count; i++) {
-      q2.offer(q1.poll());
+    while (queue.size() > 1) {
+      queue_tmp.offer(queue.poll());
     }
-    q1.poll();
-    while (!q2.isEmpty()) {
-      q1.offer(q2.poll());
+    queue.clear();
+    while (!queue_tmp.isEmpty()) {
+      queue.offer(queue_tmp.poll());
     }
   }
 
   // Get the top element.
   public int top() {
-    int count = q1.size();
-    for (int i = 1; i < count; i++) {
-      q2.offer(q1.poll());
+    while (queue.size() > 1) {
+      queue_tmp.offer(queue.poll());
     }
-    int ret = q1.poll();
-    q2.offer(ret);
-    while (!q2.isEmpty()) {
-      q1.offer(q2.poll());
+    int ret = queue.peek();
+    queue_tmp.offer(queue.poll());
+    while (!queue_tmp.isEmpty()) {
+      queue.offer(queue_tmp.poll());
     }
     return ret;
   }
 
   // Return whether the stack is empty.
   public boolean empty() {
-    return q1.isEmpty();
+    return queue.isEmpty();
   }
 }
