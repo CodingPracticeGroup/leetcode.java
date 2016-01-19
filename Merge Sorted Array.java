0b960a42627e5fc6634028f2fc119b5bcaec512a
huijunwu <huijun.wu.2010@gmail.com>
2016-01-18 19:08:09 -0700

    array, two pointers, reader and writer

--- Merge Sorted Array.java
+++ Merge Sorted Array.java
@@ -1,28 +1,20 @@
 public class Solution {
   public void merge(int[] nums1, int m, int[] nums2, int n) {
-    int writer = m + n - 1;
     int reader1 = m - 1;
     int reader2 = n - 1;
+    int writer1 = m + n - 1;
     while (reader1 >= 0 && reader2 >= 0) {
       if (nums1[reader1] < nums2[reader2]) {
-        nums1[writer] = nums2[reader2];
-        writer--;
-        reader2--;
+        nums1[writer1--] = nums2[reader2--];
       } else {
-        nums1[writer] = nums1[reader1];
-        writer--;
-        reader1--;
+        nums1[writer1--] = nums1[reader1--];
       }
     }
     while (reader1 >= 0) {
-      nums1[writer] = nums1[reader1];
-      writer--;
-      reader1--;
+      nums1[writer1--] = nums1[reader1--];
     }
     while (reader2 >= 0) {
-      nums1[writer] = nums2[reader2];
-      writer--;
-      reader2--;
+      nums1[writer1--] = nums2[reader2--];
     }
   }
 }
