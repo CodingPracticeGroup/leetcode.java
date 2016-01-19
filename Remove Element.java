83805655072e38a20bdfd1f320878c12302f6dbe
huijunwu <huijun.wu.2010@gmail.com>
2016-01-18 17:15:28 -0700

    array two pointers: reader and writer, for reader

--- Remove Element.java
+++ Remove Element.java
@@ -1,30 +1,11 @@
 public class Solution {
-  private int removeElement_findNextRead(int[] nums, int val, int read) {
-    read++;
-    while (read < nums.length) {
-      if (nums[read] != val) {
-        return read;
-      } else {
-        read++;
+  public int removeElement(int[] nums, int val) {
+    int writer = 0;
+    for (int reader = 0; reader < nums.length; reader++) {
+      if (nums[reader] != val) {
+        nums[writer++] = nums[reader];
       }
     }
-    return read;
-  }
-
-  public int removeElement(int[] nums, int val) {
-    int start = 0;
-    while (start < nums.length && nums[start] == val) {
-      start++;
-    }
-    if (start == nums.length) {
-      return 0;
-    }
-    int read = start, write = 0;
-    while (read < nums.length) {
-      nums[write] = nums[read];
-      write++;
-      read = removeElement_findNextRead(nums, val, read);
-    }
-    return write;
+    return writer;
   }
 }
