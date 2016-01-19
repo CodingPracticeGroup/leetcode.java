5a8e0e9cebdad6ca58ae654cb0f06fc46422ebc8
huijunwu <huijun.wu.2010@gmail.com>
2016-01-18 17:22:10 -0700

    array dp

--- House Robber.java
+++ House Robber.java
@@ -1,10 +1,15 @@
 public class Solution {
   public int rob(int[] nums) {
-    if (nums.length == 0)
+    if (nums.length == 0) {
       return 0;
-    int dp[] = new int[nums.length + 1];
-    for (int i = 0; i < nums.length; i++) {
-      dp[i] = Math.max(i - 1 >= 0 ? dp[i - 1] : 0, (i - 2 >= 0 ? dp[i - 2] : 0) + nums[i]);
+    } else if (nums.length == 1) {
+      return nums[0];
+    }
+    int dp[] = new int[nums.length];
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0], nums[1]);
+    for (int i = 2; i < nums.length; i++) {
+      dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
     }
     return dp[nums.length - 1];
   }
