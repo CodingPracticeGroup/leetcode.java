e6b2e912ebb1bca3829133eb36ca6485a187a417
huijunwu <huijun.wu.2010@gmail.com>
2016-01-20 10:00:57 -0700

    string, scan from tail to head

--- Length of Last Word.java
+++ Length of Last Word.java
@@ -1,10 +1,23 @@
 public class Solution {
-  public int lengthOfLastWord(String s) {
+  public int lengthOfLastWord_(String s) {
     s = s.trim();
     int i = s.length() - 1;
     while (i >= 0 && s.charAt(i) != ' ') {
       i--;
     }
     return s.length() - 1 - i;
+  }
+
+  public int lengthOfLastWord(String s) {
+    int ret = 0;
+    int idx = s.length() - 1;
+    while (idx >= 0 && s.charAt(idx) == ' ') {
+      idx--;
+    }
+    while (idx >= 0 && s.charAt(idx) != ' ') {
+      idx--;
+      ret++;
+    }
+    return ret;
   }
 }
