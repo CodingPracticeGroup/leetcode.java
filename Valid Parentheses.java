9dcfa95c13cec4c7ece178f6e1aa7583cf8ed96b
huijunwu <huijun.wu.2010@gmail.com>
2016-01-20 10:12:38 -0700

    string, stack

--- Valid Parentheses.java
+++ Valid Parentheses.java
@@ -1,29 +1,16 @@
 public class Solution {
-  private boolean isValid_match(char left, char right) {
-    if (left == '(' && right == ')') {
-      return true;
-    }
-    if (left == '[' && right == ']') {
-      return true;
-    }
-    if (left == '{' && right == '}') {
-      return true;
-    }
-    return false;
-  }
-
   public boolean isValid(String s) {
-    char[] c = s.toCharArray();
     Deque<Character> stack = new ArrayDeque<>();
-    for (int i = 0; i < c.length; i++) {
-      if (stack.isEmpty()) {
-        stack.push(c[i]);
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+      if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+        stack.pop();
+      } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+        stack.pop();
+      } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+        stack.pop();
       } else {
-        if (isValid_match(stack.peek(), c[i])) {
-          stack.pop();
-        } else {
-          stack.push(c[i]);
-        }
+        stack.push(c);
       }
     }
     return stack.isEmpty();
