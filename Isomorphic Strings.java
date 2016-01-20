7fb859477fa078010bf6fe8797be97733eae10ef
huijunwu <huijun.wu.2010@gmail.com>
2016-01-20 10:20:51 -0700

    string, HashMap, java8stream, stream_groupby_counting

--- Isomorphic Strings.java
+++ Isomorphic Strings.java
@@ -17,4 +17,23 @@ public class Solution {
         .collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).values()
         .stream().filter(x -> x > 1).count() == 0;
   }
+
+  private boolean mapping(String s, String t) {
+    Map<Character, Character> m = new HashMap<>();
+    for (int i = 0; i < s.length(); i++) {
+      char s_ = s.charAt(i);
+      char t_ = t.charAt(i);
+      if (!m.containsKey(s_)) {
+        m.put(s_, t_);
+      }
+      if (!m.get(s_).equals(t_)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public boolean isIsomorphic_(String s, String t) {
+    return mapping(s, t) && mapping(t, s);
+  }
 }
