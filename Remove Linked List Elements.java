c4a94ca52f65aaa17e9099f2ddfb384f97c18369
huijunwu <huijun.wu.2010@gmail.com>
2016-01-21 08:33:05 -0700

    singly linked list, myhead

--- Remove Linked List Elements.java
+++ Remove Linked List Elements.java
@@ -12,4 +12,17 @@ public class Solution {
     }
     return myhead.next;
   }
+
+  public ListNode removeElements_(ListNode head, int val) {
+    ListNode myhead = new ListNode(0);
+    myhead.next = head;
+    ListNode p = myhead;
+    while (p != null && p.next != null) {
+      while (p.next != null && p.next.val == val) {
+        p.next = p.next.next;
+      }
+      p = p.next;
+    }
+    return myhead.next;
+  }
 }
