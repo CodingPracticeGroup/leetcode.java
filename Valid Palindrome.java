03369a2b3b7cbc3db61c78c3b6735662668a5613
huijunwu <huijun.wu.2010@gmail.com>
2016-01-21 09:59:19 -0700

    character functions

--- Valid Palindrome.java
+++ Valid Palindrome.java
@@ -1,45 +1,20 @@
 public class Solution {
-    private boolean isPalindrome_alphanumeric (char c) {
-        if ('a'<=c && c <= 'z') {
-            return true;
-        }
-        if ('A'<=c && c <= 'Z') {
-            return true;
-        }
-        if ('0'<=c && c <= '9') {
-            return true;
-        }
+  public boolean isPalindrome(String s) {
+    int i = 0;
+    int j = s.length() - 1;
+    while (i < j) {
+      while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+        i++;
+      }
+      while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+        j--;
+      }
+      if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
         return false;
+      }
+      i++;
+      j--;
     }
-    
-    private boolean isPalindrome_match (char c1, char c2) {
-        if (c1==c2) {
-            return true;
-        }
-        if (Character.toUpperCase(c1)== Character.toUpperCase(c2)) {
-            return true;
-        }
-        return false;
-    }
-    public boolean isPalindrome(String s) {
-        int low = 0; 
-        int high = s.length()-1;
-        while (low<high) {
-            while (low<high && !isPalindrome_alphanumeric(s.charAt(low))) {
-                low ++;
-            }
-            while (low<high && !isPalindrome_alphanumeric(s.charAt(high))) {
-                high --;
-            }
-            if (low<high){
-                if ( !isPalindrome_match(s.charAt(low),s.charAt(high))) {
-                    return false;
-                } else {
-                    low++;
-                    high--;
-                }
-            } 
-        }
-        return true;
-    }
+    return true;
+  }
 }
