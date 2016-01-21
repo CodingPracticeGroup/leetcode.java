77ed546867e05d2b1d1946b5d19ebdede5c94f13
huijunwu <huijun.wu.2010@gmail.com>
2016-01-21 09:20:45 -0700

    string, string.startswith(str, offset)

--- Implement strStr().java
+++ Implement strStr().java
@@ -7,4 +7,16 @@ public class Solution {
     }
     return -1;
   }
+
+  public int strStr_(String haystack, String needle) {
+    if (haystack.length() == needle.length() && haystack.equals(needle)) {
+      return 0;
+    }
+    for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
+      if (haystack.startsWith(needle, i)) {
+        return i;
+      }
+    }
+    return -1;
+  }
 }
