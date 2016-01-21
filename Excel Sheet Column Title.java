f1be08cedd1276a83b87c480f797597280f4a37d
huijunwu <huijun.wu.2010@gmail.com>
2016-01-21 16:44:59 -0700

    math, (... _*26^3 + _*26^2 + _*26^1 + _*26^0 = n)/26

--- Excel Sheet Column Title.java
+++ Excel Sheet Column Title.java
@@ -1,5 +1,5 @@
 public class Solution {
-  public String convertToTitle(int n) {
+  public String convertToTitle_(int n) {
     StringBuilder sb = new StringBuilder();
     while (n > 0) {
       int t = n % 26;
@@ -12,5 +12,18 @@ public class Solution {
       }
     }
     return sb.reverse().toString();
+  }
+
+  public String convertToTitle(int n) {
+    StringBuilder sb = new StringBuilder();
+    // (... _*26^3 + _*26^2 + _*26^1 + _*26^0 = n)/26, after removing 26^0, then divide by 26,
+    // should still be ==
+    while (n > 0) {
+      int i = n % 26;
+      i = (i + 25) % 26;
+      sb.insert(0, (char) ('A' + i));
+      n = (n - i - 1) / 26;
+    }
+    return sb.toString();
   }
 }
