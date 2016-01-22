000e4667332b055d8f13d1674d494191d292f5b4
huijunwu <huijun.wu.2010@gmail.com>
2016-01-22 08:42:48 -0700

    array, dp, calculus

--- Best Time to Buy and Sell Stock II.java
+++ Best Time to Buy and Sell Stock II.java
@@ -1,5 +1,5 @@
 public class Solution {
-  public int maxProfit(int[] prices) {
+  public int maxProfit_(int[] prices) {
     if (prices.length == 0)
       return 0;
     int dp[] = new int[prices.length - 1];
@@ -7,5 +7,15 @@ public class Solution {
       dp[i] = Math.max(0, prices[i + 1] - prices[i]);
     }
     return Arrays.stream(dp).reduce(0, Integer::sum);
+  }
+
+  public int maxProfit(int[] prices) {
+    int ret = 0;
+    for (int i = 1; i < prices.length; i++) {
+      if (prices[i - 1] < prices[i]) {
+        ret += prices[i] - prices[i - 1]; // Calculus
+      }
+    }
+    return ret;
   }
 }
