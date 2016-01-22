01f672b45bff8e4721733109249a74f8f8576636
huijunwu <huijun.wu.2010@gmail.com>
2016-01-22 08:31:44 -0700

    math, bit, xor, array

--- Single Number.java
+++ Single Number.java
@@ -2,4 +2,12 @@ public class Solution {
   public int singleNumber(int[] nums) {
     return Arrays.stream(nums).reduce((x, y) -> x ^ y).getAsInt();
   }
+
+  public int singleNumber_(int[] nums) {
+    int ret = 0;
+    for (int i : nums) {
+      ret ^= i;
+    }
+    return ret;
+  }
 }
