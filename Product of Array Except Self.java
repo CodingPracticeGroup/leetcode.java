4d0199d8a20ec18a7f40c2f46917cc4f74ce26f0
huijunwu <huijun.wu.2010@gmail.com>
2016-01-22 08:51:18 -0700

    array, left->right, right->left, two times, dp using ret[]

--- Product of Array Except Self.java
+++ Product of Array Except Self.java
@@ -1,15 +1,14 @@
 public class Solution {
   public int[] productExceptSelf(int[] nums) {
     int[] ret = new int[nums.length];
-    ret[0] = nums[0];
+    ret[0] = 1;
     for (int i = 1; i < nums.length; i++) {
-      ret[i] = ret[i - 1] * nums[i];
+      ret[i] = ret[i - 1] * nums[i - 1];
     }
-    int rl = 1;
-    for (int i = nums.length - 1; i >= 0; i--) {
-      if (i + 1 < nums.length)
-        rl *= nums[i + 1];
-      ret[i] = (i - 1 >= 0 ? ret[i - 1] : 1) * rl;
+    int p = 1;
+    for (int i = nums.length - 2; i >= 0; i--) {
+      p *= nums[i + 1];
+      ret[i] *= p;
     }
     return ret;
   }
