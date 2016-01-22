755edb58cf51617fb154216d38e4db3339d826c6
huijunwu <huijun.wu.2010@gmail.com>
2016-01-21 17:18:43 -0700

    string.split("\\."), java8stream count(), compare two arrays

--- Compare Version Numbers.java
+++ Compare Version Numbers.java
@@ -25,4 +25,34 @@ public class Solution {
     }
     return 0;
   }
+
+  public int compareVersion_(String version1, String version2) {
+    long c1 = version1.chars().filter(x -> x == '.').count();
+    long c2 = version2.chars().filter(x -> x == '.').count();
+    if (c1 < c2) {
+      StringBuilder sb = new StringBuilder(version1);
+      for (int i = 0; i < c2 - c1; i++) {
+        sb.append(".0");
+      }
+      version1 = sb.toString();
+    } else if (c1 > c2) {
+      StringBuilder sb = new StringBuilder(version2);
+      for (int i = 0; i < c1 - c2; i++) {
+        sb.append(".0");
+      }
+      version2 = sb.toString();
+    }
+    String[] v1 = version1.split("\\.");
+    String[] v2 = version2.split("\\.");
+    for (int i = 0; i < v1.length && i < v2.length; i++) {
+      int v1_ = Integer.parseInt(v1[i]);
+      int v2_ = Integer.parseInt(v2[i]);
+      if (v1_ < v2_) {
+        return -1;
+      } else if (v1_ > v2_) {
+        return 1;
+      }
+    }
+    return 0;
+  }
 }
