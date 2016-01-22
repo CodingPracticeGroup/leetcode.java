208fcd879cd8f6356ebbfbc8a4e19ae3ef317e06
huijunwu <huijun.wu.2010@gmail.com>
2016-01-22 12:22:29 -0700

    tree, dp, combination: multiply

--- Unique Binary Search Trees.java
+++ Unique Binary Search Trees.java
@@ -3,9 +3,11 @@ public class Solution {
     int dp[] = new int[n + 1];
     Arrays.fill(dp, 0);
     dp[0] = 1;
-    for (int i = 1; i <= n; i++) { // node count ~ value array [1,i]
-      for (int root = 1; root <= i; root++) { // root value
-        dp[i] += dp[root - 1] * dp[i - root];
+    for (int i = 1; i <= n; i++) { // len
+      for (int root = 0; root < i; root++) { // where root
+        int left = root - 0;
+        int right = i - root - 1;
+        dp[i] += dp[left] * dp[right];
       }
     }
     return dp[n];
