ab56b1527fa9608c5a06663b41a9dce31d840916
huijunwu <huijun.wu.2010@gmail.com>
2016-01-22 08:37:41 -0700

    math, bit, integer.lowestonebit(), mask categerization

--- Single Number III.java
+++ Single Number III.java
@@ -1,19 +1,19 @@
 public class Solution {
   public int[] singleNumber(int[] nums) {
-    int xor = 0; // x ^ 0 = x
-    for (int n : nums) {
-      xor ^= n;
+    int xor = 0;
+    for (int i : nums) {
+      xor ^= i;
     }
-    int mask = Integer.lowestOneBit(xor); //
-    int xor_mask0 = 0;
-    int xor_mask1 = 0;
-    for (int n : nums) {
-      if ((n & mask) == 0) {
-        xor_mask0 ^= n; // x ^ x = 0
+    int mask = Integer.lowestOneBit(xor);
+    int xor_grp1 = 0;
+    int xor_grp2 = 0;
+    for (int i : nums) {
+      if ((mask & i) == 0) {
+        xor_grp1 ^= i;
       } else {
-        xor_mask1 ^= n; // x ^ x = 0
+        xor_grp2 ^= i;
       }
     }
-    return new int[] {xor_mask0, xor_mask1};
+    return new int[] {xor_grp1, xor_grp2};
   }
 }
