a9d1863adf3ac92780a4ed5d5e0f565f8ab982a2
huijunwu <huijun.wu.2010@gmail.com>
2016-01-22 09:17:12 -0700

    math

--- Bulb Switcher.java
+++ Bulb Switcher.java
@@ -1,5 +1,10 @@
 public class Solution {
   public int bulbSwitch(int n) {
+    /*
+     * 对于第i栈灯泡，当i的因子个数为奇数时，最终会保持点亮状态，例如9的因子为1，3，9
+	 * 而当i的因子个数为偶数时，最终会保持熄灭状态，例如8的因子为：1，2，4，8
+     * 当且仅当i为完全平方数时，其因子个数为奇数
+     */
     return (int) Math.sqrt(n);
   }
 }
