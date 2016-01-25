08cfeac7b3a7ca18e34ca59a793bc252aff813cc
huijunwu <huijun.wu.2010@gmail.com>
2016-01-25 11:03:24 -0700

    array, low high binary search

--- Find Minimum in Rotated Sorted Array.java
+++ Find Minimum in Rotated Sorted Array.java
@@ -12,7 +12,23 @@ public class Solution {
     }
   }
 
-  public int findMin(int[] nums) {
+  public int findMin_(int[] nums) {
     return findMin_range(nums, 0, nums.length);
+  }
+
+  public int findMin(int[] nums) {
+    int low = 0;
+    int high = nums.length - 1;
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      if (nums[mid] > nums[high]) {
+        low = mid + 1;
+      } else if (nums[low] > nums[mid]) {
+        high = mid;
+      } else {
+        return nums[low];
+      }
+    }
+    return 0;
   }
 }
