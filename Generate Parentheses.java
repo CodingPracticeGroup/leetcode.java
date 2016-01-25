290d246ba41cdefd7957999e906a84b14c5d278c
huijunwu <huijun.wu.2010@gmail.com>
2016-01-25 10:07:39 -0700

    recursion, combination, dfs, stringbuilder explore

--- Generate Parentheses.java
+++ Generate Parentheses.java
@@ -12,4 +12,28 @@ public class Solution {
     }
     return new ArrayList<String>(ret);
   }
+
+  public ArrayList<String> generateParenthesis_(int n) {
+    ArrayList<String> ret = new ArrayList<String>();
+    StringBuilder str = new StringBuilder();
+    dfs(ret, str, 0, 0, n);
+    return ret;
+  }
+
+  public void dfs(ArrayList<String> ret, StringBuilder str, int left, int right, int n) {
+    if (left == n && right == n) {
+      ret.add(str.toString());
+      return;
+    }
+    if (left < n) {
+      str.append('(');
+      dfs(ret, str, left + 1, right, n);
+      str.deleteCharAt(str.length() - 1);
+    }
+    if (right < left) {
+      str.append(')');
+      dfs(ret, str, left, right + 1, n);
+      str.deleteCharAt(str.length() - 1);
+    }
+  }
 }
