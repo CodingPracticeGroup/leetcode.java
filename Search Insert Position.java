1caec52bcb43e09d3135ea3d6c3538c387fc40ff
huijunwu <huijun.wu.2010@gmail.com>
2016-01-25 09:15:39 -0700

    arrary, low high binary search

--- Search Insert Position.java
+++ Search Insert Position.java
@@ -4,7 +4,33 @@ public class Solution {
     while (low <= high) {
       int mid = (low + high) / 2;
       if (target < nums[mid]) {
-        high = mid - 1;
+        high = mid - 1; // move
+      } else if (nums[mid] < target) {
+        low = mid + 1; // move
+      } else {
+        return mid;
+      }
+    }
+    return low;
+  }
+
+  public int searchInsert_(int[] nums, int target) {
+    int low = 0;
+    int high = nums.length - 1;
+    while (low <= high) {
+      // low <= insertion <= high
+      if (low == high) {
+        if (nums[low] < target) {
+          return low + 1;
+        } else if (target < nums[low]) {
+          return low;
+        } else {
+          return low;
+        }
+      }
+      int mid = low + (high - low) / 2;
+      if (target < nums[mid]) {
+        high = mid; // does not move
       } else if (nums[mid] < target) {
         low = mid + 1;
       } else {
