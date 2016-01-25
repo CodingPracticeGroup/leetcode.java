97ba89e505316d3c6e6f2a056d70cd38bd49017e
huijunwu <huijun.wu.2010@gmail.com>
2016-01-25 11:10:31 -0700

    bit, math, mirror

--- Gray Code.java
+++ Gray Code.java
@@ -1,22 +1,15 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Solution {
-  private void grayCode_set_bit(int[] board, int idx, int bit) {
-    board[idx] = board[idx] | 1 << bit;
-  }
-
   public List<Integer> grayCode(int n) {
-    int[] board = new int[(int) Math.pow(2, n)];
-    Arrays.fill(board, 0);
-    for (int bitidx = 0; bitidx < n; bitidx++) {
-      int len = (int) Math.pow(2, bitidx);
-      for (int i = 0; i < len; i++) {
-        board[len * 2 - 1 - i] = board[i];
-        grayCode_set_bit(board, len * 2 - 1 - i, bitidx);
+    List<Integer> ret = new ArrayList<>();
+    ret.add(0);
+    for (int i = 0; i < n; i++) {
+      List<Integer> mirror = new ArrayList<>(ret);
+      Collections.reverse(mirror);
+      for (int j = 0; j < mirror.size(); j++) {
+        mirror.set(j, mirror.get(j) | (1 << i));
       }
+      ret.addAll(mirror);
     }
-    return Arrays.stream(board).boxed().collect(Collectors.toList());
+    return ret;
   }
 }
