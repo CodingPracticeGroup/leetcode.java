8196a1bd001436610e0fed49e70ee95402740105
huijunwu <huijun.wu.2010@gmail.com>
2016-01-25 09:49:28 -0700

    tree, inorder interation with count

--- Kth Smallest Element in a BST.java
+++ Kth Smallest Element in a BST.java
@@ -1,6 +1,24 @@
 public class Solution {
   public int kthSmallest(TreeNode root, int k) {
     Deque<TreeNode> stack = new ArrayDeque<>();
+    TreeNode p = root;
+    while (!stack.isEmpty() || p != null) {
+      while (p != null) {
+        stack.push(p);
+        p = p.left;
+      }
+      p = stack.pop();
+      k--;
+      if (k == 0) {
+        return p.val;
+      }
+      p = p.right;
+    }
+    return 0;
+  }
+
+  public int kthSmallest_(TreeNode root, int k) {
+    Deque<TreeNode> stack = new ArrayDeque<>();
     for (TreeNode tn = root; tn != null; tn = tn.left) {
       stack.push(tn);
     }
