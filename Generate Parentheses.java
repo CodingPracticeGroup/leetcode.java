ecca81c17a76bc05d42066df3bc9dff6744300b3
huijunwu <huijun.wu.2010@gmail.com>
2016-01-25 10:00:47 -0700

    recursion, combination

--- Generate Parentheses.java
+++ Generate Parentheses.java
@@ -1,24 +1,15 @@
 public class Solution {
-  private String generateParenthesis_insert(String str, int pos) {
-    return new String(str.substring(0, pos) + "()" + str.substring(pos));
-  }
-
-  private Set<String> generateParenthesis_hashset(int n) {
+  public List<String> generateParenthesis(int n) {
     Set<String> ret = new HashSet<>();
     if (n == 1) {
       ret.add("()");
-      return ret;
+      return new ArrayList<String>(ret);
     }
-    Set<String> l = generateParenthesis_hashset(n - 1);
-    for (String s : l) {
+    for (String s : generateParenthesis(n - 1)) {
       for (int i = 0; i <= s.length(); i++) {
-        ret.add(generateParenthesis_insert(s, i));
+        ret.add(s.substring(0, i) + "()" + s.substring(i));
       }
     }
-    return ret;
-  }
-
-  public List<String> generateParenthesis(int n) {
-    return new ArrayList<String>(generateParenthesis_hashset(n));
+    return new ArrayList<String>(ret);
   }
 }
