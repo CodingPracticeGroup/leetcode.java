f7969000f0f125f45c797eb8a18139d8bf897974
huijunwu <huijun.wu.2010@gmail.com>
2016-01-26 10:31:15 -0700

    matrix, mark first row/col

--- Set Matrix Zeroes.java
+++ Set Matrix Zeroes.java
@@ -1,44 +1,51 @@
 public class Solution {
   public void setZeroes(int[][] matrix) {
-    boolean firstRow0 = false;
-    for (int i = 0; i < matrix[0].length; i++) {
+    int m = matrix.length;
+    int n = matrix[0].length;
+
+    boolean firstRowZero = false;
+    for (int i = 0; i < n; i++) {
       if (matrix[0][i] == 0) {
-        firstRow0 = true;
+        firstRowZero = true;
         break;
       }
     }
-    boolean firstCol0 = false;
-    for (int i = 0; i < matrix.length; i++) {
+
+    boolean firstColZero = false;
+    for (int i = 0; i < m; i++) {
       if (matrix[i][0] == 0) {
-        firstCol0 = true;
+        firstColZero = true;
         break;
       }
     }
-    for (int i = 1; i < matrix.length; i++) {
-      for (int j = 1; j < matrix[0].length; j++) {
+
+    for (int i = 1; i < m; i++) {
+      for (int j = 1; j < n; j++) {
         if (matrix[i][j] == 0) {
           matrix[0][j] = 0;
           matrix[i][0] = 0;
         }
       }
     }
-    for (int i = 1; i < matrix.length; i++) {
+
+    for (int i = 1; i < m; i++) {
       if (matrix[i][0] == 0) {
         Arrays.fill(matrix[i], 0);
       }
     }
-    for (int i = 1; i < matrix[0].length; i++) {
+    for (int i = 1; i < n; i++) {
       if (matrix[0][i] == 0) {
-        for (int j = 0; j < matrix.length; j++) {
+        for (int j = 1; j < m; j++) {
           matrix[j][i] = 0;
         }
       }
     }
-    if (firstRow0) {
+
+    if (firstRowZero) {
       Arrays.fill(matrix[0], 0);
     }
-    if (firstCol0) {
-      for (int i = 0; i < matrix.length; i++) {
+    if (firstColZero) {
+      for (int i = 0; i < m; i++) {
         matrix[i][0] = 0;
       }
     }
