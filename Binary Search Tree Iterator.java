a2b3f45342406a24ef36c0cabad07ac27675bc3c
huijunwu <huijun.wu.2010@gmail.com>
2016-01-26 10:19:32 -0700

    tree, bst, stack iteration, for-3 statements, process when pop

--- Binary Search Tree Iterator.java
+++ Binary Search Tree Iterator.java
@@ -15,9 +15,38 @@ public class BSTIterator {
   /** @return the next smallest number */
   public int next() {
     TreeNode tn = stack.pop();
-    int ret = tn.val;
     for (TreeNode p = tn.right; p != null; p = p.left)
       stack.push(p);
-    return ret;
+    return tn.val;
+  }
+}
+
+public class BSTIterator {
+
+  Deque<TreeNode> stack = new ArrayDeque<>();
+
+  public BSTIterator(TreeNode root) {
+    while (root != null) {
+      stack.push(root);
+      root = root.left;
+    }
+  }
+
+  /** @return whether we have a next smallest number */
+  public boolean hasNext() {
+    return !stack.isEmpty();
+  }
+
+  /** @return the next smallest number */
+  public int next() {
+    TreeNode p = stack.pop();
+
+    TreeNode q = p.right;
+    while (q != null) {
+      stack.push(q);
+      q = q.left;
+    }
+
+    return p.val;
   }
 }
