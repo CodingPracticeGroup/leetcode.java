e186fb1dd6a2f6752288584f546a9b58f6289c82
huijunwu <huijun.wu.2010@gmail.com>
2016-01-26 02:11:07 -0700

    matrix, swap rotate

--- Rotate Image.java
+++ Rotate Image.java
@@ -1,26 +1,21 @@
 public class Solution {
-  private void rotate_updown(int[][] matrix) {
-    for (int i = 0; i < matrix.length / 2; i++) {
-      for (int j = 0; j < matrix.length; j++) {
-        int tmp = matrix[i][j];
-        matrix[i][j] = matrix[matrix.length - 1 - i][j];
-        matrix[matrix.length - 1 - i][j] = tmp;
-      }
-    }
-  }
-
-  private void rotate_diag(int[][] matrix) {
-    for (int i = 0; i < matrix.length; i++) {
-      for (int j = i; j < matrix.length; j++) {
-        int tmp = matrix[i][j];
-        matrix[i][j] = matrix[j][i];
-        matrix[j][i] = tmp;
-      }
-    }
+  private void swap(int[][] matrix, int i1, int j1, int i2, int j2) {
+    int tmp = matrix[i1][j1];
+    matrix[i1][j1] = matrix[i2][j2];
+    matrix[i2][j2] = tmp;
   }
 
   public void rotate(int[][] matrix) {
-    rotate_updown(matrix);
-    rotate_diag(matrix);
+    int n = matrix.length;
+    for (int i = 0; i < n; i++) {
+      for (int j = i; j < n; j++) {
+        swap(matrix, i, j, j, i);
+      }
+    }
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n / 2; j++) {
+        swap(matrix, i, j, i, n - 1 - j);
+      }
+    }
   }
 }
