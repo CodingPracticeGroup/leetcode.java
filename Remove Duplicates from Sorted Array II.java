074ed8ceb97930c8adf7e94813664a51bf9c98d5
huijunwu <huijun.wu.2010@gmail.com>
2016-01-26 12:45:35 -0700

    array, two pointers, scan with reader, writer

--- Remove Duplicates from Sorted Array II.java
+++ Remove Duplicates from Sorted Array II.java
@@ -1,23 +1,13 @@
 public class Solution {
   public int removeDuplicates(int[] nums) {
-    int i = 0; // writer
-    int j = 0; // reader
-    while (j < nums.length) {
-      if (j < nums.length && j + 1 < nums.length && nums[j] == nums[j + 1]) {
-        nums[i] = nums[j];
-        nums[i + 1] = nums[j + 1];
-        i += 2;
-        j += 2;
+    int writer = 2;
+    for (int reader = 2; reader < nums.length; reader++) {
+      if (nums[writer - 2] == nums[reader] && nums[writer - 1] == nums[reader]) {
+        // do nothing
       } else {
-        nums[i] = nums[j];
-        i += 1;
-        j += 1;
-      }
-      int k = j - 1;
-      while (j < nums.length && nums[k] == nums[j]) {
-        j++;
+        nums[writer++] = nums[reader];
       }
     }
-    return i;
+    return writer;
   }
 }
