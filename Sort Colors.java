d0d29580832db01c2483719463a1469ea3b318fa
huijunwu <huijun.wu.2010@gmail.com>
2016-01-26 01:49:28 -0700

    array, reader scan with writer swap

--- Sort Colors.java
+++ Sort Colors.java
@@ -1,5 +1,5 @@
 public class Solution {
-  public void sortColors(int[] nums) {
+  public void sortColors_(int[] nums) {
     int i = 0, j = 0; // insert pos
     int k = 0; // scan head
     while (k < nums.length) {
@@ -17,6 +17,37 @@ public class Solution {
         j++;
       }
       k++;
+    }
+  }
+
+  private void swap(int[] nums, int i, int j) {
+    int tmp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = tmp;
+  }
+
+  public void sortColors(int[] nums) {
+    int writer0 = 0;
+    while (writer0 < nums.length && nums[writer0] == 0) {
+      writer0++;
+    }
+    int writer2 = nums.length - 1;
+    while (writer2 >= 0 && nums[writer2] == 2) {
+      writer2--;
+    }
+    for (int reader = writer0; reader <= writer2; reader++) {
+      if (nums[reader] == 2) {
+        swap(nums, reader, writer2--);
+        while (writer2 >= 0 && nums[writer2] == 2) {
+          writer2--;
+        }
+      }
+      if (nums[reader] == 0) {
+        swap(nums, reader, writer0++);
+        while (writer0 < reader && nums[writer0] == 0) {
+          writer0++;
+        }
+      }
     }
   }
 }
