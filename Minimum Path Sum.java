ff11b10acf018b6eab403c2740e13c7f3479ffc8
huijunwu <huijun.wu.2010@gmail.com>
2016-01-26 01:33:50 -0700

    matrix dp

--- Minimum Path Sum.java
+++ Minimum Path Sum.java
@@ -15,4 +15,19 @@ public class Solution {
     }
     return grid[m - 1][n - 1];
   }
+
+  public int minPathSum_(int[][] grid) {
+    for (int i = 1; i < grid.length; i++) {
+      grid[i][0] = grid[i - 1][0] + grid[i][0];
+    }
+    for (int i = 1; i < grid[0].length; i++) {
+      grid[0][i] = grid[0][i - 1] + grid[0][i];
+    }
+    for (int i = 1; i < grid.length; i++) {
+      for (int j = 1; j < grid[0].length; j++) {
+        grid[i][j] = Math.min(grid[i - 1][j] + grid[i][j], grid[i][j - 1] + grid[i][j]);
+      }
+    }
+    return grid[grid.length - 1][grid[0].length - 1];
+  }
 }
