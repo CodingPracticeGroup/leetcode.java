44c2b4a977c675431f35379a0667ce126176595e
huijunwu <huijun.wu.2010@gmail.com>
2016-01-26 01:20:33 -0700

    dfs stack with bt, dfs with recursion

--- Permutations.java
+++ Permutations.java
@@ -6,7 +6,7 @@ public class Solution {
     } else {
       for (Integer i : idx) {
         stack.push(orig_nums[i]);
-        Set<Integer> forchild = new HashSet<>(idx);
+        Set<Integer> forchild = new HashSet<>(idx); // necessary, otherwise Line 7: java.util.ConcurrentModificationException
         forchild.remove(i);
         permute_bt(orig_nums, forchild, ret, stack);
         stack.pop();
@@ -19,5 +19,28 @@ public class Solution {
     permute_bt(nums, IntStream.range(0, nums.length).boxed().collect(Collectors.toSet()), ret,
         new ArrayDeque<Integer>());
     return ret;
+  }
+
+  private Set<List<Integer>> p(int[] nums, int start, int end) {
+    Set<List<Integer>> ret = new HashSet<>();
+    if (start + 1 == end) {
+      List<Integer> l = new ArrayList<>();
+      l.add(nums[start]);
+      ret.add(l);
+      return ret;
+    }
+    Set<List<Integer>> s = p(nums, start + 1, end);
+    for (List<Integer> l : s) {
+      for (int i = 0; i <= l.size(); i++) {
+        List<Integer> ll = new ArrayList<>(l);
+        ll.add(i, nums[start]);
+        ret.add(ll);
+      }
+    }
+    return ret;
+  }
+
+  public List<List<Integer>> permute_(int[] nums) {
+    return new ArrayList<List<Integer>>(p(nums, 0, nums.length));
   }
 }
