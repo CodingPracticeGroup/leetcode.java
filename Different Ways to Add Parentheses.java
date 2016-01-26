730f0469a1fe39b31032f1163bbcecd7ce454843
huijunwu <huijun.wu.2010@gmail.com>
2016-01-26 12:39:06 -0700

    string, pick root in string, recursion return, dp mem

--- Different Ways to Add Parentheses.java
+++ Different Ways to Add Parentheses.java
@@ -37,3 +37,42 @@ public class Solution {
     return diffWaysToCompute_bi_re(input, 0, input.length(), new HashMap<String, List<Integer>>());
   }
 }
+-----------
+public class Solution {
+  private List<Integer> d(String input, int start, int end) {
+    List<Integer> ret = new ArrayList<>();
+    for (int i = start; i < end; i++) {
+      if (!('0' <= input.charAt(i) && input.charAt(i) <= '9')) {
+        List<Integer> left = d(input, start, i);
+        List<Integer> right = d(input, i + 1, end);
+        if (input.charAt(i) == '+') {
+          for (int m : left) {
+            for (int n : right) {
+              ret.add(m + n);
+            }
+          }
+        } else if (input.charAt(i) == '-') {
+          for (int m : left) {
+            for (int n : right) {
+              ret.add(m - n);
+            }
+          }
+        } else if (input.charAt(i) == '*') {
+          for (int m : left) {
+            for (int n : right) {
+              ret.add(m * n);
+            }
+          }
+        }
+      }
+    }
+    if (ret.isEmpty()) {
+      ret.add(Integer.valueOf(input.substring(start, end)));
+    }
+    return ret;
+  }
+
+  public List<Integer> diffWaysToCompute(String input) {
+    return d(input, 0, input.length());
+  }
+}
