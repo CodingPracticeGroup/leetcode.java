de76af6b4b5a29645119904e66da785d3f49affd
huijunwu <huijun.wu.2010@gmail.com>
2016-01-26 15:18:36 -0700

    array, recursion with range, Arrays.binarySearch(arr, start, to, key)

--- Search in Rotated Sorted Array II.java
+++ Search in Rotated Sorted Array II.java
@@ -32,3 +32,33 @@ public class Solution {
     return search_(nums, target, 0, nums.length - 1);
   }
 }
+----------------
+public class Solution {
+  private boolean s(int[] nums, int target, int low, int high) {
+    if (high < low) {
+      return false;
+    }
+    int mid = low + (high - low) / 2;
+    if (nums[low] == nums[high]) {
+      if (nums[mid] == target) {
+        return true;
+      } else {
+        return s(nums, target, low, mid - 1) || s(nums, target, mid + 1, high);
+      }
+    } else {
+      if (nums[mid] == target) {
+        return true;
+      } else if (nums[low] <= target && target <= nums[mid]) { // benefit from sorted subarray
+        return Arrays.binarySearch(nums, low, mid + 1, target) >= 0;
+      } else if (nums[mid] <= target && target <= nums[high]) { // benefit from sorted subarray
+        return Arrays.binarySearch(nums, mid, high + 1, target) >= 0;
+      } else {
+        return s(nums, target, low, mid - 1) || s(nums, target, mid + 1, high);
+      }
+    }
+  }
+
+  public boolean search(int[] nums, int target) {
+    return s(nums, target, 0, nums.length - 1);
+  }
+}
