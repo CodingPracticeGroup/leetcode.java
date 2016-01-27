607276b0514d03c9dddc026d9713f84a42b02293
huijunwu <huijun.wu.2010@gmail.com>
2016-01-27 11:04:21 -0700

    circle to arrays, dp

--- House Robber II.java
+++ House Robber II.java
@@ -25,3 +25,31 @@ public class Solution {
     return Math.max(rob_internal(nums, 0, nums.length - 1), rob_internal(nums, 1, nums.length));
   }
 }
+---------
+public class Solution {
+  public int rob(int[] nums) {
+    if (nums.length == 0) {
+      return 0;
+    }
+    if (nums.length == 1) {
+      return nums[0];
+    }
+    if (nums.length == 2) {
+      return Math.max(nums[0], nums[1]);
+    }
+    int dp[] = new int[nums.length - 1];
+    //
+    for (int i = 0; i < nums.length - 1; i++) {
+      dp[i] = Math.max((i - 1 >= 0 ? dp[i - 1] : 0), (i - 2 >= 0 ? dp[i - 2] : 0) + nums[i]);
+    }
+    int r1 = dp[nums.length - 2];
+    //
+    for (int i = 1; i < nums.length; i++) {
+      int j = i - 1;
+      dp[j] = Math.max((j - 1 >= 0 ? dp[j - 1] : 0), (j - 2 >= 0 ? dp[j - 2] : 0) + nums[i]);
+    }
+    int r2 = dp[nums.length - 2];
+    //
+    return Math.max(r1, r2);
+  }
+}
