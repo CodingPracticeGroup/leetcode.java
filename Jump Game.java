b12481dfa7e5c013c963c86dc45931bdae9486bd
huijunwu <huijun.wu.2010@gmail.com>
2016-01-28 00:06:08 -0700

    array two pointers anchor prober greedy scan

--- Jump Game.java
+++ Jump Game.java
@@ -16,3 +16,23 @@ public class Solution {
     return false;
   }
 }
+----------
+public class Solution {
+  public boolean canJump(int[] nums) {
+    int prober = 0 + nums[0];
+    int anchor = 0;
+    while (anchor < nums.length - 1) {
+      int tmp_prober = 0;
+      for (int i = anchor + 1; i < nums.length && i <= prober; i++) {
+        tmp_prober = Math.max(tmp_prober, i + nums[i]);
+      }
+      if (tmp_prober == 0) {
+        return false;
+      }
+
+      anchor = prober;
+      prober = tmp_prober;
+    }
+    return true;
+  }
+}
