fcba8e1d067b4c3947c46a008dc2756dde78ac3c
huijunwu <huijun.wu.2010@gmail.com>
2016-01-27 22:39:55 -0700

    math, bit, >>/<<, mask

--- Bitwise AND of Numbers Range.java
+++ Bitwise AND of Numbers Range.java
@@ -9,3 +9,18 @@ public class Solution {
     return m << i;
   }
 }
+--------
+public class Solution {
+  public int rangeBitwiseAnd(int m, int n) {
+    if (Integer.highestOneBit(m) != Integer.highestOneBit(n)) {
+      return 0;
+    }
+    for (int i = 0; i < 32; i++) {
+      if ((m >> i) == (n >> i)) {
+        return (m >> i) << i;
+      }
+    }
+    return 0;
+  }
+}
+------
