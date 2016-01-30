3d49eb515a55ceb3834377676289b3b3f468ff38
huijunwu <huijun.wu.2010@gmail.com>
2016-01-30 13:40:25 -0700

    matrix dfs recursion with marker

--- Number of Islands.java
+++ Number of Islands.java
@@ -32,3 +32,42 @@ public class Solution {
     return count;
   }
 }
+-------------
+public class Solution {
+  private void mark2(char[][] grid, int i, int j) {
+    grid[i][j] = '2';
+    if (i - 1 >= 0 && grid[i - 1][j] == '1') {
+      mark2(grid, i - 1, j);
+    }
+    if (j - 1 >= 0 && grid[i][j - 1] == '1') {
+      mark2(grid, i, j - 1);
+    }
+    if (i + 1 < grid.length && grid[i + 1][j] == '1') {
+      mark2(grid, i + 1, j);
+    }
+    if (j + 1 < grid[0].length && grid[i][j + 1] == '1') {
+      mark2(grid, i, j + 1);
+    }
+  }
+
+  public int numIslands(char[][] grid) {
+    int ret = 0;
+    int m = grid.length;
+    if (m == 0) {
+      return ret;
+    }
+    int n = grid[0].length;
+    if (n == 0) {
+      return ret;
+    }
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (grid[i][j] == '1') {
+          ret++;
+          mark2(grid, i, j);
+        }
+      }
+    }
+    return ret;
+  }
+}
