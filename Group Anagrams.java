65c377394c5fbc716adab3fd12d9fc1c5191bfb6
huijunwu <huijun.wu.2010@gmail.com>
2016-01-30 15:15:09 -0700

    sort inside string as eigenvalue, java8stream pick map.values() as list

--- Group Anagrams.java
+++ Group Anagrams.java
@@ -13,3 +13,37 @@ public class Solution {
     return ret;
   }
 }
+-------------
+/****************
+this cannot pass large set
+****************/
+public class Solution {
+  public List<List<String>> groupAnagrams(String[] strs) {
+    Map<Map<Integer, Long>, List<String>> es = new HashMap<>();
+    for (String s : strs) {
+      Map<Integer, Long> m = s.chars().boxed()
+          .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+      if (!es.containsKey(m)) {
+        es.put(m, new ArrayList<String>());
+      }
+      es.get(m).add(s);
+    }
+    for (List<String> l : es.values()) {
+      Collections.sort(l);
+    }
+    return new ArrayList<List<String>>(es.values());
+  }
+}
+---------------
+public class Solution {
+  public List<List<String>> groupAnagrams(String[] strs) {
+    return Arrays.stream(strs).collect(Collectors.groupingBy(x -> {
+      char[] ca = x.toCharArray();
+      Arrays.sort(ca);
+      return new String(ca);
+    })).values().stream().map(x -> {
+      Collections.sort(x);
+      return x;
+    }).collect(Collectors.toList());
+  }
+}
