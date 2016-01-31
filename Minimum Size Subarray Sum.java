f821bffeebc2c8d8fb79e9979ea564f5b5e4e102
huijunwu <huijun.wu.2010@gmail.com>
2016-01-30 17:10:52 -0700

    scan array with two pointers, move i, j follows i, maintain window sum

--- Minimum Size Subarray Sum.java
+++ Minimum Size Subarray Sum.java
@@ -17,3 +17,23 @@ public class Solution {
     return min == nums.length + 1 ? 0 : min;
   }
 }
+-------------
+public class Solution {
+  public int minSubArrayLen(int s, int[] nums) {
+    int ret = Integer.MAX_VALUE;
+    int last = 0;
+    int sum = 0;
+    for (int i = 0; i < nums.length; i++) {
+      sum += nums[i];
+      while (sum >= s) {
+        ret = Math.min(ret, i - last + 1);
+        sum -= nums[last++];
+      }
+    }
+    if (ret == Integer.MAX_VALUE) {
+      return 0;
+    } else {
+      return ret;
+    }
+  }
+}
