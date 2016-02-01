7bde404336128244a8b747e48ba967883b5e0ba7
huijunwu <huijun.wu.2010@gmail.com>
2016-01-31 22:14:32 -0700

    use inorder as base, use postorder to determine root, recursion division on inroder array

--- Construct Binary Tree from Inorder and Postorder Traversal.java
+++ Construct Binary Tree from Inorder and Postorder Traversal.java
@@ -23,3 +23,25 @@ public class Solution {
     return buildTree_range(inorder, 0, inorder.length, postorder, 0, postorder.length);
   }
 }
+-----------
+public class Solution {
+  private TreeNode b(int[] in, int i_s, int i_e, int[] post, int p_s, int p_e) {
+    if (i_s >= i_e || p_s >= p_e) {
+      return null;
+    }
+    int root = post[p_e - 1];
+    TreeNode ret = new TreeNode(root);
+    for (int i = i_s; i < i_e; i++) {
+      if (root == in[i]) {
+        ret.left = b(in, i_s, i, post, p_s, p_s + (i - i_s));
+        ret.right = b(in, i + 1, i_e, post, (p_e - 1) - (i_e - (i + 1)), p_e - 1);
+        return ret;
+      }
+    }
+    return ret;
+  }
+
+  public TreeNode buildTree(int[] inorder, int[] postorder) {
+    return b(inorder, 0, inorder.length, postorder, 0, postorder.length);
+  }
+}
