7bf22c33d28216a907a4a0f78f49b0e4dff8e600
huijunwu <huijun.wu.2010@gmail.com>
2016-01-31 22:06:11 -0700

    two pointers low & high -> center in sorted array

--- 3Sum Closest.java
+++ 3Sum Closest.java
@@ -20,3 +20,28 @@ public class Solution {
     return ret;
   }
 }
+----------------
+public class Solution {
+  public int threeSumClosest(int[] nums, int target) {
+    int ret = nums[0] + nums[1] + nums[2];
+    Arrays.sort(nums);
+    for (int i = 0; i < nums.length; i++) {
+      int j = i + 1;
+      int k = nums.length - 1;
+      while (j < k) {
+        int sum = nums[i] + nums[j] + nums[k];
+        if (Math.abs(target - sum) < Math.abs(target - ret)) {
+          ret = sum;
+        }
+        if (sum < target) {
+          j++;
+        } else if (sum > target) {
+          k--;
+        } else {
+          return ret;
+        }
+      }
+    }
+    return ret;
+  }
+}
