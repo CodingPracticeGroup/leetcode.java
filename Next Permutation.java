a80893e9c0833b222a9b8996443cc84674736667
huijunwu <huijun.wu.2010@gmail.com>
2016-01-31 21:24:39 -0700

    array, math, swap, reverse

--- Next Permutation.java
+++ Next Permutation.java
@@ -41,3 +41,34 @@ public class Solution {
     Arrays.sort(nums, i + 1, nums.length);
   }
 }
+------------------
+public class Solution {
+  public void nextPermutation(int[] nums) {
+    int idx = nums.length - 2;
+    while (idx >= 0 && nums[idx] >= nums[idx + 1]) {
+      idx--;
+    }
+    if (idx == -1) {
+      reverse(nums, 0, nums.length - 1);
+      return;
+    }
+    int idx2 = nums.length - 1;
+    while (idx2 > idx && nums[idx] >= nums[idx2]) {
+      idx2--;
+    }
+    swap(nums, idx, idx2);
+    reverse(nums, idx + 1, nums.length - 1);
+  }
+
+  private void swap(int nums[], int i, int j) {
+    int tmp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = tmp;
+  }
+
+  private void reverse(int[] nums, int start, int end) {
+    while (start < end) {
+      swap(nums, start++, end--);
+    }
+  }
+}
