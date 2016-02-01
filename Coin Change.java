2e94232b7eb38d5166993db7f5b4ef4cea7db33b
huijunwu <huijun.wu.2010@gmail.com>
2016-01-31 17:21:07 -0700

    dp array, init dp with sparse 1's, accumulate along dp array scan

--- Coin Change.java
+++ Coin Change.java
@@ -16,3 +16,32 @@ public class Solution {
     }
   }
 }
+--------------
+public class Solution {
+  public int coinChange(int[] coins, int amount) {
+    if (coins.length == 0 || amount <= 0) {
+      return 0;
+    }
+    long dp[] = new long[amount + 1];
+    Arrays.fill(dp, Integer.MAX_VALUE);
+    int min = coins[0];
+    for (int i : coins) {
+      if (i <= amount) {
+        dp[i] = 1;
+        min = Math.min(min, i);
+      }
+    }
+    for (int i = min + 1; i <= amount; i++) {
+      for (int j : coins) {
+        if (i - j >= 0) {
+          dp[i] = Math.min(dp[i], dp[i - j] + 1);
+        }
+      }
+    }
+    if (dp[amount] == Integer.MAX_VALUE) {
+      return -1;
+    } else {
+      return Math.toIntExact(dp[amount]);
+    }
+  }
+}
