cb8741b5df80c8ae834a57fedfd5bd32204ec24c
huijunwu <huijun.wu.2010@gmail.com>
2016-01-31 21:37:22 -0700

    tree recursion, use inorder as base, use preorder to determine root

--- Construct Binary Tree from Preorder and Inorder Traversal.java
+++ Construct Binary Tree from Preorder and Inorder Traversal.java
@@ -21,3 +21,26 @@ public class Solution {
     return buildTree_range(preorder, 0, preorder.length, inorder, 0, inorder.length);
   }
 }
+-----------
+public class Solution {
+  private TreeNode b(int[] pre, int p_s, int p_e, int[] in, int i_s, int i_e) {
+    if (p_s >= p_e || i_s >= i_e) {
+      return null;
+    }
+    int root = pre[p_s];
+    TreeNode ret = new TreeNode(root);
+    for (int i = i_s; i < i_e; i++) {
+      if (root == in[i]) {
+        int left_len = i - i_s;
+        ret.left = b(pre, p_s + 1, (p_s + 1) + (i - i_s), in, i_s, i);
+        ret.right = b(pre, p_e - (i_e - (i + 1)), p_e, in, i + 1, i_e);
+        return ret;
+      }
+    }
+    return ret;
+  }
+
+  public TreeNode buildTree(int[] preorder, int[] inorder) {
+    return b(preorder, 0, preorder.length, inorder, 0, inorder.length);
+  }
+}
