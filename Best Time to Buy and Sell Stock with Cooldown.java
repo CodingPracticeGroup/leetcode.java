acbbec3558af695e5f6cb3f94d6410aa457abc7c
huijunwu <huijun.wu.2010@gmail.com>
2016-01-31 23:40:06 -0700

    dp, state machine, 存量 as state, action as state transfer

--- Best Time to Buy and Sell Stock with Cooldown.java
+++ Best Time to Buy and Sell Stock with Cooldown.java
@@ -16,3 +16,24 @@ public class Solution {
     return Math.max(sell_yesterday, cooldown_yesterday);
   }
 }
+------------
+public class Solution {
+  // https://leetcode.com/discuss/72030/share-my-dp-solution-by-state-machine-thinking
+  public int maxProfit(int[] prices) {
+    if (prices.length < 2) { // 来不及一次完整的交易
+      return 0;
+    }
+    int afterbuy[] = new int[prices.length + 1]; // 钱包，存量
+    int aftersell[] = new int[prices.length + 1];
+    int aftercooldown[] = new int[prices.length + 1];
+    afterbuy[0] = -prices[0]; // 假设存量0，从当前时间点开始
+    aftersell[0] = 0; // 以前的收益不算，从当前时间点开始
+    aftercooldown[0] = 0;
+    for (int i = 0; i < prices.length; i++) { // 状态机
+      afterbuy[i + 1] = Math.max(afterbuy[i], aftercooldown[i] - prices[i]);
+      aftersell[i + 1] = prices[i] + afterbuy[i];
+      aftercooldown[i + 1] = Math.max(aftercooldown[i], aftersell[i]);
+    }
+    return Math.max(aftersell[prices.length], aftercooldown[prices.length]); // 因为cooldown可以停，不必进入buy，buy比cooldown少，所以只考虑sell和cooldown
+  }
+}
