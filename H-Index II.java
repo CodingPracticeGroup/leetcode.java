9bafba09b19229b57ff614fb881698bd753c1438
huijunwu <huijun.wu.2010@gmail.com>
2016-01-31 22:50:48 -0700

    low high binary search, tie breaking -> low/high+- or low<high

--- H-Index II.java
+++ H-Index II.java
@@ -18,3 +18,22 @@ public class Solution {
     return citations.length - low;
   }
 }
+---------------
+public class Solution {
+  public int hIndex(int[] citations) {
+    if (citations.length == 0) {
+      return 0;
+    }
+    int low = 0;
+    int high = citations.length - 1;
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      if (citations.length - mid <= citations[mid]) {
+        high = mid - 1; // Tie-breaking
+      } else {
+        low = mid + 1;
+      }
+    }
+    return citations.length - low;
+  }
+}
