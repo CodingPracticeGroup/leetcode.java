a00990e9e69bc56da89883027ae14bf10afb9e5d
huijunwu <huijun.wu.2010@gmail.com>
2016-02-02 23:04:06 -0700

    dp, nested scan inside previous dp

--- Word Break.java
+++ Word Break.java
@@ -13,3 +13,27 @@ public class Solution {
     return dp[s.length()];
   }
 }
+---------
+// dfs cannot pass large test, no matter string operation or boolean[][]
+// will dfs with prune work?
+public class Solution {
+  public boolean wordBreak(String s, Set<String> wordDict) {
+    boolean dp[] = new boolean[s.length() + 1];
+    Arrays.fill(dp, false);
+    for (int len = 1; len <= s.length(); len++) {
+      if (wordDict.contains(s.substring(0, len))) {
+        dp[len] = true;
+      } else {
+        for (int last = 1; last < len; last++) {
+          if (dp[last] == true) {
+            if (wordDict.contains(s.substring(last, len))) {
+              dp[len] = true;
+              break;
+            }
+          }
+        }
+      }
+    }
+    return dp[s.length()];
+  }
+}
