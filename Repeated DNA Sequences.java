6fa0147a68089ed57acfde5a710d8dc3c22a5145
huijunwu <huijun.wu.2010@gmail.com>
2016-02-03 15:26:51 -0700

    scan string with hashset hit

--- Repeated DNA Sequences.java
+++ Repeated DNA Sequences.java
@@ -13,3 +13,19 @@ public class Solution {
     return new ArrayList<String>(ret);
   }
 }
+------------
+public class Solution {
+  public List<String> findRepeatedDnaSequences(String s) {
+    Set<String> ret = new HashSet<>();
+    Set<String> seen = new HashSet<>();
+    for (int i = 10; i <= s.length(); i++) {
+      String ten = s.substring(i - 10, i);
+      if (seen.contains(ten)) {
+        ret.add(ten);
+      } else {
+        seen.add(ten);
+      }
+    }
+    return new ArrayList<String>(ret);
+  }
+}
