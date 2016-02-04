7b596f0b7d861199d6e6d13b4579b446e4a40514
huijunwu <huijun.wu.2010@gmail.com>
2016-02-03 19:18:22 -0700

    singly linked list, find half(step1, step2; len, half), reverse, find tail, merge (cut, insert)

--- Reorder List.java
+++ Reorder List.java
@@ -27,3 +27,40 @@ public class Solution {
     }
   }
 }
+-----------
+public class Solution {
+  public void reorderList(ListNode head) {
+    int len = 0;
+    for (ListNode ln = head; ln != null; ln = ln.next) {
+      len++;
+    }
+    if (len == 0) {
+      return;
+    }
+    // find half
+    ListNode ln = head;
+    int half = len / 2;
+    for (int i = 0; i < half; i++) {
+      ln = ln.next;
+    }
+    ListNode second = ln.next;
+    ln.next = null;
+    // reverse
+    ListNode reverse = new ListNode(0);
+    while (second != null) {
+      ListNode n = second;
+      second = second.next;
+
+      n.next = reverse.next;
+      reverse.next = n;
+    }
+    // merge
+    for (ListNode l = head; l != null && reverse.next != null; l = l.next.next) {
+      ListNode tmp = reverse.next;
+      reverse.next = reverse.next.next;
+
+      tmp.next = l.next;
+      l.next = tmp;
+    }
+  }
+}
