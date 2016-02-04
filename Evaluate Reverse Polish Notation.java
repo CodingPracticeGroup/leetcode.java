a255291ffe04598d89c300a483be1698bb3dabfb
huijunwu <huijun.wu.2010@gmail.com>
2016-02-03 17:57:21 -0700

    stack, switch string, math

--- Evaluate Reverse Polish Notation.java
+++ Evaluate Reverse Polish Notation.java
@@ -25,3 +25,63 @@ public class Solution {
     return stack.pop();
   }
 }
+----------------
+public class Solution {
+  public int evalRPN(String[] a) {
+    Stack<Integer> stack = new Stack<Integer>();
+
+    for (int i = 0; i < a.length; i++) {
+      switch (a[i]) {
+        case "+":
+          stack.push(stack.pop() + stack.pop());
+          break;
+
+        case "-":
+          stack.push(-stack.pop() + stack.pop());
+          break;
+
+        case "*":
+          stack.push(stack.pop() * stack.pop());
+          break;
+
+        case "/":
+          int n1 = stack.pop(), n2 = stack.pop();
+          stack.push(n2 / n1);
+          break;
+
+        default:
+          stack.push(Integer.parseInt(a[i]));
+      }
+    }
+
+    return stack.pop();
+  }
+}
+----------------
+public class Solution {
+  public int evalRPN(String[] tokens) {
+    Deque<Integer> stack = new ArrayDeque<>();
+    for (int i = 0; i < tokens.length; i++) {
+      if (tokens[i].equals("+")) {
+        int d2 = stack.pollLast();
+        int d1 = stack.pollLast();
+        stack.offerLast(d1 + d2);
+      } else if (tokens[i].equals("-")) {
+        int d2 = stack.pollLast();
+        int d1 = stack.pollLast();
+        stack.offerLast(d1 - d2);
+      } else if (tokens[i].equals("*")) {
+        int d2 = stack.pollLast();
+        int d1 = stack.pollLast();
+        stack.offerLast(d1 * d2);
+      } else if (tokens[i].equals("/")) {
+        int d2 = stack.pollLast();
+        int d1 = stack.pollLast();
+        stack.offerLast(d1 / d2);
+      } else {
+        stack.offerLast(Integer.parseInt(tokens[i]));
+      }
+    }
+    return stack.poll();
+  }
+}
