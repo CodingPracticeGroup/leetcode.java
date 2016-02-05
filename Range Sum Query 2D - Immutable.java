900c4ed286b635d6fdfca891041c57b0a6cba0a3
huijunwu <huijun.wu.2010@gmail.com>
2016-02-05 02:48:52 -0700

    matrix area with sum pre-process

--- Range Sum Query 2D - Immutable.java
+++ Range Sum Query 2D - Immutable.java
@@ -32,3 +32,38 @@ public class NumMatrix {
 // NumMatrix numMatrix = new NumMatrix(matrix);
 // numMatrix.sumRegion(0, 1, 2, 3);
 // numMatrix.sumRegion(1, 2, 3, 4);
+--------------
+public class NumMatrix {
+
+  int[][] sum = null;
+
+  public NumMatrix(int[][] matrix) {
+    int m = matrix.length;
+    if (m == 0)
+      return;
+    int n = matrix[0].length;
+    if (n == 0)
+      return;
+    sum = new int[m][n];
+    sum[0][0] = matrix[0][0];
+    for (int i = 1; i < m; i++)
+      sum[i][0] = sum[i - 1][0] + matrix[i][0];
+    for (int j = 1; j < n; j++)
+      sum[0][j] = sum[0][j - 1] + matrix[0][j];
+    for (int i = 1; i < m; i++) {
+      for (int j = 1; j < n; j++) {
+        sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + matrix[i][j];
+      }
+    }
+  }
+
+  private int sum(int row, int col) {
+    if (row >= 0 && col >= 0 && sum != null)
+      return sum[row][col];
+    return 0;
+  }
+
+  public int sumRegion(int row1, int col1, int row2, int col2) {
+    return sum(row2, col2) - sum(row1 - 1, col2) - sum(row2, col1 - 1) + sum(row1 - 1, col1 - 1);
+  }
+}
