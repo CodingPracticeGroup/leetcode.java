4d4cb035c3ab0da5efcde676ebdfec24d98344a6
huijunwu <huijun.wu.2010@gmail.com>
2016-02-06 00:48:22 -0700

    array scan, divide and conquer, two line tracking flip

--- Maximum Product Subarray.java
+++ Maximum Product Subarray.java
@@ -56,3 +56,93 @@ public class Solution {
     return max;
   }
 }
+-----------
+public class Solution {
+  private int mp(int[] nums, int i, int j) {
+    if (i >= j) {
+      return Integer.MIN_VALUE;
+    }
+    if (i + 1 == j) {
+      return nums[i];
+    }
+
+    int p = 1;
+    int first = -1;
+    for (int k = i; k < j; k++) {
+      p *= nums[k];
+      if (nums[k] < 0) {
+        first = k;
+        break;
+      }
+    }
+    if (first == -1) {
+      return p;
+    }
+
+    int q = 1;
+    int last = -1;
+    for (int k = j - 1; k >= 0; k--) {
+      q *= nums[k];
+      if (nums[k] < 0) {
+        last = k;
+        break;
+      }
+    }
+    if (first == last) {
+      return Math.max(p / nums[first], q / nums[last]);
+    }
+
+    int r = 1;
+    for (int k = first + 1; k < last; k++) {
+      r *= nums[k];
+    }
+    if (r > 0) {
+      return p * q * r;
+    } else {
+      return Math.max(p * r, q * r);
+    }
+  }
+
+  public int maxProduct(int[] nums) {
+    int ret = Integer.MIN_VALUE;
+    int i = 0;
+    int p = 1;
+    for (int j = 0; j < nums.length; j++) {
+      p *= nums[j];
+      if (nums[j] == 0) {
+        ret = Math.max(ret, mp(nums, i, j));
+        ret = Math.max(ret, 0);
+        i = j + 1;
+      }
+    }
+    ret = Math.max(ret, mp(nums, i, nums.length));
+    if (ret == Integer.MIN_VALUE) {
+      return p;
+    }
+    return ret;
+  }
+}
+---------------
+public class Solution {
+  public int maxProduct(int[] A) {
+    if (A.length == 0) {
+      return 0;
+    }
+
+    int maxherepre = A[0];
+    int minherepre = A[0];
+    int maxsofar = A[0];
+    int maxhere, minhere;
+
+    for (int i = 1; i < A.length; i++) {
+      maxhere = Math.max(Math.max(maxherepre * A[i], minherepre * A[i]), A[i]);
+      minhere = Math.min(Math.min(maxherepre * A[i], minherepre * A[i]), A[i]);
+
+      maxsofar = Math.max(maxhere, maxsofar);
+
+      maxherepre = maxhere;
+      minherepre = minhere;
+    }
+    return maxsofar;
+  }
+}
