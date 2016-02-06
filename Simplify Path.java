1fc5313f257e1ffb072d0e3e2ae1cc2a927da799
huijunwu <huijun.wu.2010@gmail.com>
2016-02-06 01:10:08 -0700

    stack, hit/miss, split replaceFirst(^x)

--- Simplify Path.java
+++ Simplify Path.java
@@ -26,3 +26,46 @@ public class Solution {
     return sb.toString();
   }
 }
+-----------
+public class Solution {
+  public String simplifyPath(String path) {
+    String[] strs = path.split("/");
+    // String [] strs = path.replaceFirst("^/", "").split("/");
+    Deque<String> stack = new ArrayDeque<>();
+    for (String s : strs) {
+      if (s.equals("..")) {
+        if (!stack.isEmpty()) {
+          stack.pollLast();
+        }
+      } else if (!s.equals(".") && !s.equals("")) {
+        stack.offerLast(s);
+      }
+    }
+    if (stack.isEmpty()) {
+      return "/";
+    }
+    StringBuilder sb = new StringBuilder();
+    for (String s : stack) {
+      sb.append("/");
+      sb.append(s);
+    }
+    return sb.toString();
+  }
+}
+--------------------
+public class Solution {
+  public String simplifyPath(String path) {
+    Deque<String> stack = new LinkedList<>();
+    Set<String> skip = new HashSet<>(Arrays.asList(".", "")); // hit/miss
+    for (String dir : path.split("/")) {
+      if (dir.equals("..") && !stack.isEmpty())
+        stack.pop();
+      else if (!skip.contains(dir))
+        stack.push(dir);
+    }
+    String res = "";
+    for (String dir : stack)
+      res = "/" + dir + res;
+    return res.isEmpty() ? "/" : res;
+  }
+}
