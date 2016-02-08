3e608bad48589071a9fa9c42d57dfeb5d7c3d23a
huijunwu <huijun.wu.2010@gmail.com>
2016-02-07 21:32:47 -0700

    bst tree with range recursion

--- Validate Binary Search Tree.java
+++ Validate Binary Search Tree.java
@@ -13,3 +13,19 @@ public class Solution {
     return isValidBST_(root, Integer.MIN_VALUE - 1L, Integer.MAX_VALUE + 1L);
   }
 }
+-----------------
+public class Solution {
+  private boolean iv(TreeNode r, long left, long right) {
+    if (r == null) {
+      return true;
+    }
+    if (r.val <= left || r.val >= right) {
+      return false;
+    }
+    return iv(r.left, left, r.val) && iv(r.right, r.val, right);
+  }
+
+  public boolean isValidBST(TreeNode root) {
+    return iv(root, Integer.MIN_VALUE - 1L, Integer.MAX_VALUE + 1L);
+  }
+}
