9fde5766ae8c8719e4e05057eadf1e28f0e7c485
huijunwu <huijun.wu.2010@gmail.com>
2016-02-07 22:10:59 -0700

    trie tree with hashmap, boundary for large test

--- Add and Search Word - Data structure design.java
+++ Add and Search Word - Data structure design.java
@@ -45,3 +45,49 @@ public class WordDictionary {
 // WordDictionary wordDictionary = new WordDictionary();
 // wordDictionary.addWord("word");
 // wordDictionary.search("pattern");
+----------------------
+public class WordDictionary {
+  HashMap<Character, HashMap> root = new HashMap<>();
+
+  // Adds a word into the data structure.
+  public void addWord(String word) {
+    HashMap<Character, HashMap> p = root;
+    for (int i = 0; i < word.length(); i++) {
+      char c = word.charAt(i);
+      if (!p.containsKey(c)) {
+        p.put(c, new HashMap<Character, HashMap>());
+      }
+      p = p.get(c);
+    }
+    p.put('$', root);
+  }
+
+  private boolean s(String word, int startPos, HashMap<Character, HashMap> p) {
+    if (startPos == word.length()) {
+      if (p.containsKey('$') && p.get('$') == root) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    char c = word.charAt(startPos); // 这个是最后一根稻草，节约时间
+    if (c == '.') {
+      for (HashMap o : p.values()) { // 直接遍历values，比遍历keySet节约时间
+        if (o != root && s(word, startPos + 1, o)) {
+          return true;
+        }
+      }
+      return false;
+    } else if (p.containsKey(c)) {
+      return s(word, startPos + 1, p.get(c));
+    } else {
+      return false;
+    }
+  }
+
+  // Returns if the word is in the data structure. A word could
+  // contain the dot character '.' to represent any one letter.
+  public boolean search(String word) {
+    return s(word, 0, root);
+  }
+}
