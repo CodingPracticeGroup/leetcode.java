2b8058505347a8955fab418eac31b74d257d144f
huijunwu <huijun.wu.2010@gmail.com>
2016-02-07 22:48:49 -0700

    graph bfs string chararray/stringbuilder

--- Word Ladder.java
+++ Word Ladder.java
@@ -29,3 +29,38 @@ public class Solution {
     return 0;
   }
 }
+------------
+public class Solution {
+  public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
+    int ret = 1;
+    Set<String> visited = new HashSet<>(); // mark
+    Deque<String> queue = new ArrayDeque<>(); // queue
+    visited.add(beginWord);
+    queue.offer(beginWord);
+    while (!queue.isEmpty()) {
+      ret++;
+      int count = queue.size(); // level
+      for (int j = 0; j < count; j++) {
+        char[] nc = queue.poll().toCharArray(); // poll
+        for (int i = 0; i < nc.length; i++) { // explorer
+          char orig = nc[i];
+          for (char c = 'a'; c <= 'z'; c++) { // explorer
+            if (c != orig) { // prune
+              nc[i] = c;
+              String s = new String(nc); // construct
+              if (s.equals(endWord)) {
+                return ret;
+              }
+              if (wordList.contains(s) && !visited.contains(s)) { // prune
+                visited.add(s);
+                queue.offer(s);
+              }
+            }
+          }
+          nc[i] = orig;
+        }
+      }
+    }
+    return 0;
+  }
+}
