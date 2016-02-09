fe1100699ed48908ec1bf7b2c43e4152a61f0c77
huijunwu <huijun.wu.2010@gmail.com>
2016-02-09 01:56:42 -0700

    string move/mirror/rotate, java8stream

--- Reverse Words in a String.java
+++ Reverse Words in a String.java
@@ -5,3 +5,31 @@ public class Solution {
         .reduce((x, y) -> x + " " + y).get();
   }
 }
+--------------
+public class Solution {
+  public String reverseWords(String s) {
+    s = s.trim();
+    s = s.replaceAll("\\s+", " ");
+    StringBuilder sb = new StringBuilder();
+    for (int i = s.length() - 1; i >= 0; i--) {
+      sb.append(s.charAt(i));
+    }
+    int i = 0;
+    while (i < sb.length()) {
+      while (i < sb.length() && sb.charAt(i) == ' ') {
+        i++;
+      }
+      int j = i + 1;
+      while (j < sb.length() && sb.charAt(j) != ' ') {
+        j++;
+      }
+      for (int l = i, r = j - 1; l < r; l++, r--) {
+        char c = sb.charAt(l);
+        sb.setCharAt(l, sb.charAt(r));
+        sb.setCharAt(r, c);
+      }
+      i = j;
+    }
+    return sb.toString();
+  }
+}
