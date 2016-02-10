365309eea6e21d128e87d8cc53c390fafd6404a7
huijunwu <huijun.wu.2010@gmail.com>
2016-02-10 02:08:19 -0700

    seems stack but bfs with level

--- Remove Invalid Parentheses.java
+++ Remove Invalid Parentheses.java
@@ -39,3 +39,45 @@ public class Solution {
     return new ArrayList<String>(ret);
   }
 }
+------------
+public class Solution {
+  private boolean check(String s) {
+    int count = 0;
+    for (int i = 0; i < s.length(); i++) {
+      if (s.charAt(i) == '(') {
+        count++;
+      } else if (s.charAt(i) == ')') {
+        count--;
+        if (count < 0) {
+          return false;
+        }
+      }
+    }
+    return count == 0;
+  }
+
+  public List<String> removeInvalidParentheses(String s) {
+    List<String> ret = new ArrayList<>();
+    Set<String> queue = new HashSet<>();
+    queue.add(s);
+    while (!queue.isEmpty()) {
+      Set<String> q2 = new HashSet<>();
+      for (String str : queue) { // level bfs
+        if (check(str)) {
+          ret.add(str);
+        } else {
+          for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(' || str.charAt(i) == ')') {
+              q2.add(str.substring(0, i) + str.substring(i + 1, str.length()));
+            }
+          }
+        }
+      }
+      if (!ret.isEmpty()) {
+        return ret;
+      }
+      queue = q2;
+    }
+    return ret;
+  }
+}
