7bd77149d83d9aa40862b003fcf2b05ea8298069
huijunwu <huijun.wu.2010@gmail.com>
2016-02-09 21:14:00 -0700

    tree stack iteration, left first vs right first, nest loop vs stack & pointer, single mimic stack process when pop for pre-order only

--- Binary Tree Postorder Traversal.java
+++ Binary Tree Postorder Traversal.java
@@ -4,15 +4,57 @@ public class Solution {
     if (root == null)
       return ret;
     Deque<TreeNode> stack = new LinkedList<>();
-    stack.push(root);
+    stack.push(root); // cannot process when push: non-order
     while (!stack.isEmpty()) {
       TreeNode tn = stack.pop();
-      ret.add(0, tn.val);
+      ret.add(0, tn.val); // process when pop: can only handle pre-order
       if (tn.left != null) {
-        stack.push(tn.left);
+        stack.push(tn.left); // cannot process when push: non-order
       }
       if (tn.right != null) {
-        stack.push(tn.right);
+        stack.push(tn.right); // cannot process when push: non-order
+      }
+    }
+    return ret;
+  }
+}
+----------------
+public class Solution {
+  public List<Integer> postorderTraversal(TreeNode root) {
+    LinkedList<Integer> ret = new LinkedList<>();
+
+    Deque<TreeNode> stack = new ArrayDeque<>();
+
+    for (TreeNode tn = root; tn != null; tn = tn.right) { // 起始
+      ret.offerFirst(tn.val);
+      stack.push(tn);
+    }
+
+    while (!stack.isEmpty()) { // 条件
+      TreeNode node = stack.pop(); // if process here, it is in-order
+
+      for (TreeNode tn = node.left; tn != null; tn = tn.right) { // 增量
+        ret.offerFirst(tn.val);
+        stack.push(tn);
+      }
+    }
+
+    return ret;
+  }
+}
+---------------
+public class Solution {
+  public List<Integer> postorderTraversal(TreeNode root) {
+    LinkedList<Integer> ret = new LinkedList<>();
+    Deque<TreeNode> stack = new ArrayDeque<>();
+    while (!stack.isEmpty() || root != null) {
+      if (root != null) { // stack push
+        ret.offerFirst(root.val); // pre-order
+        stack.push(root);
+        root = root.right; // right first
+      } else { // stack pop and try next layer
+        root = stack.pop();
+        root = root.left;
       }
     }
     return ret;
