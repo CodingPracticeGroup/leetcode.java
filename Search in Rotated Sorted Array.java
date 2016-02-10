27d67bd7f860e3783315c2e7755f81654a21c9f0
huijunwu <huijun.wu.2010@gmail.com>
2016-02-10 02:23:34 -0700

    recursive binary search with low and high

--- Search in Rotated Sorted Array.java
+++ Search in Rotated Sorted Array.java
@@ -39,3 +39,37 @@ public class Solution {
     return search_range(nums, target, low, high);
   }
 }
+-----------
+public class Solution {
+  private int s(int[] nums, int target, int low, int high) {
+    if (low > high) {
+      return -1;
+    }
+    if (nums[low] <= nums[high]) { // no rotate
+      int r = Arrays.binarySearch(nums, low, high + 1, target);
+      return r < 0 ? -1 : r;
+    }
+
+    int mid = low + (high - low) / 2;
+    if (nums[mid] == target) { // hit
+      return mid;
+    } else if (nums[low] <= target && target < nums[mid]) { // find range
+      int r = Arrays.binarySearch(nums, low, mid, target);
+      return r < 0 ? -1 : r;
+    } else if (nums[mid] < target && target <= nums[high]) { // find range
+      int r = Arrays.binarySearch(nums, mid + 1, high + 1, target);
+      return r < 0 ? -1 : r;
+    }
+
+    int ret = s(nums, target, low, mid - 1); // try left
+    if (ret >= 0) {
+      return ret;
+    }
+    ret = s(nums, target, mid + 1, high); // try right
+    return ret;
+  }
+
+  public int search(int[] nums, int target) {
+    return s(nums, target, 0, nums.length - 1);
+  }
+}
