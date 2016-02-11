be3d68fd535243b14f674c22a12ad77504805a32
huijunwu <huijun.wu.2010@gmail.com>
2016-02-11 02:20:37 -0700

    dp with two str len, all 3 cases are used, two string equal role, cases by char=char?

--- Edit Distance.java
+++ Edit Distance.java
@@ -21,3 +21,27 @@ public class Solution {
     return dp[len1][len2];
   }
 }
+------------------
+public class Solution {
+  public int minDistance(String word1, String word2) {
+    int dp[][] = new int[word1.length() + 1][word2.length() + 1];
+    dp[0][0] = 0;
+    for (int i = 1; i <= word1.length(); i++) {
+      dp[i][0] = i;
+    }
+    for (int j = 1; j <= word2.length(); j++) {
+      dp[0][j] = j;
+    }
+    for (int i = 1; i <= word1.length(); i++) {
+      for (int j = 1; j <= word2.length(); j++) {
+        dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + 1; // insert/delete
+        if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+          dp[i][j] = Math.min(dp[i][j], dp[i - 1][j - 1]);
+        } else {
+          dp[i][j] = Math.min(dp[i][j], dp[i - 1][j - 1] + 1); // replace
+        }
+      }
+    }
+    return dp[word1.length()][word2.length()];
+  }
+}
