01f1fa76df360f63208378f3560469800c4cd1e1
huijunwu <huijun.wu.2010@gmail.com>
2016-02-11 02:11:46 -0700

    dp with two string length, not necessary relate all i-1 combination, i-1 cases by char=char?

--- Distinct Subsequences.java
+++ Distinct Subsequences.java
@@ -22,3 +22,26 @@ public class Solution {
     return dp[slen][tlen];
   }
 }
+------------------
+public class Solution {
+  public int numDistinct(String s, String t) {
+    int dp[][] = new int[s.length() + 1][t.length() + 1];
+    dp[0][0] = 1; // an empty string contains the empty string 1 time
+    for (int i = 1; i <= s.length(); i++) {
+      dp[i][0] = 1; // the empty string is a subsequence of any string but only 1 time
+    }
+    for (int j = 1; j <= t.length(); j++) {
+      dp[0][j] = 0; // an empty string cannot contain a non-empty string as a substring
+    }
+    for (int i = 1; i <= s.length(); i++) {
+      for (int j = 1; j <= t.length(); j++) {
+        dp[i][j] = dp[i - 1][j];
+        if (s.charAt(i - 1) == t.charAt(j - 1)) {
+          dp[i][j] += dp[i - 1][j - 1];
+        }
+        // dp[i][j-1]
+      }
+    }
+    return dp[s.length()][t.length()];
+  }
+}
