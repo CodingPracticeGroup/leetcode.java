f441838949ae682ac2633d8d9b10f33e7d1d4aad
huijunwu <huijun.wu.2010@gmail.com>
2016-02-11 03:27:01 -0700

    bucket size can be smaller but at least 1, bucket num = bucket id +1

--- Maximum Gap.java
+++ Maximum Gap.java
@@ -29,3 +29,49 @@ public class Solution {
     return ret;
   }
 }
+-------------------
+public class Solution {
+  public int maximumGap(int[] nums) {
+    int count = nums.length;
+    if (count < 2)
+      return 0;
+
+    int max = Arrays.stream(nums).max().getAsInt();
+    int min = Arrays.stream(nums).min().getAsInt();
+    if (max == min) {
+      return 0;
+    }
+
+    int bucket_size = Math.max(1, (max - min) / (count - 1)); // can be smaller, so that more empty bucket, but at least 1
+    int bucket_num = (max - min) / bucket_size + 1; // the max id, so that should +1 for len
+
+    int bucket_max[] = new int[bucket_num];
+    Arrays.fill(bucket_max, -1);
+    int bucket_min[] = new int[bucket_num];
+    Arrays.fill(bucket_min, -1);
+
+    for (int i : nums) {
+      int id = (i - min) / bucket_size;
+      if (bucket_max[id] == -1) {
+        bucket_max[id] = i;
+      }
+      bucket_max[id] = Math.max(bucket_max[id], i);
+      if (bucket_min[id] == -1) {
+        bucket_min[id] = i;
+      }
+      bucket_min[id] = Math.min(bucket_min[id], i);
+    }
+
+    int ret = 0;
+    int last_max = -1;
+    for (int i = 0; i < bucket_num; i++) {
+      if (bucket_min[i] != -1 && last_max != -1) { // at least 1 empty bucket
+        ret = Math.max(ret, bucket_min[i] - last_max);
+      }
+      if (bucket_max[i] != -1) {
+        last_max = bucket_max[i];
+      }
+    }
+    return ret;
+  }
+}
