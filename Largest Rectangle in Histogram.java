5e7615afe06cff1bd5ec5c5c00876d33afdb3eca
huijunwu <huijun.wu.2010@gmail.com>
2016-02-13 20:59:02 -0700

    sorted inside stack, boundary around peek

--- Largest Rectangle in Histogram.java
+++ Largest Rectangle in Histogram.java
@@ -22,3 +22,24 @@ public class Solution {
     return max;
   }
 }
+------
+public class Solution {
+  public int largestRectangleArea(int[] heights) {
+    int ret = 0;
+    Deque<Integer> stack = new ArrayDeque<>();
+    for (int i = 0; i < heights.length; i++) {
+      while (!stack.isEmpty() && heights[stack.peek()] > heights[i]) {
+        int idx = stack.poll();
+        int last = stack.isEmpty() ? -1 : stack.peek();
+        ret = Math.max(ret, heights[idx] * (i - last - 1));
+      }
+      stack.push(i);
+    }
+    while (!stack.isEmpty()) {
+      int idx = stack.poll();
+      int last = stack.isEmpty() ? -1 : stack.peek();
+      ret = Math.max(ret, heights[idx] * (heights.length - last - 1));
+    }
+    return ret;
+  }
+}
