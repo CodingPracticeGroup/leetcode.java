abf1c74ff202285ffb4e4d1620c39f12ba646b17
huijunwu <huijun.wu.2010@gmail.com>
2016-02-14 22:47:20 -0700

    double center palidrome check, dp and dp, scan array with scanner+last dp

--- Palindrome Partitioning II.java
+++ Palindrome Partitioning II.java
@@ -31,3 +31,47 @@ public class Solution {
     return dp2[len];
   }
 }
+-----------------
+public class Solution {
+  private void is(String s, int i, int j, boolean dp[][]) { // inclusive
+    while (i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)) {
+      dp[i--][j++] = true;
+    }
+  }
+
+  public int minCut(String s) {
+    boolean dp[][] = new boolean[s.length()][s.length()]; // inclusive
+    for (int i = 0; i < s.length(); i++) {
+      Arrays.fill(dp[i], false);
+    }
+    for (int i = 0; i < s.length(); i++) {
+      is(s, i, i, dp);
+      if (i + 1 < s.length()) {
+        is(s, i, i + 1, dp);
+      }
+    }
+
+    int ret = 0;
+    Set<Integer> tabu = new HashSet<>(); // visited
+    Deque<Integer> q = new ArrayDeque<>(); // stores range head
+    tabu.add(0);
+    q.offer(0);
+    while (!q.isEmpty()) {
+      int count = q.size();
+      for (int i = 0; i < count; i++) {
+        int idx = q.poll(); // poll
+        if (idx == s.length()) { // process
+          return ret - 1;
+        }
+        for (int j = idx; j < s.length(); j++) { // offer
+          if (dp[idx][j] && !tabu.contains(j + 1)) {
+            tabu.add(j + 1);
+            q.offer(j + 1);
+          }
+        }
+      }
+      ret++;
+    }
+    return -1;
+  }
+}
