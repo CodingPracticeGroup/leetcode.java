07c1d3bee8b4ab8f45cb1fab35c0c12ec26836e8
huijunwu <huijun.wu.2010@gmail.com>
2016-02-15 23:28:26 -0700

    two center check, StringBuilder.reverse.toString

--- Shortest Palindrome.java
+++ Shortest Palindrome.java
@@ -43,3 +43,31 @@ public class Solution {
     return null;
   }
 }
+---------------------
+public class Solution {
+  private boolean check(String s, int i, int j) {
+    while (i >= 0 && s.charAt(i) == s.charAt(j)) {
+      i--;
+      j++;
+    }
+    return i == -1;
+  }
+
+  private String head(String s, int start) {
+    StringBuilder sb = new StringBuilder(s.substring(start));
+    sb.reverse();
+    return sb.toString();
+  }
+
+  public String shortestPalindrome(String s) {
+    for (int i = s.length() / 2; i >= 0; i--) {
+      if (i - 0 <= s.length() - 1 - i && check(s, i, i)) {
+        return head(s, (i - 0) + (i - 0) + 1) + s;
+      }
+      if (i - 1 >= 0 && i - 1 - 0 <= s.length() - 1 - i && check(s, i - 1, i)) {
+        return head(s, (i - 0) * 2) + s;
+      }
+    }
+    return new StringBuilder(s).reverse().toString() + s;
+  }
+}
