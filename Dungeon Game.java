413ff2bb344eac3e659939002498b631f9ea186c
huijunwu <huijun.wu.2010@gmail.com>
2016-02-15 21:35:17 -0700

    matrix dp

--- Dungeon Game.java
+++ Dungeon Game.java
@@ -20,3 +20,43 @@ public class Solution {
     return dp[0][0];
   }
 }
+-----------------
+public class Solution {
+  public int calculateMinimumHP(int[][] dungeon) {
+    int m = dungeon.length;
+    int n = dungeon[0].length;
+    int[][] dp = new int[m][n]; // hp when entering i,j
+    if (dungeon[m - 1][n - 1] < 0) {
+      dp[m - 1][n - 1] = 1 - dungeon[m - 1][n - 1];
+    } else {
+      dp[m - 1][n - 1] = 1;
+    }
+    for (int i = m - 2; i >= 0; i--) {
+      if (dungeon[i][n - 1] < 0) {
+        dp[i][n - 1] = dp[i + 1][n - 1] - dungeon[i][n - 1];
+      } else {
+        dp[i][n - 1] = Math.max(dp[i + 1][n - 1] - dungeon[i][n - 1], 1);
+      }
+    }
+    for (int j = n - 2; j >= 0; j--) {
+      if (dungeon[m - 1][j] < 0) {
+        dp[m - 1][j] = dp[m - 1][j + 1] - dungeon[m - 1][j];
+      } else {
+        dp[m - 1][j] = Math.max(dp[m - 1][j + 1] - dungeon[m - 1][j], 1);
+      }
+    }
+
+    for (int i = m - 2; i >= 0; i--) {
+      for (int j = n - 2; j >= 0; j--) {
+        if (dungeon[i][j] < 0) {
+          dp[i][j] = Math.min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j];
+        } else {
+          dp[i][j] = Math.min(Math.max(dp[i + 1][j] - dungeon[i][j], 1),
+              Math.max(dp[i][j + 1] - dungeon[i][j], 1));
+        }
+      }
+    }
+
+    return dp[0][0];
+  }
+}
