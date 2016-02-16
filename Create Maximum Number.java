f047feb2bdfcf7ea8a5f26ca0ea71ec2918ed60e
huijunwu <huijun.wu.2010@gmail.com>
2016-02-15 22:28:27 -0700

    combination, stack/window/queue pick greedy

--- Create Maximum Number.java
+++ Create Maximum Number.java
@@ -91,3 +91,97 @@ public class Solution {
     return ret;
   }
 }
+--------------
+public class Solution {
+  private int[] mn(int[] nums, int k) {
+    int ret[] = new int[k];
+    PriorityQueue<Integer> maxheap = new PriorityQueue<>(Collections.reverseOrder());
+    int reader = 0;
+    for (int i = 0; i < k; i++) {
+      maxheap.clear();
+      for (int j = reader; nums.length - j >= k - i; j++) { // how many choices
+        maxheap.add(nums[j]);
+      }
+      ret[i] = maxheap.peek();
+      while (nums[reader] != maxheap.peek()) { // move to the choice
+        reader++;
+      }
+      reader++;
+    }
+    return ret;
+  }
+
+  private int[] merge(int[] n1, int[] n2) {
+    int ret[] = new int[n1.length + n2.length];
+    int i1 = 0;
+    int i2 = 0;
+    while (i1 < n1.length || i2 < n2.length) {
+      if (i1 < n1.length && i2 < n2.length) {
+        if (n1[i1] < n2[i2]) {
+          ret[i1 + i2] = n2[i2];
+          i2++;
+        } else if (n1[i1] > n2[i2]) {
+          ret[i1 + i2] = n1[i1];
+          i1++;
+        } else {
+          int t1 = i1;
+          int t2 = i2;
+          while (t1 < n1.length && t2 < n2.length && n1[t1] == n2[t2]) {
+            t1++;
+            t2++;
+          }
+          if (t1 != n1.length && t2 != n2.length) {
+            if (n1[t1] < n2[t2]) {
+              ret[i1 + i2] = n2[i2];
+              i2++;
+            } else if (n1[t1] > n2[t2]) {
+              ret[i1 + i2] = n1[i1];
+              i1++;
+            }
+          } else if (t1 == n1.length && t2 != n2.length) {
+            ret[i1 + i2] = n2[i2];
+            i2++;
+          } else if (t1 != n1.length && t2 == n2.length) {
+            ret[i1 + i2] = n1[i1];
+            i1++;
+          } else { // either
+            ret[i1 + i2] = n1[i1];
+            i1++;
+          }
+        }
+      } else if (i1 < n1.length) {
+        ret[i1 + i2] = n1[i1];
+        i1++;
+      } else if (i2 < n2.length) {
+        ret[i1 + i2] = n2[i2];
+        i2++;
+      }
+    }
+    return ret;
+  }
+
+  private int[] compare(int[] n1, int[] n2) {
+    for (int i = 0; i < n1.length && i < n2.length; i++) {
+      if (n1[i] < n2[i]) {
+        return n2;
+      } else if (n1[i] > n2[i]) {
+        return n1;
+      }
+    }
+    return n1;
+  }
+
+  public int[] maxNumber(int[] nums1, int[] nums2, int k) {
+    int[] ret = new int[k];
+    Arrays.fill(ret, Integer.MIN_VALUE);
+    for (int i = 0; i <= k; i++) {
+      if (i <= nums1.length && k - i <= nums2.length) {
+        int[] s1 = mn(nums1, i);
+        int[] s2 = mn(nums2, k - i);
+        int[] s3 = merge(s1, s2);
+        ret = compare(ret, s3);
+      }
+    }
+    return ret;
+  }
+}
