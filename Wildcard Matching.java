77611a86b9c6abcbc4bafbea326fe0fb2785ddc7
huijunwu <huijun.wu.2010@gmail.com>
2016-02-16 01:09:22 -0700

    two string length dp, cases by char=?, all 3 cases are used

--- Wildcard Matching.java
+++ Wildcard Matching.java
@@ -40,3 +40,36 @@ public class Solution {
     return dp[s.length()][p.length()];
   }
 }
+-------------
+public class Solution {
+  public boolean isMatch(String s, String p) {
+    boolean dp[][] = new boolean[s.length() + 1][p.length() + 1];
+    dp[0][0] = true;
+    for (int i = 1; i <= s.length(); i++) {
+      dp[i][0] = false;
+    }
+    for (int j = 1; j <= p.length(); j++) {
+      if (p.charAt(j - 1) == '*') {
+        dp[0][j] = dp[0][j - 1];
+      } else {
+        dp[0][j] = false;
+      }
+    }
+    for (int i = 1; i <= s.length(); i++) {
+      for (int j = 1; j <= p.length(); j++) {
+        if (p.charAt(j - 1) == '?') {
+          dp[i][j] = dp[i - 1][j - 1];
+        } else if (p.charAt(j - 1) == '*') {
+          dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+        } else {
+          if (s.charAt(i - 1) == p.charAt(j - 1)) {
+            dp[i][j] = dp[i - 1][j - 1];
+          } else {
+            dp[i][j] = false;
+          }
+        }
+      }
+    }
+    return dp[s.length()][p.length()];
+  }
+}
