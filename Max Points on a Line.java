b59c04008ea5ae7775730afbba23977b67620376
huijunwu <huijun.wu.2010@gmail.com>
2016-02-17 12:52:46 -0700

    math, same x/p/k, -0 vs 0

--- Max Points on a Line.java
+++ Max Points on a Line.java
@@ -41,3 +41,34 @@ public class Solution {
     return max;
   }
 }
+------------
+public class Solution {
+  public int maxPoints(Point[] points) {
+    int ret = 0;
+    for (int i = 0; i < points.length; i++) {
+      int samex = 1; // case 1
+      int samep = 0; // case 2
+      Map<Double, Integer> kc = new HashMap<>(); // case 3
+      for (int j = i + 1; j < points.length; j++) {
+        if (points[i].x == points[j].x && points[i].y == points[j].y) {
+          samep++;
+        } else if (points[i].x == points[j].x) {
+          samex++;
+        } else {
+          double k = (points[i].y - 1.0 * points[j].y) / (points[i].x - points[j].x);
+          if (k == 0) // -0 != 0
+            k = 0;
+          if (!kc.containsKey(k)) {
+            kc.put(k, 1);
+          }
+          kc.put(k, kc.get(k) + 1);
+        }
+      }
+      ret = Math.max(ret, samex + samep);
+      if (!kc.isEmpty()) {
+        ret = Math.max(ret, kc.values().stream().mapToInt(x -> x).max().getAsInt() + samep);
+      }
+    }
+    return ret;
+  }
+}
