5aeb4edc5d2da38987d9415d6dda9d60056fb888
huijunwu <huijun.wu.2010@gmail.com>
2016-02-18 01:42:02 -0700

    math, list seen check

--- Fraction to Recurring Decimal.java
+++ Fraction to Recurring Decimal.java
@@ -30,3 +30,46 @@ public class Solution {
     return (negative ? "-" : "") + sb.toString() + sb2.toString();
   }
 }
+----------------
+public class Solution {
+  private boolean check(List<Long> seen, long n) {
+    return seen.stream().filter(x -> x == n).count() == 0;
+  }
+
+  public String fractionToDecimal(int numerator, int denominator) {
+    long n = Math.abs((long) numerator);
+    long d = Math.abs((long) denominator);
+    StringBuilder sb = new StringBuilder(String.valueOf(n / d));
+    if (numerator < 0 && denominator > 0)
+      sb.insert(0, "-");
+    if (numerator > 0 && denominator < 0)
+      sb.insert(0, "-");
+    if (numerator % denominator == 0) {
+      return sb.toString();
+    }
+    sb.append('.');
+
+    n = n % d;
+    List<Long> seen = new ArrayList<>();
+    while (n != 0 && check(seen, n)) {
+      seen.add(n);
+      sb.append(n * 10 / d);
+      n = (n * 10) % d;
+    }
+
+    if (n == 0)
+      return sb.toString();
+
+    int idx = sb.length() - 1;
+    for (int i = seen.size() - 1; i >= 0; i--) {
+      if (seen.get(i) == n) {
+        break;
+      }
+      idx--;
+    }
+    sb.insert(idx, "(");
+    sb.append(')');
+
+    return sb.toString();
+  }
+}
