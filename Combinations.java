6884ba76f46d750ff9e5de23bf32dc6e295ba1f5
huijunwu <huijun.wu.2010@gmail.com>
2016-03-09 01:28:27 -0700

    dfs with path collection

--- Combinations.java
+++ Combinations.java
@@ -43,3 +43,29 @@ public class Solution {
     return c(n, k, 1);
   }
 }
+-----------------
+public class Solution {
+  List<List<Integer>> c(int n, int startPos, int k) {
+    List<List<Integer>> ret = new ArrayList<>(); // return
+
+    if (k == 0) { // termination
+      List<Integer> l = new ArrayList<>(); // termination item
+      ret.add(l);
+      return ret;
+    }
+
+    for (int i = startPos; i <= n; i++) { // branch
+      List<List<Integer>> r = c(n, i + 1, k - 1); // dfs recursion children
+      for (List<Integer> l : r) { // iterate children
+        l.add(0, i); // construct
+        ret.add(l); // add to current node
+      }
+    }
+
+    return ret; // default termination
+  }
+
+  public List<List<Integer>> combine(int n, int k) {
+    return c(n, 1, k);
+  }
+}
