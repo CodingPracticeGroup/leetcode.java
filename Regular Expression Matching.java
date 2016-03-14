81680bb993332ca28c6f2c3e1d5d7dd0db084500
huijunwu <huijun.wu.2010@gmail.com>
2016-03-14 04:10:24 -0700

    batch twitter

--- Regular Expression Matching.java
+++ Regular Expression Matching.java
@@ -119,3 +119,36 @@ public class Solution {
     return dp[s.length()][p.length()];
   }
 }
+-------------------------
+public class Solution {
+  public boolean isMatch(String s, String p) {
+    boolean dp[][] = new boolean[s.length() + 1][p.length() + 1];
+    dp[0][0] = true;
+    for (int i = 1; i <= s.length(); i++) {
+      dp[i][0] = false;
+    }
+    for (int j = 1; j <= p.length(); j++) {
+      if (p.charAt(j - 1) == '*') {
+        dp[0][j] = dp[0][j - 2];
+      } else {
+        dp[0][j] = false;
+      }
+    }
+    for (int i = 1; i <= s.length(); i++) {
+      for (int j = 1; j <= p.length(); j++) {
+        if (p.charAt(j - 1) == '.') {
+          dp[i][j] = dp[i - 1][j - 1];
+        } else if (p.charAt(j - 1) == '*') {
+          dp[i][j] = false;
+          dp[i][j] |= dp[i][j - 2]; // 0
+          dp[i][j] |= dp[i][j - 1]; // 1
+          dp[i][j] |= dp[i - 1][j] && p.charAt(j - 2) == '.'; // multiple
+          dp[i][j] |= dp[i - 1][j] && p.charAt(j - 2) == s.charAt(i - 1); // multiple
+        } else {
+          dp[i][j] = dp[i - 1][j - 1] && s.charAt(i - 1) == p.charAt(j - 1);
+        }
+      }
+    }
+    return dp[s.length()][p.length()];
+  }
+}
--- String to Integer (atoi).java
+++ String to Integer (atoi).java
@@ -44,3 +44,41 @@ public class Solution {
     return (int) l;
   }
 }
+-------------------
+public class Solution {
+  public int myAtoi(String str) {
+    str = str.trim();
+    if (str.isEmpty())
+      return 0;
+
+    boolean negative = false;
+    if (str.charAt(0) == '-') {
+      negative = true;
+      str = str.substring(1);
+    } else if (str.charAt(0) == '+') {
+      str = str.substring(1);
+    }
+
+    long d = 0;
+    for (int i = 0; i < str.length(); i++) {
+      if (str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+        d *= 10;
+        d += str.charAt(i) - '0';
+        if (d > 1L + Integer.MAX_VALUE) {
+          break;
+        }
+      } else {
+        break;
+      }
+    }
+
+    if (!negative && d > Integer.MAX_VALUE) {
+      return Integer.MAX_VALUE;
+    }
+    if (negative && -d < Integer.MIN_VALUE) {
+      return Integer.MIN_VALUE;
+    }
+
+    return negative ? -(int) d : (int) d;
+  }
+}
