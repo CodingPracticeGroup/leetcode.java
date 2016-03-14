bb6683631ef03cf92e1159cd1f87ca3d4a8dc959
huijunwu <huijun.wu.2010@gmail.com>
2016-03-14 07:24:44 -0700

    batch matrix dp less storage

--- Unique Paths II.java
+++ Unique Paths II.java
@@ -76,3 +76,28 @@ public class Solution {
     return obstacleGrid[m - 1][n - 1] > 0 ? obstacleGrid[m - 1][n - 1] : 0;
   }
 }
+--------------------
+public class Solution {
+  public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+    int dp[] = new int[obstacleGrid[0].length];
+    dp[0] = obstacleGrid[0][0] == 1 ? 0 : 1;
+    for (int i = 1; i < obstacleGrid[0].length; i++) {
+      if (obstacleGrid[0][i] == 1) {
+        dp[i] = 0;
+      } else {
+        dp[i] = dp[i - 1];
+      }
+    }
+    for (int i = 1; i < obstacleGrid.length; i++) {
+      dp[0] = obstacleGrid[i][0] == 1 ? 0 : dp[0];
+      for (int j = 1; j < obstacleGrid[0].length; j++) {
+        if (obstacleGrid[i][j] == 1) {
+          dp[j] = 0;
+        } else {
+          dp[j] += dp[j - 1];
+        }
+      }
+    }
+    return dp[obstacleGrid[0].length - 1];
+  }
+}
--- Unique Paths.java
+++ Unique Paths.java
@@ -15,3 +15,17 @@ public class Solution {
     return dp[m - 1][n - 1];
   }
 }
+--------------
+public class Solution {
+  public int uniquePaths(int m, int n) {
+    int dp[] = new int[n];
+    Arrays.fill(dp, 1);
+    for (int i = 1; i < m; i++) {
+      dp[0] = 1;
+      for (int j = 1; j < n; j++) {
+        dp[j] += dp[j - 1];
+      }
+    }
+    return dp[n - 1];
+  }
+}
