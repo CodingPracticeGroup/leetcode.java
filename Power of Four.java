b0ea767bc1ad19d9ffc22d7157378d2905df5bb0
huijunwu <huijun.wu.2010@gmail.com>
2016-04-18 02:30:05 -0700

    342 bit

---
+++ Power of Four.java
@@ -0,0 +1,37 @@
+public class Solution {
+  public boolean isPowerOfFour(int num) {
+    if (num == (1 << 0))
+      return true;
+    if (num == (1 << 2))
+      return true;
+    if (num == (1 << 4))
+      return true;
+    if (num == (1 << 6))
+      return true;
+    if (num == (1 << 8))
+      return true;
+    if (num == (1 << 10))
+      return true;
+    if (num == (1 << 12))
+      return true;
+    if (num == (1 << 14))
+      return true;
+    if (num == (1 << 16))
+      return true;
+    if (num == (1 << 18))
+      return true;
+    if (num == (1 << 20))
+      return true;
+    if (num == (1 << 22))
+      return true;
+    if (num == (1 << 24))
+      return true;
+    if (num == (1 << 26))
+      return true;
+    if (num == (1 << 28))
+      return true;
+    if (num == (1 << 30))
+      return true;
+    return false;
+  }
+}
